e013558be6abcd52d7f3189993c6f06486359e37
Anibel America <ameriabel@gmail.com>
2018-09-04 14:48:06 -0700

    adds tests to check for invalid input dates to DateRange

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,9 +1,15 @@
+require 'date'
+
 class DateRange
   attr_reader :start_date, :end_date
 
   def initialize(start_date, end_date)
-    @start_date = start_date
-    @end_date = end_date
+    if start_date > end_date
+      raise ArgumentError, "Invalid dates given"
+    else
+      @start_date = start_date
+      @end_date = end_date
+    end
   end
 
 end
--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -14,9 +14,16 @@ describe 'DateRange class' do
       expect(DateRange.new(@start_date, @end_date)).must_be_instance_of DateRange
     end
 
-    # possibly unnecessary
-    it 'raises an error if either input argument is ""' do
-      expect{DateRange.new()}.must_raise StandardError
+    # # possibly unnecessary
+    # it 'raises an error if either input argument is ""' do
+    #   expect{DateRange.new()}.must_raise StandardError
+    # end
+
+    it 'raises an Argument error if given invalid date range' do
+      expect{
+        DateRange.new(@start_date, @start_date - 2)
+      }.must_raise ArgumentError
     end
+
   end
 end
