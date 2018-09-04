3275135ac668469f37bb5030573865cac3e8d71e
kangazoom <michellelcronin@gmail.com>
2018-09-04 16:24:15 -0700

    Created Reservation#dates_reserved + tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,13 +1,20 @@
+require 'time'
+
 module Hotel
   class Reservation
-    attr_accessor :id, :room_num, :date_start, :date_end, :cost
+    attr_accessor :id, :room_num, :start_date, :end_date, :cost
 
-    def initialize(id, room_num, date_start, date_end, cost=200)
+    def initialize(id, room_num, start_date, end_date, cost=200)
       @id = id
       @room_num = room_num
-      @date_start = date_start
-      @date_end = date_end
+      @start_date = start_date
+      @end_date = end_date
       @cost = cost
+    end
+
+    def dates_reserved()
+      # don't include the end end_date since that is the checkout date
+      return (@start_date...@end_date).to_a
     end
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -16,14 +16,33 @@ describe "Reservation" do
   # TODO: test cost as float?
   # TODO: test id as any pos int
   # TODO: test room_num as 1-20
-  let(:start_date) {Time.new(2004, 7, 1)}
-  let(:end_date) {Time.new(2004, 7, 4)}
+  let(:start_date) {Date.new(2004, 7, 1)}
+  let(:end_date) {Date.new(2004, 7, 4)}
   let(:reservation) {Hotel::Reservation.new(2, 3, start_date, end_date)}
 
-  describe "#initialize"
+  describe "#initialize" do
     it "can create a new instance of reservation" do
       expect(reservation).must_be_kind_of Hotel::Reservation
       expect(reservation.id).must_equal 2
+    end
+  end
+
+  describe "#dates_reserved" do
+    # TODO: test edge cases like same start/end date
+    # TODO: error handling for bad start/end dates (above?)
+    it "returns an array of Time objects" do
+
+      expect(reservation.dates_reserved).must_be_kind_of Array
+      expect(reservation.dates_reserved()[0]).must_be_kind_of Date
+
+      second_date = Date.new(2004, 7, 2)
+      expect(reservation.dates_reserved[1]).must_equal second_date
+    end
+    it "can list all the dates of a reservation" do
+      expect(reservation.dates_reserved.length).must_equal 3
+
+      second_date = Date.new(2004, 7, 2)
+      expect(reservation.dates_reserved[1]).must_equal second_date
     end
   end
 end
