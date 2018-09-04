4dc2b0ec4a25024460daca95a4f40e31cb4cc4d4
Anibel America <ameriabel@gmail.com>
2018-09-04 14:21:09 -0700

    adds class instantiation test for date range class

--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -1,1 +1,13 @@
+require 'date'
+
 require_relative 'spec_helper'
+
+describe 'DateRange class' do
+  describe 'DateRange instantiation' do
+    it 'creates an instance of DateRange' do
+      start_date = Date.new(2000, 1, 1)
+      end_date = start_date + 2
+      expect(DateRange.new(start_date, end_date)).must_be_instance_of DateRange
+    end
+  end
+end
