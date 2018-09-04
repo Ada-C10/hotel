2b292b800ebde3040a4a5417a344dd1f1e18c6cd
Karis Kim <ouniiq@hotmail.com>
2018-09-04 16:25:42 -0700

    added test to see if initialize method raises argument errors and if 3 instance variables are created

--- spec/reservation_creator_spec.rb
+++ spec/reservation_creator_spec.rb
@@ -1,12 +1,22 @@
 require_relative 'spec_helper'
 
 describe "BookingSystem class" do
-  before do
-    list = Hotel::Reservation.new
-  end
+  # before do
+  #   list = Hotel::Reservation.new
+  # end
 
   it "rooms in hotel method" do
-    expect(list.rooms_in_hotel).must_be_kind_of Array
-    expect(list.rooms_in_hotel).must_equal [*"Room 1".."Room 20"]
+    list = Hotel::Reservation.new(2, 180904, 180905)
+
+    expect(list).must_be_kind_of Hotel::Reservation
+    expect(list.room_number).must_be_kind_of Integer
+    expect(list.room_number).must_equal 2
+    expect(list.check_in_date).must_equal Date.parse("2018-09-04")
+    expect(list.check_out_date).must_equal Date.parse("2018-09-05")
+  end
+
+  it "raises ArgumentError if check out date is before check in date" do
+
+    expect{(list = Hotel::Reservation.new(2, 180904, 180903))}.must_raise ArgumentError
   end
 end
