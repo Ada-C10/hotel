21a6ef07628eb3c2a0d7f3e3c090c15dcd24285c
Kat Perreira <kat.perreira@gmail.com>
2018-09-04 15:41:50 -0700

    Wrote test to check for invalid room argument

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,16 +2,24 @@
 require_relative 'spec_helper.rb'
 
 describe "initialize" do
-  it 'Takes in a room number and avaliability status' do
+
+# Test to make sure it takes correct parameters
+  it 'Takes in a room number and availability status' do
     room_number = 5
-    avaliability = :avaliable
-    test_room = Room.new(room_number, avaliability)
+    availability = :avaliable
+    test_room = Room.new(room_number, availability)
 
     expect(test_room).must_respond_to :room_number
     expect(test_room.room_number).must_equal room_number
 
-    expect(test_room).must_respond_to :avaliability
-    expect(test_room.avaliability).must_equal avaliability
-
+    expect(test_room).must_respond_to :availability
+    expect(test_room.availability).must_equal availability
   end
+
+ # Test to make sure that rooms checked are only 1-20
+  it 'Raise argument error for invalid room number' do
+    expect{Room.new(237, :avaliable)}.must_raise ArgumentError
+  end
+
+# initialize end
 end
