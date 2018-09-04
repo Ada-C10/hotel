dadfa4b07c5577c8392cddf9c7ea35ba70531ffc
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-04 14:10:15 -0700

    Test and implement Room.all

--- lib/room.rb
+++ lib/room.rb
@@ -1,7 +1,18 @@
 module Hotel
   class Room
+    attr_reader :id
     def initialize(id)
       @id = id
+    end
+
+    def self.all
+      rooms = []
+
+      20.times do |i|
+        rooms << Room.new(i+1)
+      end
+
+      return rooms
     end
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -6,5 +6,37 @@ describe "Room" do
       room = Hotel::Room.new(1)
       expect(room).must_be_kind_of Hotel::Room
     end
+
+    it "Keeps track of ID" do
+      id = 1337
+      room = Hotel::Room.new(id)
+      expect(room).must_respond_to :id
+      expect(room.id).must_equal id
+    end
+  end
+
+  describe "all" do
+    it "Returns an array full of Rooms" do
+      rooms = Hotel::Room.all
+
+      rooms.each do |room|
+        expect(room).must_be_kind_of Hotel::Room
+      end
+    end
+
+    it "Returns the correct number of rooms" do
+      rooms = Hotel::Room.all
+      expect(rooms.length).must_equal 20
+    end
+
+    it "Gets the first Market from the array" do
+      rooms = Hotel::Room.all
+      expect(rooms.first.id).must_equal 1
+    end
+
+    it "Gets the last Market from the array" do
+      rooms = Hotel::Room.all
+      expect(rooms.last.id).must_equal 20
+    end
   end
 end
