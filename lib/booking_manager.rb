a8c928fdf4db1f79d93ddc9c4bd43b7482da5894
Katricia Smith <smithkatricia@gmail.com>
2018-09-04 15:26:08 -0700

    Added BookingManager#populate_room_list to create list of hotel rooms

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -5,14 +5,21 @@ module Hotel
     attr_accessor :rooms, :reservations
 
     def initialize
-      @rooms = list_rooms
-      @reservations = list_reservations
+      @rooms = populate_room_list
+      @reservations = reservations
     end # of def initialize
 
     # Create list of rooms as list of room Instances
-    def populate_room_list
+    def populate_room_list(number_rooms)
+      rooms = []
+      num = 1
 
-    end
+      number_rooms.times do |room|
+        room = Room.new(num)
+        rooms << room
+        num += 1
+      end # of number_rooms loop
+    end # of populate_room_list method 
 
     # Method to list all rooms in hotel
     # def list_rooms
@@ -24,6 +31,6 @@ module Hotel
     # end
 
     # Method to get total cost of reservation
-    
+
   end # of class BookingManager
 end # of module Hotel
