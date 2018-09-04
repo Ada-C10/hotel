befe294c446e595ca8141c94aecca1ba6bf39f42
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-04 16:44:54 -0700

    Added method .is_available? for class Room and corresponding unit test

--- lib/room.rb
+++ lib/room.rb
@@ -3,13 +3,25 @@ require 'date'
 
 class Room
   attr_reader :room_number, :dates_booked
-  
+
   def initialize(room_number)
     @room_number = room_number
     @dates_booked = []
   end
 
+  # date must be entered in the format: yyyy,mm,dd
   def is_available?(date)
 
+    parsed_date = date.split(",")
+    year = parsed_date[0].to_i
+    month = parsed_date[1].to_i
+    day = parsed_date[2].to_i
+    formated_date = Date.new(year,month,day)
+
+    if @dates_booked.include?(formated_date)
+      return false
+    else
+      return true
+    end
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -6,4 +6,24 @@ describe 'Room' do
     expect(new_room).must_be_kind_of Room
   end
 
+  it 'checks if the room is available on the specified date' do
+    new_room = Room.new(15)
+    reservation_date_1 = Date.new(2018, 9, 10)
+    reservation_date_2 = Date.new(2018, 9, 11)
+    new_room.dates_booked << reservation_date_1
+    new_room.dates_booked << reservation_date_2
+
+    expect(new_room.is_available?("2018, 9, 11")).must_equal false
+  end
+
+  it 'checks if the room is available on the specified date' do
+    new_room = Room.new(15)
+    reservation_date_1 = Date.new(2018, 9, 10)
+    reservation_date_2 = Date.new(2018, 9, 11)
+    new_room.dates_booked << reservation_date_1
+    new_room.dates_booked << reservation_date_2
+
+    expect(new_room.is_available?("2018, 9, 13")).must_equal true
+  end
+
 end
