1cdae780056f7b69f9ee8dc5f4a149605d2504af
Anibel America <ameriabel@gmail.com>
2018-09-04 15:39:01 -0700

    adds nominal test case for get_cost method of reservation class

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -2,16 +2,27 @@ require_relative 'spec_helper'
 
 describe 'Reservation class' do
 
+  let (:reservation) {
+    start_date = Date.new(2020, 1, 1)
+    end_date = start_date + 2
+    Hotel::Reservation.new(start_date, end_date, 1)
+  }
+
   describe 'Reservation instantiation' do
-    before do
-      @start_date = Date.new(2020, 1, 1)
-      @end_date = @start_date + 2
-    end
 
     it 'creates an instance of Reservation' do
-      reservation = Hotel::Reservation.new(@start_date, @end_date, 1)
       expect(reservation).must_be_instance_of Hotel::Reservation
     end
+
+  end
+
+  describe 'get_cost method' do
+
+    it 'returns the correct cost of a room' do
+      expect(reservation.get_cost).must_equal BASE_COST * 2
+    end
+
+
   end
 
 end
