66e4bd9d16fbbcc94cb12a35c47dc769f530fa21
Addison Cole <addison.cole85@gmail.com>
2018-09-04 16:55:36 -0700

    add spec to test that load_rooms helper method loads only valid room numbers.

--- spec/hotel_manager_spec.rb
+++ spec/hotel_manager_spec.rb
@@ -43,6 +43,13 @@ describe "Hotel Manager class" do
       expect(@hotel.find_room(20)).must_be_kind_of Room
       # binding.pry
     end
+
+    it 'must be a valid room number 1-20' do
+      ROOM_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      @hotel.rooms.each do |room|
+        expect(ROOM_NUMBERS.include?(room.room_number)).must_equal true
+      end
+    end
   end
 
 end
