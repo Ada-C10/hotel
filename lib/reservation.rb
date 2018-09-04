99d8ee2325ef682824267300db824b6fdd8d2ec0
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-04 15:20:28 -0700

    Added logic to class Reservation to check that a valid room number was entered and corresponding unit test. quit close

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -3,15 +3,23 @@ class Reservation
 
   def initialize(reservation_num, room_num, start_date, end_date, room_type)
     @reservation_num = reservation_num.to_i
-    @room_num = room_num.to_i
 
-    # dates must be entered in the format: yyyy,mm,dd
+    # Verify valid room number entered
+    all_room_numbers = [*1..20]
+    if all_room_numbers.include?(room_num.to_i)
+      @room_num = room_num.to_i
+    else
+      raise ArgumentError.new("Invalid room number entered.")
+    end
+
+    # start date must be entered in the format: yyyy,mm,dd
     parsed_start_date = start_date.split(",")
     start_year = parsed_start_date[0].to_i
     start_month = parsed_start_date[1].to_i
     start_day = parsed_start_date[2].to_i
     @start_date = Date.new(start_year, start_month, start_day)
 
+    # end date must be entered in the format: yyyy,mm,dd
     parsed_end_date = end_date.split(",")
     end_year = parsed_end_date[0].to_i
     end_month = parsed_end_date[1].to_i
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,6 +1,7 @@
 require_relative 'spec_helper'
 
 describe 'Reservation' do
+
   it 'creates an instance of Reservation' do
     reservation_num = 5555
     room_num = 1
@@ -11,6 +12,16 @@ describe 'Reservation' do
     new_reservation = Reservation.new(reservation_num, room_num, start_date, end_date, room_type)
 
     expect(new_reservation).must_be_kind_of Reservation
+  end
+
+  it 'raises an ArgumentError if an invalid room is entered' do
+    reservation_num = 666
+    room_num = 30
+    start_date = "2018,10,6"
+    end_date = "2018,10,8"
+    room_type = :standard
+
+    expect{Reservation.new(reservation_num, room_num, start_date, end_date, room_type)}.must_raise ArgumentError
   end
 
 end
