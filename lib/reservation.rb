ce3ba4b0a3ea0024ceb7301d85a739adbe7ed9ae
Aurea <fu.sakura@gmail.com>
2018-09-04 14:37:58 -0700

    add reservation class with total_nights and total_cost method

---
+++ lib/reservation.rb
@@ -0,0 +1,45 @@
+module Hotel
+
+class Reservation
+
+  attr_reader :room_num, :start_date, :end_date, :rate
+
+  def initialize(room_num, start_date, end_date, rate = 200)
+
+    unless start_date.is_a?(Date) && end_date.is_a?(Date)
+      raise ArgumentError, "Start and/or End dates are not in correct format"
+    end
+
+
+    unless room_num.is_a?(Integer) && room_num >= 1 && room_num <= 20
+      raise ArgumentError, "Room number is not valid number"
+    end
+
+    unless rate.is_a?(Numeric) && rate > 0
+      raise ArgumentError, "Rate is not valid number"
+    end
+
+
+    @room_num = room_num
+    @start_date = start_date
+    @end_date = end_date
+    @rate = rate
+
+    unless total_nights > 0
+      raise ArgumentError, "Invalid start/end dates"
+    end
+
+  end
+
+  def total_nights
+    return (end_date - start_date).to_i
+  end
+
+  def total_cost
+    return (total_nights * rate).round(2)
+  end
+
+
+
+end
+end
---
+++ spec/reservation_spec.rb
@@ -0,0 +1,67 @@
+require_relative 'spec_helper'
+
+describe 'reservation class' do
+  describe 'initializing a reservation' do
+
+    it 'returns reservation object given correct inputs' do
+
+      expect( Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,8) )).must_be_instance_of(Hotel::Reservation)
+      expect( Hotel::Reservation.new(10, Date.new(2018,6,4), Date.new(2018,7,4), 201 )).must_be_instance_of(Hotel::Reservation)
+    end
+
+    it 'raise argument error given wrong input format' do
+
+      expect{
+        Hotel::Reservation.new('1', Date.new(2018,7,4), Date.new(2018,7,8))
+      }.must_raise(ArgumentError)
+      expect{
+        Hotel::Reservation.new(1, '20180503', Date.new(2018,7,8))
+      }.must_raise(ArgumentError)
+      expect{
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,8), 'RATE')
+      }.must_raise(ArgumentError)
+      expect{
+        Hotel::Reservation.new([], Date.new(2018,7,4), Date.new(2018,7,8) , 300)
+      }.must_raise(ArgumentError)
+    end
+
+    it 'raise argument error if start date is after end date' do
+
+      expect{
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,1))
+      }.must_raise(ArgumentError)
+    end
+
+    it 'raise argument error if start date is same as end date' do
+
+      expect{
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,4))
+      }.must_raise(ArgumentError)
+    end
+  end
+
+  describe 'days stayed' do
+    it 'returns correct number of days stayed' do
+
+      expect(
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,5)).num_nights
+      ).must_equal(1)
+      expect(
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,10)).num_nights
+      ).must_equal(6)
+    end
+
+  describe 'total_cost' do
+    it 'returns correct total cost of reservation' do
+      expect(
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,5)).total_cost
+      ).must_equal(200)
+      expect(
+        Hotel::Reservation.new(1, Date.new(2018,7,4), Date.new(2018,7,10), 305).total_cost
+      ).must_equal(1830)
+    end
+  end
+
+  end
+
+end
