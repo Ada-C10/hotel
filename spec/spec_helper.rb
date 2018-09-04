000220e86e273a99160836b3d125ebd3f588aa87
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-04 15:26:31 -0700

    Add lines to require relative for lib files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,13 @@ SimpleCov.start
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# Require_relative your lib files here!
+require_relative '../lib/room'
+require_relative '../lib/reservation'
+require_relative '../lib/reservation_agent'
+require_relative '../lib/reservation_tracker'
+require_relative '../lib/reservation_maker'
