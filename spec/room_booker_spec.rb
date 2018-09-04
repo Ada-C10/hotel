f7e7024ac573c0494bdf8fe20a52bfb55390d8a3
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-04 16:18:06 -0700

    Add test for RoomBooker#new_reservation

--- spec/room_booker_spec.rb
+++ spec/room_booker_spec.rb
@@ -10,5 +10,15 @@ describe 'Wave 1' do
     end
   end
 
+  describe 'new_reservation method' do
+    it 'adds a new reservation to master collection' do
+      room_id = 14
+      check_in = Date.new(2018, 4, 1)
+      check_out = Date.new(2018, 4, 2)
+      reservation = hotel.new_reservation(room_id, check_in, check_out)
 
+      expect(@reservations.last).must_be_instance_of Reservation
+      expect(@reservations.last).must_equal reservation
+    end
+  end
 end
