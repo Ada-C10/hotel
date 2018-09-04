8602647dad2d98c38900599242e35631e7d3522e
Katricia Smith <smithkatricia@gmail.com>
2018-09-04 15:41:26 -0700

    Added require relative booking manager to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,7 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative '../lib/room.rb'
+require_relative '../lib/booking_manager.rb'
+
+# require_relative '..'
