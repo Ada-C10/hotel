127b6cbfaa37735058c40e51a805b97af8759259
Katricia Smith <smithkatricia@gmail.com>
2018-09-04 15:36:13 -0700

    Added instance variable for availabiity and Room#check_availability pseudocode

--- lib/room.rb
+++ lib/room.rb
@@ -1,11 +1,22 @@
+require_relative 'booking_manager'
 
 module Hotel
   class Room
     attr_reader :number
+    attr_accessor :availability
 
     def initialize(number)
-      @number = number
+      @number = number.to_i
+      @availability = check_availability
     end
+
+    def check_availability(start_date, end_date)
+      # Do..something with dates.
+      # if date of check is within range
+      # return boolean
+      availability = true 
+      return availability
+    end # of check_availability
 
   end # of class Room
 end # of module Hotel
