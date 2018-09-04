295ddd984ed3324b090f5c43bf8ffc13eccef3a0
Anibel America <ameriabel@gmail.com>
2018-09-04 14:13:43 -0700

    added fix to guardfile, added empty files for classes and spec files

---
+++ lib/hotel.rb
@@ -0,0 +1,8 @@
+class Hotel
+  attr_reader :hotel
+
+  def initialize
+    @hotel
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
+require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/reporters'
 # Add simplecov
@@ -6,3 +10,9 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+# require_relative 'hotel_spec'
+# require_relative '../lib/hotel'
+
+require_relative '../lib/reservation'
+require_relative '../lib/date_range'
+require_relative '../lib/reservation_manager'
