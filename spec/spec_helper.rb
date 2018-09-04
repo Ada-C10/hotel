5fbf623ad89932838b2929c66021526710a06ead
Kat Perreira <kat.perreira@gmail.com>
2018-09-04 14:45:57 -0700

    Added require_relative to room.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
+
+require 'simplecov'
+SimpleCov.start
+
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require_relative '../lib/room.rb'
 # Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
