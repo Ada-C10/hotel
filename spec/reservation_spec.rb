a767ab27635a6c9a801d5516f95f950212ae91b1
Maryam Shitu <maryamshitu@gmail.com>
2018-09-04 15:07:31 -0700

    intial file set up spec files running

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,12 +1,18 @@
-require 'simplecov'
-SimpleCov.start
+require_relative 'spec_helper'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative '../lib/room.rb'
 require_relative '../lib/reservation.rb'
 # Require_relative your lib files here!
+
+describe "Reservation" do
+  it "returns an instance of Reservation" do
+    new_room = Reservation.new
+    expect(new_room).must_be_kind_of Reservation
+  end
+
+end
