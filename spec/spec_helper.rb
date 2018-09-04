15f4230af65002481ca121b1946582e417d158e1
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-04 14:24:01 -0700

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
