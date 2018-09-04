9f8bc68c8773214799502bcd9a807199e95cdfb0
Kat Perreira <kat.perreira@gmail.com>
2018-09-04 15:41:16 -0700

    Raises Argument error for an invalid room number

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,21 @@
 
+require 'pry'
 
 class Room
   attr_reader :room_number
-  attr_reader :avaliability
+  attr_reader :availability
 
-  def initialize(room_number, avaliability)
+  def initialize(room_number, availability)
     @room_number = room_number
-    @avaliability = avaliability
+    @availability = availability
+
+    valid_rooms = (1..20).to_a
+    if valid_rooms.include?(room_number)
+      red_rum = false
+    else
+      raise ArgumentError.new("Invalid room number")
+    end
+    # binding.pry
   end
 
 
