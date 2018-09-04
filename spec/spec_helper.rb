2383bdfc813f5585cbbdc9e9fb22725668774f75
Danielle Metzner <danimetzner@gmail.com>
2018-09-04 15:03:39 -0700

    Added reservation manager to the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
 require_relative '../lib/room.rb'
+require_relative '../lib/reservation_mgr.rb'
