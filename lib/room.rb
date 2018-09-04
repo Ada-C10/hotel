18898cc07dccd8ed3e27bb065ab20142371b9852
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-04 15:27:46 -0700

    remove super in input, and modify checking room number range

--- lib/room.rb
+++ lib/room.rb
@@ -7,13 +7,12 @@ module Hotel
     attr_reader :room_number, :booked_reservations, :status
 
     def initialize(input)
-      super(input)
       @room_number = input[:room_number]
       @booked_reservations = input[:booked_reservations].nil? ? [] : input[:booked_reservations]
       @status = input[:status].nil? ? :AVAILABLE : input[:status]
 
       raise ArgumentError.new("That is an invalid status") unless STATUS_OPTIONS.include?(@status)
-      raise ArgumentError.new("That is an invalid Room Number") unless @room_number > 0 && < 21
+      raise ArgumentError.new("That is an invalid Room Number") unless @room_number.between?(1, 20)
     end
 
     def add_booked_reservation(reservation)
