e3ede8fa2d907b8f84dd8fb1f66e49b6d7b9d9f1
Anibel America <ameriabel@gmail.com>
2018-09-04 14:35:27 -0700

    added test to check for input, may remove later

--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -3,11 +3,20 @@ require 'date'
 require_relative 'spec_helper'
 
 describe 'DateRange class' do
+
   describe 'DateRange instantiation' do
+    before do
+      @start_date = Date.new(2000, 1, 1)
+      @end_date = @start_date + 2
+    end
+
     it 'creates an instance of DateRange' do
-      start_date = Date.new(2000, 1, 1)
-      end_date = start_date + 2
-      expect(DateRange.new(start_date, end_date)).must_be_instance_of DateRange
+      expect(DateRange.new(@start_date, @end_date)).must_be_instance_of DateRange
+    end
+
+    # possibly unnecessary
+    it 'raises an error if either input argument is ""' do
+      expect{DateRange.new()}.must_raise StandardError
     end
   end
 end
