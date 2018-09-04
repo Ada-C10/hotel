3cd6c0408985dcffdc378eb4d0f881b4f5893af0
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-04 16:55:25 -0700

    Initialize RoomBooker class, add RoomBooker#populate_rooms

--- lib/room_booker.rb
+++ lib/room_booker.rb
@@ -1,4 +1,26 @@
 module BookingLogic
   class RoomBooker
+    attr_reader :rooms
+    attr_accessor :reservations, :blocks
+
+    def initialize
+      @rooms = populate_rooms
+      @reservations = []
+      @blocks = []
+    end
+
+    Room = Struct.new(:id, :cost)
+
+    def populate_rooms
+      rooms_array = []
+
+      20.times do |id|
+        room = Room.new((id + 1), 200)
+        rooms_array << room
+      end
+
+      return rooms_array
+    end
+
   end
 end
