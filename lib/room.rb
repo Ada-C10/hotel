12f19e51206d623cad52e26a26e66fee42191ea7
Kat Perreira <kat.perreira@gmail.com>
2018-09-04 14:43:42 -0700

    Created a Room class, initialized room and status

---
+++ lib/room.rb
@@ -0,0 +1,14 @@
+
+
+class Room
+  attr_reader :room_number
+  attr_reader :avaliability
+
+  def initialize(room_number, avaliability)
+    @room_number = room_number
+    @avaliability = avaliability
+  end
+
+
+
+end
