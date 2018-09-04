78608fdb3f0ba3ce06261fe77f06095447a80f83
Danielle Metzner <danimetzner@gmail.com>
2018-09-04 13:55:46 -0700

    Created spec_helper and required room in file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative '../lib/room.rb'
