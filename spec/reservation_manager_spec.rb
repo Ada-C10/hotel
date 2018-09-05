5e2693af7ced2fff7dc7e808545779f646be4af3
Anibel America <ameriabel@gmail.com>
2018-09-04 19:05:50 -0700

    edits let statements in Reservation manager spec file, adds additional tests for initialization method

--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -2,29 +2,48 @@ require_relative 'spec_helper'
 
 describe 'ReservationManager class' do
 
-  let (:reservation) {
+  let (:reservation1) {
     start_date = Date.today + 1
     end_date = start_date + 2
     Hotel::Reservation.new(start_date, end_date, 1)
   }
 
+  let (:reservation2) {
+    start_date = Date.today + 2
+    end_date = start_date + 5
+    Hotel::Reservation.new(start_date, end_date, 2)
+  }
+
   let (:reservation_manager) {
-    Hotel::ReservationManager.new
+    Hotel::ReservationManager.new(all_reservations: [reservation1, reservation2])
   }
 
   describe 'ReservationManager instantiation' do
     it 'creates an instance of ReservationManager class' do
-      expect(reservation_manager).must_be_instance_of Hotel::ReservationManager
+      expect(Hotel::ReservationManager.new).must_be_instance_of Hotel::ReservationManager
     end
 
     it 'creates an instance of ReservationManager class with reservations as input'do
-      expect(Hotel::ReservationManager.new(all_reservations: reservation)).must_be_instance_of Hotel::ReservationManager
+      expect(reservation_manager).must_be_instance_of Hotel::ReservationManager
+    end
+
+    it 'if reservations passed in, it stores all reservations in an array of Reservations' do
+      reservation_manager.all_reservations.each do |reservation|
+        expect(reservation).must_be_instance_of Hotel::Reservation
+      end
     end
   end
 
-  describe 'list_all_rooms function' do
+  describe 'list_all_rooms method' do
     it 'returns a formatted string of all the rooms' do
       expect(reservation_manager.list_all_rooms).must_be_instance_of String
+    end
+  end
+
+  describe 'get_all_reservations method' do
+    it 'returns a list of all reservations' do
+      # reservation_manager
+
     end
   end
 end
