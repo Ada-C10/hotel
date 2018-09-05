24c1d9c3dccf0b301e9ef43c601b4c24d32b5187
Carly Reiter <yepodacarly@gmail.com>
2018-09-04 21:35:21 -0700

    Added 5 instantiation tests to reservation_maker_specs and a class variable of @@reservations to the main reservation_maker file.

--- lib/reservation_maker.rb
+++ lib/reservation_maker.rb
@@ -6,7 +6,7 @@ class ReservationMaker
   #input is start and end date
   #output if, if a room is available, sends start, end, and room# to reservation to make an instance of reservation.
 
-  attr_reader :start_date, :end_date, :room_num
+  attr_reader :start_date, :end_date, :room_num, :reservation
 
   @@reservations = []
 
@@ -14,31 +14,32 @@ class ReservationMaker
     @start_date = start_date
     @end_date = end_date
     @room_num = room_num
+    @reservation = reservation
   end
 
-  def find_available_room
-    #find available room by comparing to list of @@reservations
-    return @room_num
-  end
+  # def find_available_room
+  #   #find available room by comparing to list of @@reservations
+  #   return @room_num
+  # end
 
   def self.reservations
     @@reservations
   end
 
   def create_reservation
-    reservation = Reservation.new(@start_date, @end_date, @room_num)
-    @@reservations << reservation
+    @reservation = Reservation.new(@start_date, @end_date, @room_num)
+    binding.pry
+    @@reservations << @reservation
     return @@reservations
   end
 
-  # Make new incidence of ReservationMaker each time a new request for a reservation is made?
-  #how compose a new reservation?  What type of data will it come in as?
-
 end
 
-# @start_date = Date.new(2018,2,3)
-# @end_date = Date.new(2018,2,5)
-# @room_num = 13
-#
-# example = ReservationMaker.new(@start_date, @end_date)
-# puts example.reservations
+start_date = Date.new(2018,2,3)
+end_date = Date.new(2018,2,5)
+@room_num = 13
+
+example = ReservationMaker.new(start_date, end_date)
+
+puts example.start_date
+puts example.create_reservation
--- spec/reservation_maker_spec.rb
+++ spec/reservation_maker_spec.rb
@@ -1,3 +1,24 @@
-require_relative 'spec_helper'
+equire_relative 'spec_helper'
 require 'pry'
 require 'date'
+
+describe "tests for various pieces in each incidence of ReservationMaker" do
+
+  before do
+    start_date = Date.new(2018,2,3)
+    end_date = Date.new(2018,2,5)
+    @new_reservation = ReservationMaker.new(start_date, end_date)
+  end
+
+  it "is an instance of ReservationMaker" do
+    expect(@new_reservation).must_be_kind_of ReservationMaker
+  end
+
+  it "is set up for specific attributes and data types" do
+
+    expect(@new_reservation.reservation).must_be_kind_of Hash
+    expect(@new_reservation.start_date).must_be_kind_of Date
+    expect(@new_reservation.end_date).must_be_kind_of Date
+
+  end
+end
