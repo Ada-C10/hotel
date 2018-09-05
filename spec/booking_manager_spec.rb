e090bb6ba349e8b244212110b82581fbc2ed447b
Katricia Smith <smithkatricia@gmail.com>
2018-09-04 17:06:17 -0700

    Deleted extra line spaces in booking_manager_spec.rb

--- spec/booking_manager_spec.rb
+++ spec/booking_manager_spec.rb
@@ -7,15 +7,12 @@ describe "BookingManager class" do
       expect(manager).must_be_kind_of Hotel::BookingManager
     end # of it instance bookingmanager
 
-    it "creates the proper structures for instance variables instantiated" do 
+    it "creates the proper structures for instance variables instantiated" do
       hotel_rooms = Hotel::BookingManager.new
 
       expect (hotel_rooms.rooms).must_be_kind_of Array
       expect (hotel_rooms.reservations).must_be_kind_of Array
     end
   end # of initializer describe
-
-
-
 
 end # end of describe BookingManager class
