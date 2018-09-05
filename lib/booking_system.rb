6f7affed1c23a23fd08bf3989132ac23a4c0e884
Naheed Arang <arang.na@gmail.com>
2018-09-05 16:46:04 -0700

    serach reservation method and tests in progress

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -10,25 +10,41 @@ module Hotel
     def load_rooms
       rooms = []
       i = 1
+
       20.times do
         rooms << i
         i += 1
       end
+
+      # @reservations = rooms.map do |room|
+      #   room = Hotel::Reservation.new(id: nil, room: room, start_date: nil, end_date: nil, price_per_night: 200)
+      # end
       return rooms
     end
 
     def make_reservation(start_date, end_date)
-      assigned_room = 1
+      assigned_room = assign_available_room(start_date, end_date)
       reservation = Hotel::Reservation.new(id: 1, room: assigned_room, start_date: start_date, end_date: end_date, price_per_night: 200)
       @reservations << reservation
     end
 
     def assign_available_room(new_d1, new_d2)
       @reservations.each do |reservation|
-        old_d1 = @reservation.start_date
-        old_d2 = @reservation.end_date
-        unless new_d1.between?(old_d1, old_d2) && new_d2.between?(old_d1, old_d2)
-        return @reservation.room
+        old_d1 = reservation.start_date
+        old_d2 = reservation.end_date
+        if new_d1.between?(old_d1, old_d2) == false && new_d2.between?(old_d1, old_d2) == false && old_d1.between?(new_d1, new_d2) == false && old_d2.between?(new_d1, new_d2) == false
+        return reservation.room
+        end
+      end
+    end
+
+    def search_reservations(specific_date)
+      reservations_within_date = []
+      @reservations.each do |reservation|
+        if specific_date.between?(reservation.start_date, reservation.end_date)
+          reservations_within_date << reservation
+          binding.pry
+          return reservations_within_date
         end
       end
     end
--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -4,31 +4,63 @@ require 'pry'
 
 describe "BookingSystem class" do
 
+  before do
+    @system = Hotel::BookingSystem.new
+  end
+
   describe "load rooms" do
     it "loads rooms in an array" do
-      @rooms = Hotel::BookingSystem.new.load_rooms
+      @rooms = @system.load_rooms
       # binding.pry
       expect(@rooms).must_be_kind_of Array
     end
+
+    # it "loads each room as an instance of Reservation" do
+    #   @rooms = @system.load_rooms
+    #   expect(@rooms[0]).must_be_instance_of Hotel::Reservation
+    # end
   end
 
   describe "make reservation" do
-    before do
-      @system = Hotel::BookingSystem.new
-    end
     it "makes a reservation" do
       @system.make_reservation(Date.new(2018,1,1), Date.new(2018,1,5))
+      reservation = Hotel::Reservation.new(id: 1, room: 1, start_date: Date.new(2018, 1, 1), end_date: Date.new(2018, 1, 5), price_per_night: 200)
       expect(@system.reservations.length).must_equal 1
-      binding.pry
-
     end
 
   end
 
-  describe "assign_available_room" do
-    it "finds the first available room" do
-    end
+  describe "assign room" do
+    it "assigns the first available room" do
 
+      # @system.make_reservation(Date.new(2018, 1, 1), Date.new(2018, 1, 5))
+
+      reservation1 = Hotel::Reservation.new(id: 1, room: 1, start_date: Date.new(2018, 1, 7), end_date: Date.new(2018, 1, 8), price_per_night: 200)
+      @system.reservations << reservation1
+
+      # @system.make_reservation(Date.new(2018, 1, 6), Date.new(2018, 1, 7))
+
+      reservation2 = Hotel::Reservation.new(id: 2, room: 2, start_date: Date.new(2018, 1, 1), end_date: Date.new(2018, 1, 5), price_per_night: 200)
+      @system.reservations << reservation2
+
+
+      reservation3 = Hotel::Reservation.new(id: 3, room: @system.assign_available_room(Date.new(2018, 1, 6), Date.new(2018, 1, 9)), start_date: Date.new(2018, 1, 6), end_date: Date.new(2018, 1, 9), price_per_night: 200)
+      @system.reservations << reservation3
+
+      expect(reservation3.room).must_equal 2
+    end
   end
 
+  describe "search reservation" do
+    it "searches for reservations that fall on a specific date" do
+
+      reservation1 = Hotel::Reservation.new(id: 1, room: 1, start_date: Date.new(2018, 1, 1), end_date: Date.new(2018, 1, 8), price_per_night: 200)
+      @system.reservations << reservation1
+
+      reservation2 = Hotel::Reservation.new(id: 2, room: 2, start_date: Date.new(2018, 1, 1), end_date: Date.new(2018, 1, 5), price_per_night: 200)
+      @system.reservations << reservation2
+
+      expect(@system.search_reservations(Date.new(2018, 1, 4))).must_be_instance_of Array
+    end
+  end
 end
