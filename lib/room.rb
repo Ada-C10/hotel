ff868d26dd4327f581198dbdba9206682c3da08b
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-05 13:56:33 -0700

    Remove status attribute

--- lib/room.rb
+++ lib/room.rb
@@ -4,24 +4,18 @@ STATUS_OPTIONS = [:AVAILABLE, :UNAVAILABLE]
 
 module Hotel
   class Room
-    attr_reader :room_number, :booked_reservations, :status
+    attr_reader :room_number, :booked_reservations
 
     def initialize(input)
       @room_number = input[:room_number]
       @booked_reservations = input[:booked_reservations].nil? ? [] : input[:booked_reservations]
-      @status = input[:status].nil? ? :AVAILABLE : input[:status]
 
-      raise ArgumentError.new("That is an invalid status") unless STATUS_OPTIONS.include?(@status)
       raise ArgumentError.new("That is an invalid Room Number") unless @room_number.between?(1, 20)
     end
 
     def add_booked_reservation(reservation)
       raise ArgumentError unless reservation.is_a? Reservation
       @booked_reservations << reservation
-    end
-
-    def becomes_unavailable
-      @status = :UNAVAILABLE
     end
   end
 end
