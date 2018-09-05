09bfbe153519fc71d642a864a096df5fbaa4dfb1
Kat Perreira <kat.perreira@gmail.com>
2018-09-05 11:48:50 -0700

    Tests for a raised argument error if check out is before check in

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -10,30 +10,33 @@ describe 'Reservation' do
   let (:check_out) {
     Date.new(2001, 1, 7)
   }
-  let (:reservation) {
+  let (:general_reservation) {
     Reservation.new(room, check_in, check_out)
+  }
+
+  let (:check_out2) {
+    Date.new(1980, 4, 10)
   }
 
 
   describe "#initialize" do
-
     # Test to make sure it takes correct parameters
     it 'Takes in a room number and availability status' do
 
-      expect(reservation).must_respond_to :room
-      expect(reservation.room).must_equal room
+      expect(general_reservation).must_respond_to :room
+      expect(general_reservation.room).must_equal room
 
-      expect(reservation).must_respond_to :check_in
-      expect(reservation.check_in).must_equal check_in
+      expect(general_reservation).must_respond_to :check_in
+      expect(general_reservation.check_in).must_equal check_in
 
-      expect(reservation).must_respond_to :check_out
-      expect(reservation.check_out).must_equal check_out
+      expect(general_reservation).must_respond_to :check_out
+      expect(general_reservation.check_out).must_equal check_out
 
-      # it 'raises an argument error if check out date is before check in date' do
-      #
-      #   bad_checkout = Date.new(1999, 1, 1)
-      #   expect {R}.must_raise ArgumentError
-      # end
+    end
+
+    it 'raises an argument error if check out date is before check in date' do
+
+      expect{Reservation.new(room, check_in, check_out2 )}.must_raise ArgumentError
 
     end
   end
