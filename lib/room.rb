181e2654793a1460c1017383f1e46f3ed7fe4bf7
Kat Perreira <kat.perreira@gmail.com>
2018-09-04 19:17:39 -0700

    removed instance variable status, removed method to check availability

--- lib/room.rb
+++ lib/room.rb
@@ -1,14 +1,10 @@
 
-require 'pry'
 
 class Room
   attr_reader :room_number
-  attr_reader :availability
 
-  def initialize(room_number, availability)
+  def initialize(room_number)
     @room_number = room_number
-    @availability = availability
-
     valid_rooms = (1..20).to_a
     if valid_rooms.include?(room_number)
       red_rum = false
@@ -16,21 +12,6 @@ class Room
       raise ArgumentError.new("Invalid room number")
     end
 
-    valid_status = [:available, :unavailable]
-    if valid_status.include?(availability)
-      bad_status = false
-    else
-      raise ArgumentError.new("Invalid room status")
-    end
-
   end
-
-  def is_available?
-    if @availability == :available
-      status = true
-    else
-      status = false
-    end
-    return status
-  end
+#
 end
