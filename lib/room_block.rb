6fcdae51dbc79b43065cd5a15aa3dba1dfb14180
Aurea <fu.sakura@gmail.com>
2018-09-05 15:51:23 -0700

    implement tests for available_rooms and total_cost, implement available_rooms method

--- lib/room_block.rb
+++ lib/room_block.rb
@@ -11,6 +11,9 @@ class RoomBlock
     unless rooms.is_a?(Array) && !rooms.empty?
       raise ArgumentError, "Room number is not valid number"
     end
+    unless rooms.length <= 5
+      raise ArgumentError, "Cannot have more than 5 rooms"
+    end
     unless rate.is_a?(Numeric) && rate > 0
       raise ArgumentError, "Rate is not valid number"
     end
@@ -20,6 +23,31 @@ class RoomBlock
     @end_date = end_date
     @rate = rate
     @reservations = []
+  end
+
+  def book_reservation(room, start_date, end_date)
+
+      unless valid_unbooked_room?(room)
+        raise ArgumentError, "This room cannot be booked within this block."
+      end
+
+      unless start_date == @start_date && end_date == @end_date
+        raise ArgumentError, "Date range does not match block"
+      end
+
+      reservation = Hotel::Reservation.new(room, start_date, end_date, @rate)
+      @reservations << reservation
+      return reservation
+  end
+
+  def valid_unbooked_room?(room)
+
+    reserved_room = @reservations.find{ |reservation| reservation.room == room }
+    return (reserved_room.nil? ? true : false) && @rooms.include?(room)
+  end
+
+  def available_rooms
+    return @rooms - @reservations.map{ |reservation| reservation.room }
   end
 
 
--- spec/room_block_spec.rb
+++ spec/room_block_spec.rb
@@ -1,28 +1,107 @@
 require_relative 'spec_helper'
 
 describe 'RoomBlock class' do
-describe 'initializing a roomblock' do
+  describe 'initializing a roomblock' do
 
-  it 'returns roomblock object given correct inputs' do
+    it 'returns roomblock object given correct inputs' do
 
-    expect( Hotel::RoomBlock.new([*1..5], Date.new(2018,7,4), Date.new(2018,7,8), 100)
-  ).must_be_instance_of(Hotel::RoomBlock)
-    expect( Hotel::RoomBlock.new([*1..5], Date.new(2018,6,4), Date.new(2018,7,4))
-  ).must_be_instance_of(Hotel::RoomBlock)
+      expect( Hotel::RoomBlock.new([*1..5], Date.new(2018,7,4), Date.new(2018,7,8), 100)
+    ).must_be_instance_of(Hotel::RoomBlock)
+      expect( Hotel::RoomBlock.new([*1..5], Date.new(2018,6,4), Date.new(2018,7,4))
+    ).must_be_instance_of(Hotel::RoomBlock)
+    end
+
+    it 'raise argument error given wrong input format' do
+
+      expect{
+        Hotel::RoomBlock.new(2, Date.new(2018,7,4), Date.new(2018,7,8), 100)
+      }.must_raise(ArgumentError)
+      expect{
+        Hotel::RoomBlock.new([2], 'stringdate', Date.new(2018,7,8), 100)
+      }.must_raise(ArgumentError)
+      expect{
+        Hotel::RoomBlock.new([2], Date.new(2018,7,4), Date.new(2018,7,8), -3)
+      }.must_raise(ArgumentError)
+    end
   end
 
-  it 'raise argument error given wrong input format' do
+  describe 'book_reservation' do
+    before do
+      @roomblock = Hotel::RoomBlock.new([*1..5], Date.new(2018,9,5), Date.new(2018,9,17), 100)
+    end
 
-    expect{
-      Hotel::RoomBlock.new(2, Date.new(2018,7,4), Date.new(2018,7,8), 100)
-    }.must_raise(ArgumentError)
-    expect{
-      Hotel::RoomBlock.new([2], 'stringdate', Date.new(2018,7,8), 100)
-    }.must_raise(ArgumentError)
-    expect{
-      Hotel::RoomBlock.new([2], Date.new(2018,7,4), Date.new(2018,7,8), -3)
-    }.must_raise(ArgumentError)
+    it 'raises argumenterror for invalid date range' do
+      expect{
+        @roomblock.book_reservation(5, Date.new(2018,9,5), Date.new(2018,9,16))
+      }.must_raise(ArgumentError)
+      expect{
+        @roomblock.book_reservation(5, Date.new(2018,9,6), Date.new(2018,9,17))
+      }.must_raise(ArgumentError)
+    end
+
+    it 'raises argumenterror for invalid room number' do
+      expect{
+        @roomblock.book_reservation(9, Date.new(2018,9,5), Date.new(2018,9,17))
+      }.must_raise(ArgumentError)
+
+      @roomblock.book_reservation(1, Date.new(2018,9,5), Date.new(2018,9,17))
+
+      expect{
+        @roomblock.book_reservation(1, Date.new(2018,9,5), Date.new(2018,9,17))
+      }.must_raise(ArgumentError)
+    end
+
+    it 'returns reservation object with correct room number and date range' do
+      reservation = @roomblock.book_reservation(1, Date.new(2018,9,5), Date.new(2018,9,17))
+
+      expect( reservation ).must_be_instance_of(Hotel::Reservation)
+      expect( reservation.room ).must_equal(1)
+      expect( reservation.start_date ).must_equal(Date.new(2018,9,5))
+      expect( reservation.end_date ).must_equal(Date.new(2018,9,17))
+    end
   end
-end
+
+  describe 'available_rooms' do
+    before do
+      @roomblock = Hotel::RoomBlock.new([*1..5], Date.new(2018,9,5), Date.new(2018,9,17), 100)
+    end
+
+    it 'returns empty array if no rooms are available' do
+      [*1..5].each do |i|
+        @roomblock.book_reservation(i, Date.new(2018,9,5), Date.new(2018,9,17))
+      end
+
+      expect( @roomblock.available_rooms ).must_equal([])
+    end
+
+    it 'correctly returns array of available rooms' do
+      @roomblock.book_reservation(2, Date.new(2018,9,5), Date.new(2018,9,17))
+      expect( @roomblock.available_rooms).must_equal([1, 3, 4, 5])
+
+      @roomblock.book_reservation(5, Date.new(2018,9,5), Date.new(2018,9,17))
+      expect( @roomblock.available_rooms).must_equal([1, 3, 4])
+    end
+  end
+
+  describe 'total_cost' do
+    before do
+      @roomblock = Hotel::RoomBlock.new([*1..5], Date.new(2018,9,5), Date.new(2018,9,17), 100)
+    end
+    it 'correctly calculates total cost of room block' do
+
+      [*1..4].each do |i|
+        @roomblock.book_reservation(i, Date.new(2018,9,5), Date.new(2018,9,17))
+      end
+      expect( @roomblock.total_cost ).must_equal(3500)
+
+      @roomblock.book_reservation(5, Date.new(2018,9,5), Date.new(2018,9,17))
+      expect( @roomblock.total_cost ).must_equal(4000)
+    end
+
+    it 'calculates total cost as 0 if no rooms have been reserved yet' do
+      expect( @roomblock.total_cost ).must_equal(0)
+    end
+
+  end
 
 end
