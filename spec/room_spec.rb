fed7a2ee577bfef1c62b76dbf86ed631a95fb726
Kat Perreira <kat.perreira@gmail.com>
2018-09-05 13:23:57 -0700

    Refactored to use let in my test

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,20 +1,36 @@
 
 require_relative 'spec_helper.rb'
 
-describe "#initialize" do
-  # Test to make sure it takes correct parameters
-  it 'Takes in a room number' do
-    room_number = 5
-    test_room = Room.new(room_number)
+describe 'Room' do
+  let (:room_number1) {
+    1
+  }
 
-    expect(test_room).must_respond_to :room_number
-    expect(test_room.room_number).must_equal room_number
+  let (:room_number2) {
+    2
+  }
 
+  let (:room_number3) {
+    3
+  }
+
+  let (:red_rum) {
+    237
+  }
+
+  let (:standard_room) {
+    Room.new(room_number1)
+  }
+
+  describe "#initialize" do
+    # Test to make sure it takes correct parameters
+    it 'Takes in a room number' do
+      expect(standard_room).must_respond_to :room_number
+      expect(standard_room.room_number).must_equal room_number1
+    end
+    # Test to make sure that rooms checked are only 1-20
+    it 'Raise argument error for invalid room number' do
+      expect{Room.new(red_rum)}.must_raise ArgumentError
+    end
   end
-
-  # Test to make sure that rooms checked are only 1-20
-  it 'Raise argument error for invalid room number' do
-    expect{Room.new(237)}.must_raise ArgumentError
-  end
-
 end
