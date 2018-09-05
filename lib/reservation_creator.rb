b15f56606f3982484e726f3c1ad861eb1ce5fe1c
Karis Kim <ouniiq@hotmail.com>
2018-09-05 16:05:24 -0700

    created count_number_of_nights method to calculate number of nights user wants to book

--- lib/reservation_creator.rb
+++ lib/reservation_creator.rb
@@ -1,27 +1,19 @@
 require 'date'
+require_relative 'rooms'
 
 module Hotel
   class Reservation
-    attr_reader :room_number, :check_in_date, :check_out_date
+    attr_reader :check_in_date, :check_out_date
 
-    def initialize(room_number = 1, check_in_date, check_out_date)
-      @room_number = room_number.to_i
-        raise ArgumentError, "Invalid room number, only 1 to 20 allowed" if room_number > 20
+    def initialize(check_in_date, check_out_date)
       @check_in_date = Date.parse("#{check_in_date}")
       @check_out_date = Date.parse("#{check_out_date}")
-        raise ArgumentError, "Checkout date cannot be before checkin date" unless check_in_date < check_out_date
+        raise StandardError, "Checkout date cannot be before checkin date" unless check_in_date < check_out_date
     end
 
-    # def rooms_in_hotel
-    #   rooms = [*"Room 1".."Room 20"]
-    # end
-    #
-    # def show_all_rooms
-    #   rooms_in_hotel.each_with_index { |room, number| return "#{number + 1}. #{room}"}
-    # end
-    #
-    # def choose_room_by_date(chosen_room)
-    #   rooms.select {|room| return room if chosen_room == room }
-    # end
+    def count_number_of_nights
+      num_of_nights = check_out_date.mjd - check_in_date.mjd
+      return num_of_nights
+    end
   end
 end
