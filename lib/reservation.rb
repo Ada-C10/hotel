0a97304f77a1c4699f5916f38482ca5fa7dae579
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-05 14:00:24 -0700

    Add parameters and instance variables to Reservation#intialize and attr_reader for all with corresponding tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,5 +1,18 @@
 class Reservation
-  def initialize
-    
+  attr_reader :check_in, :number_of_nights, :cost
+  PRICE = 200
+  def initialize(check_in, number_of_nights)
+    @check_in = Date.parse(check_in.to_s)
+    @number_of_nights = number_of_nights
+    @cost = PRICE * number_of_nights
   end
+  # def get_all_dates
+  #   all_dates = []
+  #   date = check_in
+  #   number_of_nights.times do
+  #     all_dates << date.next
+  #     date = date.next
+  #   end
+  #   return all_dates
+  # end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,10 +1,34 @@
 require_relative 'spec_helper'
 
-describe "Reservation" do
+describe Reservation do
   describe "#initialize" do
     it "can be instantiated" do
-      @reservation = Reservation.new
+      @reservation = Reservation.new(181202, 2)
       expect(@reservation).must_be_kind_of Reservation
     end
+    it "takes check_in and number_of_nights" do
+      @reservation = Reservation.new(181202, 2)
+      expect(@reservation).must_respond_to :check_in
+      expect(@reservation).must_respond_to :number_of_nights
+    end
+    it "converts check_in to Date object" do
+      @reservation = Reservation.new(181202, 2)
+      expect(@reservation.check_in).must_be_kind_of Date
+      # expect(@reservation.check_in).must_equal 2018-12-02
+    end
+    it "calculates and stores cost" do
+      @reservation = Reservation.new(181202, 2)
+      expect(@reservation.cost).must_equal 400
+      expect(@reservation).must_respond_to :cost
+    end
   end
+  # describe "#get_all_dates" do
+  #   it "returns Array" do
+  #     expect(@reservation.get_all_dates).must_be_kind_of Array
+  #   end
+  #   it "contains all dates for reservation" do
+  #     expect(@reservation.get_all_dates).must_equal
+  #   end
+  # end
+  # end
 end
