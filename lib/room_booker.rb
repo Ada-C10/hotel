d388c3ec6cf0235b218ed18d30d8c47868e91267
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-05 15:15:48 -0700

    Fix typo in RoomBooker#find_room_by_id, implement RoomBooker#list_reservations, and implement RoomBooker#reservation_cost plus helper method RoomBooker#find_reservation

--- lib/room_booker.rb
+++ lib/room_booker.rb
@@ -1,6 +1,8 @@
 require 'date'
 require_relative 'reservation'
 
+require 'pry'
+
 module BookingLogic
   class RoomBooker
     attr_reader :rooms
@@ -30,12 +32,39 @@ module BookingLogic
     end
 
     def find_room_by_id(room_id)
-      return rooms.find { |room| room.id = room_id }
+      return rooms.find { |room| room.id == room_id }
     end
 
     def new_reservation(room_id, check_in, check_out)
       room = find_room_by_id(room_id)
       reservations << BookingLogic::Reservation.new(room, check_in, check_out)
     end
+
+    def list_reservations(date)
+      list_of_reservations = []
+
+      reservations.each do |reservation|
+        if (reservation.check_in..reservation.check_out).cover?(date)
+          list_of_reservations << reservation
+        end
+      end
+
+      return list_of_reservations
+    end
+
+    def find_reservation(room_id, check_in)
+      reservations.each do |reservation|
+        if reservation.room.id == room_id && reservation.check_in == check_in
+          return reservation
+        end
+      end
+    end
+
+    def reservation_cost(room_id, check_in)
+      reservation = find_reservation(room_id, check_in)
+      days = reservation.check_out - reservation.check_in
+      return days.to_i * reservation.room.cost
+    end
+
   end
 end
