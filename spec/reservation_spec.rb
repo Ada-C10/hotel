834953a3a18bf912835dac80edca0f413d1bc633
Lindsay Terchin <lterchin@gmail.com>
2018-09-05 14:29:07 -0700

    created tests to validate reservation initialization & to make sure that the dates are the correct class

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -9,8 +9,17 @@ describe "Reservation class" do
       expect(@reservation).must_be_kind_of Hotel::Reservation
     end
 
-
+    it "must throw an argument error if the end date is before the start date" do
+      expect {Hotel::Reservation.new(2015, 03, 05, 2015, 03, 01)}.must_raise ArgumentError
+    end
   end
 
+  describe "Generate date" do
+    it "is a kind of date" do
+
+      date = Date.new(2018,1,3)
+      expect(date).must_be_kind_of Date
+    end
+  end
 
 end
