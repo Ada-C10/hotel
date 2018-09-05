293cd42968ebf9ef1815cad696fa7eb04b48ae69
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-05 14:22:01 -0700

    Add Calendar#initialize and Calendar#add_reservation methods and corresponding tests

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,4 +1,10 @@
 class Calendar
+  attr_reader :reservations
+  ROOMS = [*1..20]
   def initialize
+    @reservations = []
+  end
+  def add_reservation(reservation)
+    @reservations << reservation
   end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -6,5 +6,17 @@ describe Calendar do
       calendar = Calendar.new
       expect(calendar).must_be_kind_of Calendar
     end
+    it "stores rooms constant" do
+      expect(Calendar::ROOMS).must_be_kind_of Array
+      expect(Calendar::ROOMS[0]).must_equal 1
+    end
+  end
+  describe "#add_reservation" do
+    it "increases length of @reservations" do
+      calendar = Calendar.new
+      reservation = Reservation.new(181202, 2)
+      calendar.add_reservation(reservation)
+      expect(calendar.reservations.length).must_equal 1
+    end
   end
 end
