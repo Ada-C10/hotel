bcf68e85e487bf02e2c54d050ac4f1e7fe47b5c9
Anibel America <ameriabel@gmail.com>
2018-09-05 15:58:49 -0700

    modifies room initialization to take in an array of room reservations

--- lib/room.rb
+++ lib/room.rb
@@ -3,13 +3,14 @@ module Hotel
 
     attr_reader :room_num, :room_reservations
 
-    def initialize(room_num)
+    def initialize(room_num, room_reservations: [])
+
       unless [*1..20].include?(room_num)
         raise ArgumentError, "Only room numbers from 1 - 20"
       end
 
       @room_num = room_num
-      @room_reservations = []
+      @room_reservations = room_reservations
     end
   end
 
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,6 +2,18 @@ require_relative 'spec_helper'
 
 describe 'Room class' do
   describe 'Room instantiation' do
+    let (:reservations) {
+      check_in = Date.today + 1
+      check_out = check_in + 3
+      reservation_1 = Hotel::Reservation.new(check_in, check_out, 1)
+
+      check_in = Date.today + 2
+      check_out = check_in + 5
+      reservation_2 = Hotel::Reservation.new(check_in, check_out, 2)
+
+      [reservation_1, reservation_2]
+    }
+
     it 'creates an instance of room' do
       expect(Hotel::Room.new(1)).must_be_instance_of Hotel::Room
     end
@@ -9,5 +21,12 @@ describe 'Room class' do
     it 'raises an ArgumentError if given an invalid room number' do
       expect{Hotel::Room.new(21)}.must_raise ArgumentError
     end
+
+    it 'creates an instance of Room class with reservations as input' do
+      room = Hotel::Room.new(1, room_reservations: reservations)
+      expect(room).must_be_instance_of Hotel::Room
+      expect(room.room_reservations).must_equal reservations
+    end
   end
+  
 end
