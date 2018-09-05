bb534b33a8e790cd8a231e8f745c48580ebf690f
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-05 15:51:20 -0700

    Created method to return the number of days in a date range

--- spec/daterange_spec.rb
+++ spec/daterange_spec.rb
@@ -2,10 +2,12 @@ require_relative 'spec_helper'
 require 'date'
 
 describe 'DateRange' do
+  before do
+    @date_range = DateRange.new(Date.new(2018, 9, 1), Date.new(2018, 9, 2))
+  end
 
   it "Can be initalized" do
-    date_range = DateRange.new(Date.new(2018, 9, 1), Date.new(2018, 9, 2))
-    expect(date_range).kind_of? Date
+    expect(@date_range).kind_of? Date
   end
 
   it "Raises an ArgumentError if check_in is not a date" do
@@ -19,12 +21,11 @@ describe 'DateRange' do
   it "Returns true if a date is within a date range" do
     # Given a date
     # Returns true if date is within check_in and check_out
-    date_range = DateRange.new(Date.new(2018, 9, 1), Date.new(2018, 9, 2))
-    expect(date_range.within_range?(Date.new(2018, 9, 2))).must_equal TRUE
+    expect(@date_range.within_range?(Date.new(2018, 9, 2))).must_equal TRUE
   end
 
   it "Returns the number of days" do
-
+    expect(@date_range.number_of_days).must_equal 1
   end
 
 end
