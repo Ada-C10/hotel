172d7e760eb59ea5ed773fc4bf00fff4345c0e0a
Addison Cole <addison.cole85@gmail.com>
2018-09-04 17:25:05 -0700

    add Reservation class, add test specs for Reservation class initialization

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,1 +1,42 @@
 require_relative 'spec_helper'
+
+describe 'reservation instantiation' do
+  before do
+    input = { name: "Vlad the Impaler",
+      room_number:  9,
+      check_in_date: Date.new(2020,9,9),
+      check_out_date: Date.new(2020,9,13),
+      nights_of_stay: [Date.new(2020,9,9), Date.new(2020,9,10), Date.new(2020,9,11), Date.new(2020,9,12)]
+    }
+    @reservation = Reservation.new(input)
+  end
+  #
+  it 'creates an instance of a Reservation' do
+    expect(@reservation).must_be_kind_of Reservation
+  end
+
+  it 'has a customer name' do
+    expect(@reservation.name).must_equal "Vlad the Impaler"
+  end
+
+  it 'has has a room number ' do
+    expect(@reservation.room_number).must equal 9
+  end
+  #
+  it 'has a check-in date' do
+    expect(@reservation.check_in_date).must_be_kind_of Date
+  end
+
+  it 'has a check-out date' do
+    expect(@reservation.check_out_date).must_be_kind_of Date
+  end
+
+  it 'stores an array of all nights in a reservation' do
+    expect(@reservation.nights_of_stay).must_be_kind_of Array
+
+    @reservation.nights_of_stay.each do |night|
+      expect(night).must_be_kind_of Date
+    end
+  end
+
+end
