a53d8fd933712fda0e79cd5a2dcca266bca8b30f
Kat Perreira <kat.perreira@gmail.com>
2018-09-04 19:18:26 -0700

    Deleted tests for status and for is_availiable? method

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -3,36 +3,18 @@ require_relative 'spec_helper.rb'
 
 describe "#initialize" do
   # Test to make sure it takes correct parameters
-  it 'Takes in a room number and availability status' do
+  it 'Takes in a room number' do
     room_number = 5
-    availability = :available
-    test_room = Room.new(room_number, availability)
+    test_room = Room.new(room_number)
 
     expect(test_room).must_respond_to :room_number
     expect(test_room.room_number).must_equal room_number
 
-    expect(test_room).must_respond_to :availability
-    expect(test_room.availability).must_equal availability
   end
 
   # Test to make sure that rooms checked are only 1-20
   it 'Raise argument error for invalid room number' do
-    expect{Room.new(237, :available)}.must_raise ArgumentError
+    expect{Room.new(237)}.must_raise ArgumentError
   end
 
-  # Test to make sure status is valid
-  it 'Raise argument error for invalid status' do
-    expect{Room.new(2, :haunted)}.must_raise ArgumentError
-  end
-end
-
-describe "#is_available?" do
-  # test to make sure it returns true if status is avaliable
-  it 'Returns true if status is set to available' do
-    room_number = 5
-    status = :available
-    test_room = Room.new(room_number, status)
-    expected = true
-    expect(test_room.is_available?).must_equal expected
-  end
 end
