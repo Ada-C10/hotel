0e405f9af37a5f62e997d02106729a0480e274e2
Katricia Smith <smithkatricia@gmail.com>
2018-09-04 17:04:24 -0700

    Deleted require relative to booking_manager in room.rb since booking_manager already requires room

--- lib/room.rb
+++ lib/room.rb
@@ -1,20 +1,18 @@
-require_relative 'booking_manager'
+# require_relative 'booking_manager'
 
 module Hotel
   class Room
     attr_reader :number
-    attr_accessor :availability
 
     def initialize(number)
-      @number = number.to_i
-      @availability = check_availability
+      @number = number # should this be a string ?
     end
 
     def check_availability(start_date, end_date)
       # Do..something with dates.
       # if date of check is within range
       # return boolean
-      availability = true 
+      availability = true
       return availability
     end # of check_availability
 
