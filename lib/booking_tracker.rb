27392ac71b4a52045fd42b7ad4b6672a639d3a3a
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-04 23:46:20 -0700

    Add date_format helper method to booking_tracker.rb

--- lib/booking_tracker.rb
+++ lib/booking_tracker.rb
@@ -13,16 +13,20 @@ class BookingTracker
 
   def new_reservation(begin_date, end_date)
     id = @all_reservations == nil ? 1 : @all_reservations.length + 1
-    begin_date = Date.strptime(begin_date, '%m-%d-%Y')
-    end_date = Date.strptime(end_date, '%m-%d-%Y')
+    begin_date = date_format(begin_date)
+    end_date = date_format(end_date)
     reservation = Reservation.new(id, begin_date, end_date, @cost)
     @all_reservations << reservation
     return reservation
   end
 
   def reservation_list_by_date(date)
-    date = Date.strptime(date, '%m-%d-%Y')
+    date = date_format(date)
     reservation_list_by_date = @all_reservations.find_all { |reservation| date >= reservation.begin_date && date <= reservation.end_date }
     return reservation_list_by_date
+  end
+
+  def date_format(date)
+    return Date.strptime(date, '%m-%d-%Y')
   end
 end
