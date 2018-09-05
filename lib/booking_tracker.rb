04bc6c3301b67b3889700751c08ee91161f4bb64
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-05 15:50:05 -0700

    Complete open_room and find_available_room methods in booking_tracker.rb

--- lib/booking_tracker.rb
+++ lib/booking_tracker.rb
@@ -12,7 +12,6 @@ class BookingTracker
   end
 
   def new_reservation(begin_date, end_date)
-
     begin_date = date_format(begin_date)
     end_date = date_format(end_date)
 
@@ -21,29 +20,50 @@ class BookingTracker
     room_num = find_available_room(begin_date, end_date)
 
     reservation = Reservation.new(begin_date, end_date, room_num, @cost)
-
     @all_reservations << reservation
-
     return reservation
   end
 
 
   def reservation_list_by_date(date)
     date = date_format(date)
+
     reservation_list_by_date = @all_reservations.find_all { |reservation| date >= reservation.begin_date && date <= reservation.end_date }
     return reservation_list_by_date
   end
 
 
-  def date_format(date)
-    return Date.strptime(date, '%m-%d-%Y')
-  end
-
-
   def find_available_room(begin_date, end_date)
-    available_room = 5
-    return available_room
+    beg_dt = date_format(begin_date)
+    end_dt = date_format(end_date)
+    return open_rooms(beg_dt, end_dt).first
   end
 
+
+  def open_rooms(begin_date, end_date)
+    available_rooms = []
+    used_rooms = []
+    beg_dt = date_format(begin_date)
+    end_dt = date_format(end_date)
+
+    (beg_dt..end_dt).each do |date|
+      reservation_list_by_date(date).each do |reservation|
+        used_rooms << reservation.room_num
+      end
+    end
+
+    used_rooms.uniq!
+    available_rooms = @rooms - used_rooms
+    return available_rooms
+  end
+
+
+  def date_format(date)
+    if date.class == Date
+      return date
+    else
+      return Date.strptime(date, '%m-%d-%Y')
+    end
+  end
 
 end
--- spec/booking_tracker_spec.rb
+++ spec/booking_tracker_spec.rb
@@ -2,18 +2,20 @@ require_relative 'spec_helper'
 require 'pry'
 
 describe "BookingTracker class" do
+
   before do
     @bookings = BookingTracker.new
-    # before do
+
     @bookings.new_reservation('9-10-2018', '10-10-2018')
     @bookings.new_reservation('9-5-2018', '9-16-2018')
     @bookings.new_reservation('9-15-2018', '9-18-2018')
-    # end
   end
+
+
 
   describe "initialization" do
 
-    it "it can access the list of all of the rooms in the hotel" do
+    it "saves list of all of the rooms in the hotel" do
       rooms = @bookings.rooms
 
       expect(rooms).must_be_kind_of Array
@@ -27,28 +29,36 @@ describe "BookingTracker class" do
     end
   end
 
+
+
   describe "it can reserve a room for a given date range" do
+
     before do
       @reservation = @bookings.new_reservation('9-15-2018', '9-17-2018')
     end
 
     it "must raise ArgumentError for invalid dates" do
-
       expect { @bookings.new_reservation('10-10-2018', '9-15-2018') }.must_raise ArgumentError
     end
 
     it "must create a new reservation instance" do
-
     expect(@reservation).must_be_kind_of Reservation
     end
 
     it "can find an available_room for the reservation" do
+      expect (@reservation.room_num).must_equal 4
+    end
 
-      expect (@reservation.room_num).must_equal 5
+    it "can get the total cost for a given reservation" do
+      reservation = @bookings.new_reservation('9-1-2018', '9-5-2018')
+
+      expect (reservation.cost).must_equal 800
     end
   end
 
-  describe "track list of reservations" do
+
+
+  describe "track lists of reservations" do
 
     it "can return a list of all reservations" do
       reservation_list = @bookings.all_reservations
@@ -63,15 +73,23 @@ describe "BookingTracker class" do
 
       expect (reservation_list_by_date).must_be_kind_of Array
       expect (reservation_list_by_date.length).must_equal 2
-
     end
 
-    it "can get the total cost for a given reservation" do
-      reservation = @bookings.new_reservation('9-1-18', '9-5-18')
-
-      expect (reservation.cost).must_equal 800
-
-    end
 
   end
+
+
+
+  describe "track lists of rooms" do
+
+    it "can access a list of rooms that are not reserved for a given date range" do
+      open_rooms = @bookings.open_rooms('9-5-2018', '9-8-2018')
+
+      expect (open_rooms).must_be_kind_of Array
+      expect (open_rooms.length).must_equal 19
+      expect (open_rooms.first).must_equal 1
+    end
+  end
+
+
 end
