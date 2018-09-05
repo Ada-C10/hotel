c1a1677f266ef5bbc63ac41c3dc7e31523e46762
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-05 12:04:44 -0700

    Refactored and created helper method format_date

--- lib/block_room.rb
+++ lib/block_room.rb
@@ -6,19 +6,9 @@ class BlockRoom
   def initialize(event_name, start_date, end_date)
     @event_name = event_name
 
-    # start date must be entered in the format: yyyy,mm,dd
-    parsed_start_date = start_date.split(",")
-    start_year = parsed_start_date[0].to_i
-    start_month = parsed_start_date[1].to_i
-    start_day = parsed_start_date[2].to_i
-    @starting_date = Date.new(start_year, start_month, start_day)
-
-    # end date must be entered in the format: yyyy,mm,dd
-    parsed_end_date = end_date.split(",")
-    end_year = parsed_end_date[0].to_i
-    end_month = parsed_end_date[1].to_i
-    end_day = parsed_end_date[2].to_i
-    @ending_date = Date.new(end_year, end_month, end_day)
+    # date must be entered in the format: yyyy,mm,dd
+    @starting_date = format_date(start_date)
+    @ending_date = format_date(end_date)
 
     @rooms_blocked = []
   end
@@ -26,4 +16,13 @@ class BlockRoom
   # def add_room_to_block(room_num)
   # end
 
+  def format_date(date_string)
+    parsed_date = date_string.split(",")
+    year = parsed_date[0].to_i
+    month = parsed_date[1].to_i
+    day = parsed_date[2].to_i
+    formatted_date = Date.new(year, month, day)
+
+    return formatted_date
+  end
 end
