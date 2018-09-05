bf5738f5761b6acd51eb53d7f5c20dfd69efc019
Lindsay Terchin <lterchin@gmail.com>
2018-09-05 14:13:34 -0700

    created helper method to generate start/end dates

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,37 +1,24 @@
 require 'date'
+require 'pry'
 
 module Hotel
   class Reservation
     attr_reader :start_date, :end_date
-    attr_accessor :reservation_id
-
-    
-    @@total_reservations = 0
-    ROOMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
-    def initialize(start_date, end_date)
+    def initialize(start_year, start_month, start_day, end_year, end_month, end_day)
 
-      @reservation_id = create_id
-      @start_date = Date.new(start_date)
-      @end_date = Date.new(end_date)
+      @start_date = generate_date(start_year, start_month, start_day)
+      @end_date = generate_date(end_year, end_month, end_day)
 
       validate_dates
 
     end
 
 
-
-    #create a new reservation id by adding 1 to total reservations
-    def create_id
-      new_id = @@total_reservations + 1
-    end
-
-
-
     #make sure there are valid start/end dates
     def validate_dates
-      raise ArgumentError.new("The end date must be after the start date") if @end_date >= @start_date
+      raise ArgumentError.new("The end date must be after the start date") if @end_date <= @start_date
 
       raise ArgumentError.new("The start date can't be nil") if @start_date == nil
 
@@ -39,6 +26,10 @@ module Hotel
     end
 
 
+
+    def generate_date(year, month, day)
+      return Date.new(year, month, day)
+    end
 
 
   end
