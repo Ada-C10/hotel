c75008617b0a99929a4102fc0528cbaac3e37f52
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-05 16:04:17 -0700

    Remove classes not planning to use

--- lib/reservation_tracker.rb
+++ lib/reservation_tracker.rb
@@ -1,8 +1,12 @@
+require 'date'
+
 require_relative 'reservation'
+require_relative 'room'
+
 module Hotel
 
   class ReservationTracker
-    attr_reader :reservations
+    attr_reader :reservations, :rooms, :date_range
 
     def initialize(reservations)
       @reservations = load_reservations
@@ -18,6 +22,10 @@ module Hotel
         reservation.date_range.include?(date)
       end
       return reservations_by_date
+    end
+
+    def reserve_room(start_date, end_date)
+      #write code to reserve room by date
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative '../lib/room'
 require_relative '../lib/reservation'
-require_relative '../lib/reservation_agent'
 require_relative '../lib/reservation_tracker'
-require_relative '../lib/reservation_maker'
