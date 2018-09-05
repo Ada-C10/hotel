58fa8040af0f1c40d3060e7b006ea97a8beacb29
Katricia Smith <smithkatricia@gmail.com>
2018-09-05 14:18:13 -0700

    Commented out Room#check_availability

--- lib/room.rb
+++ lib/room.rb
@@ -8,13 +8,13 @@ module Hotel
       @number = number # should this be a string ?
     end
 
-    def check_availability(start_date, end_date)
-      # Do..something with dates.
-      # if date of check is within range
-      # return boolean
-      availability = true
-      return availability
-    end # of check_availability
+    # def check_availability(start_date, end_date)
+    #   # Do..something with dates.
+    #   # if date of check is within range
+    #   # return boolean
+    #   availability = true
+    #   return availability
+    # end # of check_availability
 
   end # of class Room
 end # of module Hotel
