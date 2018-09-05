1470dfc60d2e83182315f0f76f21dabca637fcb2
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-05 15:46:15 -0700

    Modify code to take DateRange

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,31 +1,21 @@
+require_relative 'date_range'
+
 RATE_PER_NIGHT = 200
 
 module Hotel
   class Reservation
-    attr_reader :id, :room, :start_date, :end_date, :cost
+    attr_reader :id, :room, :cost, :date_range
+
 
     def initialize(input)
       @id = input[:id]
       @room = input[:room]
-      @start_date = input[:start_date]
-      @end_date = input[:end_date]
+      @date_range = Hotel::DateRange.new(input[:start_date], input[:end_date])
       @cost = calculate_cost
-
-      raise ArgumentError.new("Start date must be before end date") if @end_date < @start_date
-    end
-
-    def date_range
-      date_range = (@start_date...@end_date)
-      return date_range
-    end
-
-    def reservation_period
-      reservation_period = @end_date - @start_date
-      return reservation_period
     end
 
     def calculate_cost
-      cost = RATE_PER_NIGHT * reservation_period
+      cost = RATE_PER_NIGHT * @date_range.get_reservation_period
     end
   end
 end
