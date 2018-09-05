415878c447e7670d663f249683085a62a589407d
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-05 15:30:00 -0700

    added test for calculates total cost of stay correctly

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,13 +1,23 @@
+require 'pry'
+
 require_relative 'spec_helper'
 
 describe "Reservation Class" do
+  let (:duration) { Hotel::Duration.new('2018-09-01', '2018-09-05') }
+  let (:reservation) { Hotel::Reservation.new(1, duration, 20) }
+
   describe "initailize" do
-    let (:duration) { Hotel::Duration.new('2018-09-01', '2018-09-05') }
-    let (:reservation) { Hotel::Reservation.new(1, duration, 20) }
-
-    it 'stores duration as an object' do
-      expect(reservation.duration).must_be_instance_of Hotel::Duration
-
+    it 'Stores duration as an object' do
+      expect(reservation.duration).must_be_kind_of Hotel::Duration
     end
+
+  describe "total_cost" do
+
+    it 'Calculates total cost of an entire stay' do
+      expect(reservation.total_cost).must_equal 800.00
+    end
+
+    it 
+  end
   end
 end
