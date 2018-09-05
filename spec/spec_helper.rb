d917169d36372f8d917eb3d438e8133384871555
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-05 14:21:08 -0700

    Add require_relative for all lib files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
+# Add simplecov
 require 'simplecov'
 SimpleCov.start
 
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
+
+require_relative '../lib/room_booker'
+require_relative '../lib/reservation.rb'
+require_relative '../lib/room_block.rb'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
