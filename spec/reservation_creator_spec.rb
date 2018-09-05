00ef5ed806cfe458bbd67f071bada6c2dc0de902
Karis Kim <ouniiq@hotmail.com>
2018-09-05 16:05:48 -0700

    created test for count_number_of_nights method and it passed

--- spec/reservation_creator_spec.rb
+++ spec/reservation_creator_spec.rb
@@ -6,17 +6,24 @@ describe "BookingSystem class" do
   # end
 
   it "rooms in hotel method" do
-    list = Hotel::Reservation.new(2, 180904, 180905)
+    list = Hotel::Reservation.new(180904, 180905)
 
     expect(list).must_be_kind_of Hotel::Reservation
-    expect(list.room_number).must_be_kind_of Integer
-    expect(list.room_number).must_equal 2
+    # expect(list.room_number).must_be_kind_of Integer
+    # expect(list.room_number).must_equal 2
     expect(list.check_in_date).must_equal Date.parse("2018-09-04")
     expect(list.check_out_date).must_equal Date.parse("2018-09-05")
   end
 
   it "raises ArgumentError if check out date is before check in date" do
-
-    expect{(list = Hotel::Reservation.new(2, 180904, 180903))}.must_raise ArgumentError
+    expect{(list = Hotel::Reservation.new(180904, 180903))}.must_raise StandardError
   end
+
+  it "calculates the number of nights the user wants to book" do
+    list = Hotel::Reservation.new(180904, 180905)
+    expect(list.count_number_of_nights).must_equal 1
+  end
+
+
+
 end
