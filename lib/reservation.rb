9f1a580c0755b3bec971e027c15b38a98bd03914
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-05 13:45:35 -0700

    Refactor init method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,9 +1,11 @@
+require_relative 'room_booker'
+
 module BookingLogic
   class Reservation
     attr_reader :room_id, :check_in, :check_out
 
-    def initialize(room_id, check_in, check_out)
-      @room = find_room_by_id(room_id)
+    def initialize(room, check_in, check_out)
+      @room = room
       @check_in = check_in
       @check_out = check_out
     end
