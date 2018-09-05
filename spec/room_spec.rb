b4b9624a27a14dbc1648f1c98fc490d2d407075d
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-05 13:56:49 -0700

    Remove tests for status attribute

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -5,7 +5,6 @@ describe "Room class" do
   before do
     @room = Hotel::Room.new(
       room_number: 1,
-      status: :AVAILABLE
     )
 
     @reservation_1 = Hotel::Reservation.new(
@@ -35,27 +34,17 @@ describe "Room class" do
       }.must_raise ArgumentError
     end
 
-    it "throws an argument error with an invalid status" do
-      expect{
-        Hotel::Room.new(
-          room_number: 1,
-          status:'available'
-        )
-      }.must_raise ArgumentError
-    end
-
     it "sets booked_reservations to an empty array if not provided" do
       expect(@room.booked_reservations).must_be_kind_of Array
       expect(@room.booked_reservations.length).must_equal 0
     end
 
     it "is set up for specific attributes and data types" do
-      [:room_number, :status].each do |prop|
+      [:room_number].each do |prop|
         expect(@room).must_respond_to prop
       end
 
       expect(@room.room_number).must_be_kind_of Integer
-      expect(@room.status).must_be_kind_of Symbol
     end
   end
 
