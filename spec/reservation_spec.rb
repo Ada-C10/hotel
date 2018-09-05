8bcd0f455741410025bcaa863fe3bace065701fc
Kat Perreira <kat.perreira@gmail.com>
2018-09-05 13:48:24 -0700

    Included test for stay_duration

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -33,17 +33,10 @@ describe 'Reservation' do
 
     end
 
-    it 'Raises an argument error if check out date is before check in date' do
+    it 'Raises a standard error if check out date is before check in date' do
 
-      expect{Reservation.new(room, check_in, throwback_date )}.must_raise ArgumentError
+      expect{Reservation.new(room, check_in, throwback_date )}.must_raise StandardError
 
-    end
-  end
-
-  describe "#total_charge" do
-    it 'Charges 200.00 a night for a reservation' do
-      # general reservation is for 7 days, 6 nights, 6 * 200 = 1200
-      expect(general_reservation.total_charge).must_equal 1200
     end
   end
 
@@ -51,8 +44,15 @@ describe 'Reservation' do
     it 'Calculates the amount of time that the reservation is active' do
       #general reservation for 6 nights
       expect (general_reservation.stay_duration).must_equal 6
-
     end
-
   end
+
+  describe "#total_charge" do
+    it 'Charges 200.00 a night for a reservation' do
+      # general reservation is for 7 days, 6 nights, 6 * 200 = 1200
+      expect(general_reservation.total_charge(6)).must_equal 1200
+    end
+  end
+
+
 end
