de65f4c3c7a8f4301438ec3361817796d88d4ef3
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-05 12:00:14 -0700

    Refactored and created a helper method format_date

--- lib/room.rb
+++ lib/room.rb
@@ -12,16 +12,23 @@ class Room
   # date must be entered in the format: yyyy,mm,dd
   def is_available?(date)
 
-    parsed_date = date.split(",")
-    year = parsed_date[0].to_i
-    month = parsed_date[1].to_i
-    day = parsed_date[2].to_i
-    formated_date = Date.new(year,month,day)
+    formated_date = format_date(date)
 
     if @dates_booked.include?(formated_date)
       return false
     else
       return true
     end
+  end
+
+  def format_date(date_string)
+    
+    parsed_date = date_string.split(",")
+    year = parsed_date[0].to_i
+    month = parsed_date[1].to_i
+    day = parsed_date[2].to_i
+    formated_date = Date.new(year,month,day)
+
+    return formated_date
   end
 end
