e4184c91b226cb6ca520daa1987ed43247c6faef
Anibel America <ameriabel@gmail.com>
2018-09-04 18:41:06 -0700

    adds tests for list_all_rooms method and starter code

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -9,6 +9,11 @@ module Hotel
       @all_reservations = all_reservations
     end
 
+    def list_all_rooms
+      list_of_rooms = @all_rooms.each_with_index.map { |room_num, order| "Room #{order + 1}: #{room_num}" }.join("\n")
+      return list_of_rooms
+    end
+
   end
 
 end
--- spec/reservation_manager_spec.rb
+++ spec/reservation_manager_spec.rb
@@ -15,7 +15,16 @@ describe 'ReservationManager class' do
   describe 'ReservationManager instantiation' do
     it 'creates an instance of ReservationManager class' do
       expect(reservation_manager).must_be_instance_of Hotel::ReservationManager
+    end
+
+    it 'creates an instance of ReservationManager class with reservations as input'do
       expect(Hotel::ReservationManager.new(all_reservations: reservation)).must_be_instance_of Hotel::ReservationManager
+    end
+  end
+
+  describe 'list_all_rooms function' do
+    it 'returns a formatted string of all the rooms' do
+      expect(reservation_manager.list_all_rooms).must_be_instance_of String
     end
   end
 end
