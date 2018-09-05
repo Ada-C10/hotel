5bf6222416586f6c4c57e0567f589e1a384239cb
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-05 12:27:45 -0700

    Add initialize/attr_reader methods to Reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,4 +1,11 @@
 module BookingLogic
   class Reservation
+    attr_reader :room_id, :check_in, :check_out
+
+    def initialize(room_id, check_in, check_out)
+      @room = find_room_by_id(room_id)
+      @check_in = check_in
+      @check_out = check_out
+    end
   end
 end
