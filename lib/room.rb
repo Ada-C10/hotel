3ed087f4ba74f576e77c11235dcb12626db2361c
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-05 12:31:37 -0700

    Disabled helper method format_date since is_available? receives date argument pre-formatted as a Date

--- lib/room.rb
+++ lib/room.rb
@@ -9,26 +9,25 @@ class Room
     @dates_booked = []
   end
 
-  # date must be entered in the format: yyyy,mm,dd
-  def is_available?(date)
+  def is_available?(formatted_date)
 
-    formated_date = format_date(date)
+    # formated_date = format_date(date)
 
-    if @dates_booked.include?(formated_date)
+    if @dates_booked.include?(formatted_date)
       return false
     else
       return true
     end
   end
 
-  def format_date(date_string)
-    
-    parsed_date = date_string.split(",")
-    year = parsed_date[0].to_i
-    month = parsed_date[1].to_i
-    day = parsed_date[2].to_i
-    formated_date = Date.new(year,month,day)
-
-    return formated_date
-  end
+  # def format_date(date_string)
+  #
+  #   parsed_date = date_string.split(",")
+  #   year = parsed_date[0].to_i
+  #   month = parsed_date[1].to_i
+  #   day = parsed_date[2].to_i
+  #   formated_date = Date.new(year,month,day)
+  #
+  #   return formated_date
+  # end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -10,20 +10,22 @@ describe 'Room' do
     new_room = Room.new(15)
     reservation_date_1 = Date.new(2018, 9, 10)
     reservation_date_2 = Date.new(2018, 9, 11)
+    date_to_check = Date.new(2018, 9, 11)
     new_room.dates_booked << reservation_date_1
     new_room.dates_booked << reservation_date_2
 
-    expect(new_room.is_available?("2018, 9, 11")).must_equal false
+    expect(new_room.is_available?(date_to_check)).must_equal false
   end
 
   it 'checks if the room is available on the specified date' do
     new_room = Room.new(15)
     reservation_date_1 = Date.new(2018, 9, 10)
     reservation_date_2 = Date.new(2018, 9, 11)
+    date_to_check = Date.new(2018, 9, 13)
     new_room.dates_booked << reservation_date_1
     new_room.dates_booked << reservation_date_2
 
-    expect(new_room.is_available?("2018, 9, 13")).must_equal true
+    expect(new_room.is_available?(date_to_check)).must_equal true
   end
 
 end
