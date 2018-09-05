a4a6262b197525dbae84534bd0afc50cbb789bbe
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-09-05 08:04:38 -0700

    Create test for Reservation#retrieve_by_date, test failing

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -5,6 +5,12 @@ describe "Reservation" do
   let (:reservation) {
     Reservation.new("SoccerMom2010@gmail.com", 1, [Date.new(2018,10,20),Date.new(2018,10,22)])
   }
+  let (:reservation2) {
+    Reservation.new("Guccifer2.0@ada.com", 1, [Date.new(2018,12,02),Date.new(2018,12,07)])
+  }
+  let (:reservation3) {
+    Reservation.new("Jfahmy07@gmail.com", 2, [Date.new(2018,12,03),Date.new(2018,12,06)])
+  }
 
   describe "#initialize" do
 
@@ -28,4 +34,19 @@ describe "Reservation" do
       expect(reservation.cost).must_equal 400.00
     end
   end
+
+  describe "Reservation#retrieve_by_date" do
+    it "raises an argument error if invalid date object is provided" do
+      expect{ (retrieve_by_date("I am a string")) }.must_raise ArgumentError
+    end
+
+    it "returns an array of the reservations relevant to that date" do
+      test_date = retrieve_by_date(Date.new(2018,12,04))
+
+      expect(test_date).must_be_instance_of Array
+      expect(test_date.first).must_be_instance_of Reservation
+      expect(test_date.length).must_equal 2
+    end
+  end
+
 end
