2e480d35ebd6f5d5ab63c7672ddb2af2ea722640
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-05 15:14:42 -0700

    Added test for initializing reservation

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,1 +1,13 @@
 require_relative 'spec_helper'
+
+describe "Reservation Class" do
+  describe "initailize" do
+    let (:duration) { Hotel::Duration.new('2018-09-01', '2018-09-05') }
+    let (:reservation) { Hotel::Reservation.new(1, duration, 20) }
+
+    it 'stores duration as an object' do
+      expect(reservation.duration).must_be_instance_of Hotel::Duration
+
+    end
+  end
+end
