29fe0493dda414bcbde8db3ad73747ac1b30ba5c
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-05 15:33:01 -0700

    Fixed start_time and end_time - was not parsing correctly

--- lib/duration.rb
+++ lib/duration.rb
@@ -1,9 +1,11 @@
+require 'date'
+
 module Hotel
   class Duration
 
     def initialize(start_date, end_date)
-      @start_date = Date.parse
-      @end_date = Date.parse
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
     end
 
     def number_of_nights
