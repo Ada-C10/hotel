9e47d43076977ccf8e1ace8f628b4cd94ea754f9
Karis Kim <ouniiq@hotmail.com>
2018-09-06 14:45:31 -0700

    created 2 tests for reservation_date_range method and passed both

--- spec/reservation_creator_spec.rb
+++ spec/reservation_creator_spec.rb
@@ -1,29 +1,27 @@
 require_relative 'spec_helper'
 
-describe "BookingSystem class" do
-  # before do
-  #   list = Hotel::Reservation.new
-  # end
+describe "Reservation class" do
+  before do
+    @reservation = Hotel::Reservation.new(180904, 180905)
+  end
 
-  it "rooms in hotel method" do
-    list = Hotel::Reservation.new(180904, 180905)
-
-    expect(list).must_be_kind_of Hotel::Reservation
+  it "initialize method" do
+    expect(@reservation).must_be_kind_of Hotel::Reservation
     # expect(list.room_number).must_be_kind_of Integer
     # expect(list.room_number).must_equal 2
-    expect(list.check_in_date).must_equal Date.parse("2018-09-04")
-    expect(list.check_out_date).must_equal Date.parse("2018-09-05")
+    expect(@reservation.check_in_date).must_equal Date.parse("2018-09-04")
+    expect(@reservation.check_out_date).must_equal Date.parse("2018-09-05")
   end
 
   it "raises ArgumentError if check out date is before check in date" do
     expect{(list = Hotel::Reservation.new(180904, 180903))}.must_raise StandardError
   end
 
-  it "calculates the number of nights the user wants to book" do
-    list = Hotel::Reservation.new(180904, 180905)
-    expect(list.count_number_of_nights).must_equal 1
+  it "returns array of all dates user wants to book" do
+    expect(@reservation.reservation_date_range).must_be_kind_of Array
   end
 
-
-
+  it "returns the array of dates except the check out date" do
+    expect(@reservation.reservation_date_range).must_equal [Date.parse("180904")]
+  end
 end
