e095f195119afccb25c06e19ec940d64cd8dcbcd
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-06 01:12:07 -0700

    Hotel class passes specs

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -9,6 +9,31 @@ require "awesome_print"
 module HotBook
 
   class Hotel
+    attr_reader :rooms, :room_count, :room_rate, :room_info
+
+    def initialize(input) #{room_count: 20, room_rate: 200.0}
+      @room_count = input[:room_count]
+      @room_rate = input[:room_rate]
+      @rooms = load_rooms # [{room_number: :1, room_rate: 200.0}]
+    end
+
+    def load_rooms
+      new_rooms = []
+      room_count.times do |i|
+        new_room = Hash.new
+        new_room[:room_number] = room_numbers[i]
+        new_room[:room_rate] = @room_rate
+        new_rooms << new_room
+      end
+      return new_rooms
+    end
+
+    def room_numbers
+      room_numbers = %i(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+      raise ArgumentError, "Wrong number of room numbers" if room_numbers.size != room_count
+      return room_numbers
+    end
+
   end
 
 end
