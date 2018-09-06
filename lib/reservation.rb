2db8b019a67f7d55e9070d5a2f41e79c32154c36
jackie <jacquelyn.cheng@me.com>
2018-09-05 23:07:41 -0700

    Modified reservation.rb to include add_room method that adds a Room to @rooms and updates that room's status, also created associated tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,21 +6,42 @@ require 'date'
 
 module Hotel
   class Reservation
-    attr_reader :checkin_date, :checkout_date, :nights, :nightly_rate, :rooms, :confirmation_number
+    attr_reader :checkin_date, :checkout_date, :confirmation_number, :rooms
 
-    RATE = 200
-
-    def initialize(checkin_date, checkout_date, nightly_rate = RATE, rooms, confirmation_number)
+    def initialize(checkin_date, checkout_date, confirmation_number)
       @checkin_date = checkin_date
       @checkout_date = checkout_date
-      @nights = checkout_date - checkin_date
-      @nightly_rate = nightly_rate
-      @rooms = rooms
+      @rooms = []
       @confirmation_number = confirmation_number
     end
 
+    # adds a room to the reservation and updates rooms status to :BOOKED
+    # checkout_date status is not updated since guest leaves in morning
+    # raises an ArgumentError if Room not provided
+    # returns updated rooms collection if successful
+    def add_room(room)
+      raise ArgumentError, "Must provide a Room." unless room.is_a? Room
+      unless rooms.empty?
+        room_nums = rooms.map { |room| room.room_num }
+        raise ArgumentError, "Room already on reservation." if room_nums.include? room.room_num
+      end
+      checkin_date.upto(checkout_date.prev_day) do |date|
+        room.change_room_status(date, :BOOKED)
+      end
+      rooms << room
+      return rooms
+    end
+
+    # calculates total cost for all rooms over all nights
+    # checkout_date nightly rate is not included since guest leaves in morning
     def total_cost
-      return (nights * nightly_rate).to_f.round(2)
+      total = 0
+      rooms.each do |room|
+        checkin_date.upto(checkout_date.prev_day) do |date|
+          total += room.nightly_rate(date)
+        end
+      end
+      return total.to_f.round(2)
     end
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -7,40 +7,85 @@ require_relative 'spec_helper.rb'
 
 describe "Reservation" do
   before do
-    @start_date = Date.parse("2019-02-01")
-    @end_date = Date.parse("2019-02-05")
-    @reservation = Hotel::Reservation.new(@start_date, @end_date, Hotel::Room.new(1), 10)
+    @checkin_date = Date.parse("2019-02-01")
+    @checkout_date = Date.parse("2019-02-05")
+    @conf_num = 10
   end
+
+  let (:res) {
+    Hotel::Reservation.new(@checkin_date, @checkout_date, @conf_num)
+  }
+
+  let (:room1) {
+    Hotel::Room.new(1, Date.parse("2019-01-01"), Date.parse("2019-12-31"))
+  }
 
   describe "Reservation#initialize" do
     it "instantiates a Reservation object" do
-      expect(@reservation).must_be_instance_of Hotel::Reservation
+      expect(res).must_be_instance_of Hotel::Reservation
     end
 
     it "has a start date" do
-      expect(@reservation.checkin_date).must_equal @start_date
+      expect(res.checkin_date).must_equal @checkin_date
     end
 
     it "has an end date" do
-      expect(@reservation.checkout_date).must_equal @end_date
+      expect(res.checkout_date).must_equal @checkout_date
     end
 
-    it "has a room assigned" do
-      expect(@reservation.rooms).must_be_instance_of Hotel::Room
+    it "initially has an empty collection of rooms" do
+      expect(res.rooms.length).must_equal 0
+      expect(res.rooms).must_be_kind_of Enumerable
     end
 
     it "has a confirmation number" do
-      expect(@reservation.confirmation_number).must_equal 10
+      expect(res.confirmation_number).must_equal @conf_num
+    end
+  end
+
+  describe "Resevation#add_room" do
+    it "updates the room's status" do
+      expect(room1.is_available? @checkin_date).must_equal true
+      expect(room1.is_available? @checkout_date.prev_day).must_equal true
+      res.add_room(room1)
+      expect(room1.is_available? @checkin_date).must_equal false
+      expect(room1.is_available? @checkout_date.prev_day).must_equal false
+    end
+
+    it "checkout_date availability is unmodified" do
+      expect(room1.is_available? @checkout_date).must_equal true
+      res.add_room(room1)
+      expect(room1.is_available? @checkout_date).must_equal true
+    end
+
+    it "adds a room to reservation @rooms" do
+      expect(res.rooms.length).must_equal 0
+      res.add_room(room1)
+      expect(res.rooms.length).must_equal 1
+      expect(res.rooms.first).must_be_instance_of Hotel::Room
+    end
+
+    it "raises an error if Room object is not provided" do
+      expect{res.add_room(4)}.must_raise ArgumentError
+    end
+
+    it "raises an error if Room object is already on reservation" do
+      res.add_room(room1)
+      expect{res.add_room(room1)}.must_raise ArgumentError
     end
   end
 
   describe "Resevation#total_cost" do
+    before do
+      res.add_room(room1)
+    end
+
     it "returns a float" do
-      expect(@reservation.total_cost).must_be_instance_of Float
+      expect(res.total_cost).must_be_instance_of Float
     end
 
     it "correctly calculates cost to two decimal places based on # of nights" do
-      expect(@reservation.total_cost).must_equal 800.00
+      expect(res.total_cost).must_equal 800.00
     end
   end
 end
