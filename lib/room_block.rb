4bf13b0b8bb3900e1d5df71229ddaa6b577ef942
Anibel America <ameriabel@gmail.com>
2018-09-06 14:00:51 -0700

    creates files for RoomBlock class

---
+++ lib/room_block.rb
@@ -0,0 +1,25 @@
+module Hotel
+
+  class RoomBlock #< Reservation
+
+    # attr_reader :room_num, :cost
+    #
+    # def initialize(check_in, check_out, rooms, discounted_rate)
+    #   @date_range = DateRange.new(check_in, check_out)
+    #   @room_num = room_num
+    #   @cost = get_cost
+    #
+    #   if !@room_num.to_s.match /\d+/
+    #     raise ArgumentError, "Invalid Room Number"
+    #   end
+    # end
+    #
+    # def get_cost
+    #   return @date_range.get_total_days * BASE_COST
+    # end
+
+
+
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 # require_relative 'hotel_spec'
 # require_relative '../lib/hotel'
 
-require_relative '../lib/reservation'
 require_relative '../lib/date_range'
 require_relative '../lib/hotel_booker'
+require_relative '../lib/reservation'
+require_relative '../lib/room_block'
