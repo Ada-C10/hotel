7100dcdd9caa13477676a66364dd8b18dab76115
Amber Lynn <griffiam@gmail.com>
2018-09-06 15:41:10 -0700

    completed tests and methods for room and reservation classes

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -9,18 +9,16 @@ module Hotel
       @room = input[:room]
       @check_in = Date.strptime(input[:check_in], '%m/%d/%Y')
       @check_out = Date.strptime(input[:check_out], '%m/%d/%Y')
-      @cost = input[:cost]
+      @cost = calculate_cost
     end
 
-    # def room_reserved
-    # return @room
-    #  - what is the room associated with this reservation
-    #  - return room instance with this reservation
-    # end
+    def room_reserved
+      return @room
+    end
 
     def calculate_cost
-      # total_days = (@check_out - @check_in).to_i
-      # return (total_days * @room.Room.price)
+      total_days = (@check_out - @check_in).to_i
+      return (total_days * @room.price)
     end
 
   end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -6,8 +6,8 @@ describe 'Reservation class' do
 
   describe "Reservation instantiation" do
     before do
-      @room = Hotel::Room.new({room_number: 1, price: 200})
-      @res_1 = Hotel::Reservation.new({room: @room, check_in: "01/10/1988", check_out: "01/17/1988", cost: 1000})
+      @honeymoon_suite = Hotel::Room.new({room_number: 1, price: 200})
+      @res_1 = Hotel::Reservation.new({room: @honeymoon_suite, check_in: "01/10/1988", check_out: "01/17/1988", cost: 1000})
     end
 
     it "it is an instance of Reservation" do
@@ -27,47 +27,28 @@ describe 'Reservation class' do
   end
 
   describe 'room_reserved' do
-    it "returns the room instance associated with this reservation" do
+    before do
+      @honeymoon_suite = Hotel::Room.new({room_number: 1, price: 200})
+      @res_2 = Hotel::Reservation.new({room: @honeymoon_suite, check_in: "01/10/1988", check_out: "01/17/1988", cost: 1000})
     end
 
+    it "returns the room instance associated with this reservation" do
+      expect(@res_2.room_reserved).must_equal @honeymoon_suite
+      expect(@res_2).must_be_kind_of Hotel::Reservation
+    end
   end
-    #
-    # describe "calculate_cost" do
-    #   it "returns total cost of room per night" do
-    #
-    #     room_data = {
-    #       room_number: 2,
-    #       price: 200
-    #     }
-    #
-    #     hyatt = Hotel::Room.new(room_data)
-    #
-    #     expect(hyatt.calculate_cost(5)).must_equal 1000
-    #   end
-    # end
 
-  # describe "calculate_cost" do
-  #   it "returns total cost of room per night" do
-  #
-  #     room_data = {
-  #       room_number: 2,
-  #       price: 200
-  #     }
-  #
-  #     hyatt = Hotel::Room.new(room_data)
-  #
-  #     reservation_data = {
-  #       rooms: hyatt,
-  #       check_in: (2018, 01, 10),
-  #       check_out: (2018, 01, 17),
-  #       cost: hyatt.Room.price
-  #     }
-  #
-  #     reservation_2 = Hotel::Reservation()
-  # #
-  #     expect(hyatt.calculate_cost('1/10/2018', '01/12/2018')).must_equal 400
-  #   end
+  describe 'calculate_cost' do
+    before do
+      @honeymoon_suite = Hotel::Room.new({room_number: 1, price: 200})
+      @res_3 = Hotel::Reservation.new({room: @honeymoon_suite, check_in: "01/10/1988", check_out: "01/17/1988", cost: nil})
+    end
 
-
+    it "returns cost of room based on days reserved" do
+      expect(@res_3.check_in).must_be_kind_of Date
+      expect(@res_3.check_out).must_be_kind_of Date
+      expect(@res_3.calculate_cost).must_equal 1400
+    end
+  end
 
 end
