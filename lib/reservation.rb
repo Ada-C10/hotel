e0738b26376c1d85afaea4332fc0fbf4bed91b3a
Kat Perreira <kat.perreira@gmail.com>
2018-09-06 14:46:13 -0700

    Creates an array of dates within check in and check out range within the constructor

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -4,15 +4,23 @@ class Reservation
   attr_reader :room
   attr_reader :check_in
   attr_reader :check_out
+  attr_reader :reservation_dates
 
   def initialize(room, check_in, check_out)
     @room = room
     @check_in = check_in
     @check_out = check_out
+    # @reservation_dates = []
+
 
     if check_out < check_in
       raise StandardError.new("Check out date is before check in date")
     end
+    @reservation_dates = (@check_in...@check_out).to_a
+    # binding.pry
+    # @reservation_dates = reservation_dates.to_a
+
+
   end
 
   def stay_duration
