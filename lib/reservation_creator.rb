4fa4d3b310e2fcdeb439692208e3f26aae60739e
Karis Kim <ouniiq@hotmail.com>
2018-09-06 15:56:21 -0700

    changed instance variables as hash key/values

--- lib/reservation_creator.rb
+++ lib/reservation_creator.rb
@@ -4,14 +4,23 @@ module Hotel
   class Reservation
     attr_reader :check_in_date, :check_out_date
 
-    def initialize(check_in_date, check_out_date)
-      @check_in_date = Date.parse("#{check_in_date}")
-      @check_out_date = Date.parse("#{check_out_date}")
+    def initialize(input)
+      @check_in_date = input[:check_in_date]
+      @check_out_date = input[:check_out_date]
         raise StandardError, "Checkout date cannot be before checkin date" unless check_in_date < check_out_date
     end
 
+    def checkin_num_to_date
+      puts :check_date
+      return Date.parse("#{check_in_date}")
+    end
+
+    def checkout_num_to_date
+      return Date.parse("#{check_out_date}")
+    end
+
     def reservation_date_range
-      date_range = [*check_in_date..check_in_date]
+      date_range = [*checkin_num_to_date..checkout_num_to_date]
       return date_range
     end
   end
