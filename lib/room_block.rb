e129b79111e2b4f6a97d2c7fdaecb3d92f3f039e
Anibel America <ameriabel@gmail.com>
2018-09-06 14:45:50 -0700

    adds tests and code for RoomBlock initialization

--- lib/room_block.rb
+++ lib/room_block.rb
@@ -2,14 +2,42 @@ module Hotel
 
   class RoomBlock #< Reservation
 
-    # attr_reader :room_num, :cost
+    attr_reader :check_in, :check_out, :blocked_rooms, :discounted_rate, :block_reservations
     #
     def initialize(check_in, check_out, blocked_rooms, discounted_rate)
       @date_range = DateRange.new(check_in, check_out)
       @blocked_rooms = blocked_rooms
       @discounted_rate = discounted_rate
+      @block_reservations = []
 
-      # TODO: check if blocked_rooms contains non-repeating rooms, only from 1 - 20
+      if @blocked_rooms.length > 5
+        raise ArgumentError, "A block can contain a maximum of 5 rooms"
+      end
+
+      if !is_number(@discounted_rate)
+        raise ArgumentError, "Discounted rate is not a valid number"
+      end
+
+      # TODO: this should be rescued?
+      if @discounted_rate > 200
+        raise ArgumentError, "Discounted rate is greater than Base rate: #{BASE_COST}."
+      end
+
+      if @blocked_rooms.uniq.length != @blocked_rooms.length
+        raise ArgumentError, "Cannot have repeating rooms"
+      end
+
+      @blocked_rooms.each do |room_num|
+        unless [*1..20].include?(room_num)
+          raise ArgumentError, "Room: #{room_num} not in hotel"
+        end
+      end
+
+      # TODO: add a block_reserved array to hotel_booker class
+    end
+
+    def is_number(num)
+      return num.to_f.to_s == num.to_s || num.to_i.to_s == num.to_s
     end
     #
     # def get_cost
--- spec/room_block_spec.rb
+++ spec/room_block_spec.rb
@@ -1,25 +1,44 @@
 require_relative 'spec_helper'
 
 describe 'RoomBlock class' do
-  # let (:check_in) {
-  #   Date.today
-  # }
-  # let (:check_out) {
-  #   Date.today + 3
-  # }
-  # let (:block_rooms) {
-  #   [*1..5]
-  # }
-  # let (:discounted_rate) {
-  #   150
-  # }
-  #
-  # let (:block) {
-  #   Hotel::RoomBlock.new(check_in, check_out, block_rooms, discounted_rate)
-  # }
+  let (:check_in) {
+    Date.today
+  }
+  let (:check_out) {
+    Date.today + 3
+  }
+  let (:blocked_rooms) {
+    [*1..5]
+  }
+  let (:discounted_rate) {
+    150
+  }
+
+  let (:block) {
+    Hotel::RoomBlock.new(check_in, check_out, blocked_rooms, discounted_rate)
+  }
 
 
   describe 'RoomBlock Instantiation' do
-    expect(Hotel::RoomBlock.new(Date.today, Date.today + 1, [*1..5], 150)).must_be_instance_of Hotel::RoomBlock
+    it 'creates a RoomBlock object' do
+      expect(block).must_be_instance_of Hotel::RoomBlock
+    end
+
+    it 'raises an error if given more than 5 rooms' do
+      expect{Hotel::RoomBlock.new(check_in, check_out, [*1..6], discounted_rate)}.must_raise ArgumentError
+    end
+
+    it 'raises an error if given an invaid discounted_rate' do
+      expect{Hotel::RoomBlock.new(check_in, check_out, blocked_rooms, "Discounted Rate")}.must_raise ArgumentError
+    end
+
+    it 'raises an error if given a list of rooms with repeating room numbers' do
+      expect{Hotel::RoomBlock.new(check_in, check_out, [1,1,1,1,1], discounted_rate)}.must_raise ArgumentError
+    end
+
+    it 'raises an error if given a list of rooms with an room number less than 1 or greater than 20' do
+      expect{Hotel::RoomBlock.new(check_in, check_out, [0, 1, 2, 3], discounted_rate)}.must_raise ArgumentError
+      expect{Hotel::RoomBlock.new(check_in, check_out, [19, 20, 21], discounted_rate)}.must_raise ArgumentError
+    end
   end
 end
