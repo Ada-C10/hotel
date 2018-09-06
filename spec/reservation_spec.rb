bf1b240ec3b13cdf3f9b6a74f68210c6efa2913c
Katricia Smith <smithkatricia@gmail.com>
2018-09-06 15:38:27 -0700

    Edited dates in Reservation initialization test

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -5,13 +5,12 @@ describe "Reservation class" do
     before do
       @manager = Hotel::BookingManager.new(1)
       @room = @manager.rooms.first
-      @new_booking = Hotel::Reservation.new(@room, guest_name: "Tina Fey", start_date: "June 4", end_date: "June 7")
+      @new_booking = Hotel::Reservation.new(@room, guest_name: "Tina Fey", start_date: "June 11, 2018", end_date: "June 14, 2018")
     end
 
     it "creates a new instance of Reservation" do
       expect(@new_booking).must_be_instance_of Hotel::Reservation
     end # of new reservation is new instance it
-
   end # of initialize method
 
 
