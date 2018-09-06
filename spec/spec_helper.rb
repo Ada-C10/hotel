cf8adaa6b9706b3ff21a207008495684f4ec366c
Amber Lynn <griffiam@gmail.com>
2018-09-06 14:02:20 -0700

    Set up reservation and room spec files. Created and passed initialization tests for both classes. Working on returning room instance in reservation class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
@@ -6,3 +8,5 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative '../lib/reservation'
+require_relative '../lib/room'
