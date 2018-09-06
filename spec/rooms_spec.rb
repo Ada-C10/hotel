95de6be0521063d7d2905924c51968171441292b
Karis Kim <ouniiq@hotmail.com>
2018-09-06 14:11:54 -0700

    created file to test rooms class

---
+++ spec/rooms_spec.rb
@@ -0,0 +1,11 @@
+# require_relative 'spec_helper'
+#
+# describe "Rooms class" do
+#   it "list rooms method" do
+#     expect(Hotel::Rooms.total_rooms_in_hotel).must_equal 20
+#   end
+
+  # it "raises ArgumentError if check out date is before check in date" do
+  #   expect{(list = Hotel::Reservation.new(180904, 180903))}.must_raise StandardError
+  # end
+# end
