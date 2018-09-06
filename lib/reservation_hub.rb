bfb88945b7ff890b13567c8c95b45d23d6c506d4
Lindsay Terchin <lterchin@gmail.com>
2018-09-06 09:13:49 -0700

    created method to return all reservations

--- lib/reservation_hub.rb
+++ lib/reservation_hub.rb
@@ -1,43 +1,53 @@
 require 'date'
+require 'pry'
+
+require_relative 'reservation'
 
 module Hotel
   class ReservationHub
-    attr_reader :start_date, :end_date
-    attr_accessor :reservation_id
-
-
+    attr_reader :start_date, :end_date, :rooms, :reservations
+    # attr_accessor :reservation_id, :reservations
 
     ROOMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
-    def initialize(all_reservations)
+    def initialize
+      @reservations = []
+      @rooms = ROOMS
+    end
+
+
+    def add_reservation(start_year, start_month, start_day, end_year, end_month, end_day)
+
+      reservation = Reservation.new(start_year, start_month, start_day, end_year, end_month, end_day)
+
+      all_reservations(reservation)
+    end
+
+
+    def find_reservations(start_date)
+      reservations_by_date = []
+      ReservationHub.add_reservation.each do |reservation|
+        if reservation.start_date = start_date
+          reservations_by_date << reservation
+        end
+
+      end
 
     end
 
 
 
-    def add_reservation(reservation_id, start_date, end_date)
-      reservation_id = create_id
-
-      reservation = Reservation.new(reservation_id, start_date, end_date)
-
+    def all_reservations(reservation)
       @reservations << reservation
+      return @reservations
     end
-
-
-
-    #create a new reservation id by adding 1 to total reservations
-    def create_id
-      new_id = @reservations.length + 1
-    end
-
-
-
-    def return_all_reservations
-      @reservations
-    end
+    #
+    #
+    # def find_reservations(start_date)
+    #
+    # end
 
 
   end
-
 end
