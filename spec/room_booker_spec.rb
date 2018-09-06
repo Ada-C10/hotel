e51bd13e3721664b0bc8e73467d911476c7d576f
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-05 22:27:41 -0700

    added test to check if invalid dates raises an argument error

--- spec/room_booker_spec.rb
+++ spec/room_booker_spec.rb
@@ -1,20 +1,46 @@
 require_relative 'spec_helper'
 
-describe "Room Booker Class" do
-  describe "initialize" do
-    let (:booking_system) {
-      Hotel::RoomBooker.new()
-    }
+describe "RoomBooker Class" do
+  let (:booking_system) { Hotel::RoomBooker.new() }
 
+  describe "initialize" do
     it 'Creates an array of room numbers' do
-      expect(booking_system.rooms).must_be_instance_of Array
+      expect(booking_system.rooms).must_be_kind_of Array
     end
 
     it 'Creates an array of reservation instances' do
-      expect(:booking_system).must_be_instance_of Hotel::Reservation 
+      expect(booking_system).must_be_kind_of Hotel::Reservation
     end
-
-
-
   end
+
+  describe "list_rooms method" do
+    it 'Returns an array of room numbers' do
+      expect(booking_system.list_rooms).must_be_kind_of Array
+    end
+  end
+
+  describe "check_valid_dates method" do
+    it 'Raises an ArgumentError for invalid date ranges' do
+
+      expect{
+        booking_system.check_valid_dates('2018-09-05', '2018-09-01')
+      }.must_raise ArgumentError
+
+      expect{
+        booking_system.check_valid_dates('', '2018-09-01')
+      }.must_raise ArgumentError
+
+      expect{
+        booking_system.check_valid_dates('2018-09-01', '')
+      }.must_raise ArgumentError
+
+      expect{
+        booking_system.check_valid_dates('', '')
+      }.must_raise ArgumentError
+
+    end
+  end
+
+
+
 end
