b6260f273c1dd911139db33265aa46d26dd08779
Kat Perreira <kat.perreira@gmail.com>
2018-09-05 17:02:30 -0700

    added booking manager to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/room.rb'
 require_relative '../lib/reservation.rb'
-require_relative '../lib/reservation_manager.rb'
+require_relative '../lib/booking_manager.rb'
 # Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
