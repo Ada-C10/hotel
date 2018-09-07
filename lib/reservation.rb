f1b9bf8e75bdd3b69750d1cd38e3f1c3a2ca223d
Karis Kim <ouniiq@hotmail.com>
2018-09-07 14:50:49 -0700

    changed file name for readability

---
+++ lib/reservation.rb
@@ -0,0 +1,30 @@
+require 'date'
+
+module Hotel
+  class Reservation
+    attr_reader :check_in_date, :check_out_date
+
+    def initialize(input)
+      @check_in_date = input[:check_in_date]
+      @check_out_date = input[:check_out_date]
+        raise StandardError, "Checkout date cannot be before checkin date" unless check_in_date < check_out_date
+      # @room_number = [*1..20].first
+      # input[:room_number]
+
+      @room_number = input[:room_number]
+    end
+
+    def checkin_num_to_date
+      return Date.parse("#{check_in_date}")
+    end
+
+    def checkout_num_to_date
+      return Date.parse("#{check_out_date}")
+    end
+
+    def booking_date_range
+      date_range = [*checkin_num_to_date...checkout_num_to_date]
+      return date_range
+    end
+  end
+end
