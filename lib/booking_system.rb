93dc58bbb60a8f5b432a6510abaa7463f0e0d994
kangazoom <michellelcronin@gmail.com>
2018-09-07 12:12:44 -0700

    Spruced up tests in BookingSystem (#initialize and #list_all_rooms)

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,6 +1,6 @@
 require "date"
 
-require_relative 'room'
+# require_relative 'room'
 require_relative 'reservation'
 
 module Hotel
@@ -8,110 +8,114 @@ module Hotel
     attr_reader :rooms, :reservations
 
     def initialize()
-      @rooms = list_rooms() #<-- array of all room objects
+      @rooms = list_all_rooms() #<-- array of all room numbeers
       @reservations = []
     end
 
-    def list_rooms()
+    def list_all_rooms()
       return (1..20).to_a
       # all_rooms = nums.map { |num| Hotel::Room.new(num: num)}
     end
 
-    # TODO: maybe use date range instead of start/end?
-    def create_reservation(start_date, end_date)
-      id = create_res_id()
-      rooms = list_avail_rooms_for_range(start_date, end_date)
-
-      # QUESTION: AM I DOING THIS RIGHT?
-      unless rooms != nil
-        raise StandardError, "No rooms are available for the given date range: #{start_date} - #{end_date}."
-      end
-
-      reservation_hash = {
-        id: id,
-        room: rooms[0],
-        check_in: start_date,
-        check_out: end_date,
-      }
-
-      new_reservation = Reservation.new(reservation_hash)
-      #NOTE: add room.change_status??? should this part go in Res???
-      # or should this be split up????
-      room.add_reservation(new_reservation)
-      @reservations << new_reservation
-
-      return new_reservation
-    end
-
-    # QUESTION: NOT NEEDED-- JUST IN RESERVATION?
-    # def add_reservation(new_reservation)
-    #   @reservations << create_reservation(new_reservation)
-    # end
-
-    # QUESTION: add loading class or hold off? should be blank list at top?
-    # def load_reservations(input)
-    #   new_reservation = create_reservation()
-    #   add_reservation(new_reservation)
-    #   return @reservations
-    # end
-
-
-# QUESTION: should i be accessing this
-# QUESTION: add a pretty to_s for listing reserved dates?
-    def list_reservations_for_date(check_date)
-      date = Date.parse(check_date)
-      #TODO error handling for date as Date object??
-      #TODO what if no dates match? should return nil?
-      # QUESTION: maybe add one to display by id?
-      # QUESTION: should room own this???
-      matching_res = @reservations.select { |reservation| reservation.dates_reserved.include? date }
-
-      # TODO: combine enumerable with ternary???? --> do in one line???
-      return matching_res.empty? ? nil : matching_res
-    end
-
-
-# QUESTION: should this method be here or in Reservation?
-# QUESTION: test nil return if nothing found
-    def find_room(room_num)
-      return @rooms.find {|room| room.num == room_num.to_i}
-    end
-
     def create_date_range(start_date, end_date)
+      # TODO: error handling should probably go to cal class (take from reservation plus tests -- 2x)
       return (start_date...end_date).to_a
     end
 
-    def overlap?(date_range_1, date_range_2)
-      return false if (date_range_1 & date_range_2).empty? else return true
-    end
-
-    def list_avail_rooms_for_date(check_date)
-      avail_rooms = @reservations.select { |reservation| !reservation.date_range.include?(check_date) } #==false
-        # check for no overlap in dates
-        # if (reservation.date_range & check_date_range).empty?
-
-      return avail_rooms.empty? ? nil : avail_rooms
-
-    end
-
-    def list_avail_rooms_for_range(start_date, end_date)
-      check_date_range = create_date_range(start_date, end_date)
-
-      avail_rooms = @reservations.select { |reservation| reservation.room if !overlap?(reservation.date_range, check_date_range)} #==false
-
-      return avail_rooms.empty? ? nil : avail_rooms
-        # check for no overlap in dates
-        # if (reservation.date_range & check_date_range).empty?
-
-    end
-
-    def create_res_id()
+    def generate_res_id()
       if @reservations.empty?
         return 1
       else
         return @reservations.max_by { |reservation| reservation.id}.id + 1
       end
     end
+
+
+#     # TODO: maybe use date range instead of start/end?
+#     def create_reservation(start_date, end_date)
+#       id = create_res_id()
+#       rooms = list_avail_rooms_for_range(start_date, end_date)
+#
+#       # QUESTION: AM I DOING THIS RIGHT?
+#       unless rooms != nil
+#         raise StandardError, "No rooms are available for the given date range: #{start_date} - #{end_date}."
+#       end
+#
+#       reservation_hash = {
+#         id: id,
+#         room: rooms[0],
+#         check_in: start_date,
+#         check_out: end_date,
+#       }
+#
+#       new_reservation = Reservation.new(reservation_hash)
+#       #NOTE: add room.change_status??? should this part go in Res???
+#       # or should this be split up????
+#       room.add_reservation(new_reservation)
+#       @reservations << new_reservation
+#
+#       return new_reservation
+#     end
+#
+#     # QUESTION: NOT NEEDED-- JUST IN RESERVATION?
+#     # def add_reservation(new_reservation)
+#     #   @reservations << create_reservation(new_reservation)
+#     # end
+#
+#     # QUESTION: add loading class or hold off? should be blank list at top?
+#     # def load_reservations(input)
+#     #   new_reservation = create_reservation()
+#     #   add_reservation(new_reservation)
+#     #   return @reservations
+#     # end
+#
+#
+# # QUESTION: should i be accessing this
+# # QUESTION: add a pretty to_s for listing reserved dates?
+#     def list_reservations_for_date(check_date)
+#       date = Date.parse(check_date)
+#       #TODO error handling for date as Date object??
+#       #TODO what if no dates match? should return nil?
+#       # QUESTION: maybe add one to display by id?
+#       # QUESTION: should room own this???
+#       matching_res = @reservations.select { |reservation| reservation.dates_reserved.include? date }
+#
+#       # TODO: combine enumerable with ternary???? --> do in one line???
+#       return matching_res.empty? ? nil : matching_res
+#     end
+#
+#
+# # QUESTION: should this method be here or in Reservation?
+# # QUESTION: test nil return if nothing found
+#     def find_room(room_num)
+#       return @rooms.find {|room| room.num == room_num.to_i}
+#     end
+#
+#     def overlap?(date_range_1, date_range_2)
+#       return false if (date_range_1 & date_range_2).empty? else return true
+#     end
+#
+#     def list_avail_rooms_for_date(check_date)
+#       avail_rooms = @reservations.select { |reservation| !reservation.date_range.include?(check_date) } #==false
+#         # check for no overlap in dates
+#         # if (reservation.date_range & check_date_range).empty?
+#
+#       return avail_rooms.empty? ? nil : avail_rooms
+#
+#     end
+#
+#     def list_avail_rooms_for_range(start_date, end_date)
+#       check_date_range = create_date_range(start_date, end_date)
+#
+#       avail_rooms = @reservations.select { |reservation| reservation.room if !overlap?(reservation.date_range, check_date_range)} #==false
+#
+#       return avail_rooms.empty? ? nil : avail_rooms
+#         # check for no overlap in dates
+#         # if (reservation.date_range & check_date_range).empty?
+#
+#     end
+#
+
   end
 end
 
--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -5,190 +5,243 @@ require_relative 'spec_helper'
 describe "BookingSystem class" do
   let(:booking_system) {Hotel::BookingSystem.new()}
 
-# TODO test that rooms contains room obj like in load rooms
   describe "#initialize" do
     it "can create a new instance of BookingSystem" do
       expect(booking_system).must_be_kind_of Hotel::BookingSystem
+    end
 
-    it "creates accurate attribute types"
+    it "creates accurate attribute types" do
       expect(booking_system.rooms).must_be_kind_of Array
       expect(booking_system.reservations).must_be_kind_of Array
     end
   end
 
-  describe "#load_rooms" do
-    let(:rooms_list) {booking_system.load_rooms()}
+  describe "#list_all_rooms" do
+    let(:rooms_list) {booking_system.list_all_rooms()}
 
     it "can load a list of rooms" do
       expect(rooms_list).must_be_kind_of Array
     end
 
-    it "accurately loads room info in array" do
+    it "accurately loads each instance of room as an integer" do
+      rooms_list.each do |room|
+        expect(room).must_be_kind_of Integer
+      end
+    end
+
+    it "accurately describes first room" do
       first_room = rooms_list[0]
+      expect(first_room).must_equal 1
+    end
+
+    it "accurately describes last room" do
       last_room= rooms_list[-1]
-
-      expect(first_room).must_be_kind_of Hotel::Room
-      expect(first_room.num).must_equal 1
-
-      expect(last_room).must_be_kind_of Hotel::Room
-      expect(last_room.num).must_equal 20
+      expect(last_room).must_equal 20
     end
   end
 
-  describe "#list_all_rooms" do
-    let(:all_rooms_str) {booking_system.list_all_rooms()}
-
-    it "lists all rooms as a string" do
-      beginning_text = "Here is a list of all rooms:"
-      room_2 = "Room 2"
-
-      expect(all_rooms_str).must_be_kind_of String
-      expect(all_rooms_str).must_include beginning_text
-      expect(all_rooms_str).must_include room_2
-    end
-  end
-
-  describe "#find_room" do
-    let(:room_num) {4}
-    let(:room_obj) {booking_system.find_room(room_num)}
-
-    it "finds room object by room number" do
-      # num = 4
-      # room_obj = booking_system.find_room(num)
-
-      expect(room_obj).must_be_kind_of Hotel::Room
-      expect(room_obj.num).must_equal room_num
-    end
-  end
-
-# TODO
   describe "#create_date_range" do
-  end
+    let(:start_date) {Date.parse("07-01-2010")}
+    let(:end_date) {Date.parse("07-04-2010")}
+    let(:date_range) {booking_system.create_date_range(start_date, end_date)}
 
-  # TODO
-  describe "#overlap?" do
-  end
-
-# TODO: add create reservation method + 2nd input as room_num??
-  describe "#create_reservation" do
-    # TODO: A reservation is allowed start on the same day that another reservation for the same room ends
-    # TODO test that it's added to Room
-    # TODO test that it's added to reservations
-    # let(:room_num) {4}
-    # let(:room_obj) {booking_system.find_room(room_num)}
-    let(:reservation_hash) {{
-      id: "5",
-      room_num: "10",
-      start_date: "2010-8-6",
-      end_date: "2010-8-10",
-      }}
-    let(:new_reservation) {booking_system.create_reservation(reservation_hash)}
-
-    it "creates a new reservation successfully" do
-      expect(new_reservation).must_be_kind_of Hotel::Reservation
+    it "returns an array of Date objects" do
+      expect(date_range).must_be_kind_of Array
+      date_range.each do |date|
+        date.must_be_kind_of Date
+      end
     end
 
-    it "loads reservation details properly" do
-      # QUESTION: should i test values or types? in rideshare, seems like mostly type was tested....
-      expect(new_reservation.id).must_equal 5
-      expect(new_reservation.room.num).must_equal 10
-      expect(new_reservation.start_date).must_be_kind_of Date
-      expect(new_reservation.end_date).must_be_kind_of Date
-      expect(new_reservation.daily_rate).must_equal 200
+    it "accurately returns first Date value" do
+      expect(date_range[0]).must_equal start_date
+    end
 
+    it "accurately returns last Date value" do
+      expect(date_range[-1]).must_equal end_date-1
     end
   end
 
-  # describe "#add_reservation" do
-  #   updated_reservations = booking_system.add_reservation(new_reservation)
-  # end
+  describe "#generate_res_id" do
+    let(:first_res) {Reservation.new("08-01-1996", "08-03-1996")}
+    let(:second_res) {Reservation.new("09-09-1996", "08-13-1996")}
+    let(:third_res) {Reservation.new("10-01-1996", "10-10-1996")}
 
-  # describe "#load_reservations" do
-  # end
+    it "creates the first reservation ID if it's the first reservation in reservations list" do
+      booking_system.reservations << first_res
 
-  describe "#list_reservations_for_date" do
-    let(:res_1) {booking_system.create_reservation({
-      id: "1",
-      room_num: "20",
-      start_date: "2010-8-1",
-      end_date: "2010-8-10"
-      })}
-
-    let(:res_2) {booking_system.create_reservation({
-      id: "2",
-      room_num: "19",
-      start_date: "2010-8-15",
-      end_date: "2010-8-18"
-      })}
-
-    let(:res_3) {booking_system.create_reservation({
-      id: "4",
-      room_num: "15",
-      start_date: "2010-8-4",
-      end_date: "2010-8-20"
-      })}
-
-    let(:matching_res) {booking_system.list_reservations_for_date("2010-8-5")}
-    # puts @matching_res}
-    # res_1 = booking_system.create_reservation({
-    #   id: "1",
-    #   room_num: "20",
-    #   start_date: "2010-8-1",
-    #   end_date: "2010-8-10",
-    #   })
-    # res_2 = booking_system.create_reservation({
-    #   id: "2",
-    #   room_num: "19",
-    #   start_date: "2010-8-15",
-    #   end_date: "2010-8-18",
-    #   })
-    # res_3 = booking_system.create_reservation({
-    #   id: "4",
-    #   room_num: "15",
-    #   start_date: "2010-8-4",
-    #   end_date: "2010-8-20",
-    #   })
-
-    # @matching_res = booking_system.list_reservations_for_date("2010-8-5")
-    # puts @matching_res
-    # end
-
-    it "should return an array of Reservation objects" do
-      expect(matching_res).must_be_kind_of Array
-      expect(matching_res[0]).must_be_kind_of Hotel::Reservation
+      expect(reservations.generate_res_id[0].id).must_equal 1
     end
 
-    it "accurately loads Reservation objects for specified date" do
-      expect(matching_res.length).must_equal 2
-      expect(matching_res[0].id).must_equal 1
-      expect(matching_res[1].id).must_equal 4
+    it "can generate accurate IDs when new reservations are added to reservations list" do
+      booking_system.reservations << first_res
+      booking_system.reservations << second_res
+
+      expect(reservations.generate_res_id[0].id).must_equal 1
+      expect(reservations.generate_res_id[1].id).must_equal 2
+
+      booking_system.reservations << third_res
+
+      expect(reservations.generate_res_id[-1].id).must_equal 3
     end
 
-    it "returns nil if no Reservations are found for specified date" do
-      matching_res = booking_system.list_reservations_for_date("2010-8-30")
-      expect(matching_res).must_equal nil
-    end
+
+
+
+
   end
+#
+#   describe "#list_all_rooms" do
+#     let(:all_rooms_str) {booking_system.list_all_rooms()}
+#
+#     it "lists all rooms as a string" do
+#       beginning_text = "Here is a list of all rooms:"
+#       room_2 = "Room 2"
+#
+#       expect(all_rooms_str).must_be_kind_of String
+#       expect(all_rooms_str).must_include beginning_text
+#       expect(all_rooms_str).must_include room_2
+#     end
+#   end
+#
+#   describe "#find_room" do
+#     let(:room_num) {4}
+#     let(:room_obj) {booking_system.find_room(room_num)}
+#
+#     it "finds room object by room number" do
+#       # num = 4
+#       # room_obj = booking_system.find_room(num)
+#
+#       expect(room_obj).must_be_kind_of Hotel::Room
+#       expect(room_obj.num).must_equal room_num
+#     end
+#   end
+#
 
-  describe "#list_avail_rooms_for_date" do
-    let(:start_date) {"2010-8-15"}
-    let(:end_date) {"2010-8-16"}
-
-    it "returns an array of room numbers" do
-      expect(booking_system.available_rooms_by_date(start_date, end_date)).must_be_kind_of Array
-
-      expect(booking_system.available_rooms_by_date(start_date, end_date)[0]).must_be_kind_of Integer
-
-    end
-  end
-
-  #TODO
-  describe "#list_avail_rooms_for_range" do
-  end
-
-  # TODO
-  describe "#create_res_id" do
-  end
-
+#
+#   # TODO
+#   describe "#overlap?" do
+#   end
+#
+# # TODO: add create reservation method + 2nd input as room_num??
+#   describe "#create_reservation" do
+#     # TODO: A reservation is allowed start on the same day that another reservation for the same room ends
+#     # TODO test that it's added to Room
+#     # TODO test that it's added to reservations
+#     # let(:room_num) {4}
+#     # let(:room_obj) {booking_system.find_room(room_num)}
+#     let(:reservation_hash) {{
+#       id: "5",
+#       room_num: "10",
+#       start_date: "2010-8-6",
+#       end_date: "2010-8-10",
+#       }}
+#     let(:new_reservation) {booking_system.create_reservation(reservation_hash)}
+#
+#     it "creates a new reservation successfully" do
+#       expect(new_reservation).must_be_kind_of Hotel::Reservation
+#     end
+#
+#     it "loads reservation details properly" do
+#       # QUESTION: should i test values or types? in rideshare, seems like mostly type was tested....
+#       expect(new_reservation.id).must_equal 5
+#       expect(new_reservation.room.num).must_equal 10
+#       expect(new_reservation.start_date).must_be_kind_of Date
+#       expect(new_reservation.end_date).must_be_kind_of Date
+#       expect(new_reservation.daily_rate).must_equal 200
+#
+#     end
+#   end
+#
+#   # describe "#add_reservation" do
+#   #   updated_reservations = booking_system.add_reservation(new_reservation)
+#   # end
+#
+#   # describe "#load_reservations" do
+#   # end
+#
+#   describe "#list_reservations_for_date" do
+#     let(:res_1) {booking_system.create_reservation({
+#       id: "1",
+#       room_num: "20",
+#       start_date: "2010-8-1",
+#       end_date: "2010-8-10"
+#       })}
+#
+#     let(:res_2) {booking_system.create_reservation({
+#       id: "2",
+#       room_num: "19",
+#       start_date: "2010-8-15",
+#       end_date: "2010-8-18"
+#       })}
+#
+#     let(:res_3) {booking_system.create_reservation({
+#       id: "4",
+#       room_num: "15",
+#       start_date: "2010-8-4",
+#       end_date: "2010-8-20"
+#       })}
+#
+#     let(:matching_res) {booking_system.list_reservations_for_date("2010-8-5")}
+#     # puts @matching_res}
+#     # res_1 = booking_system.create_reservation({
+#     #   id: "1",
+#     #   room_num: "20",
+#     #   start_date: "2010-8-1",
+#     #   end_date: "2010-8-10",
+#     #   })
+#     # res_2 = booking_system.create_reservation({
+#     #   id: "2",
+#     #   room_num: "19",
+#     #   start_date: "2010-8-15",
+#     #   end_date: "2010-8-18",
+#     #   })
+#     # res_3 = booking_system.create_reservation({
+#     #   id: "4",
+#     #   room_num: "15",
+#     #   start_date: "2010-8-4",
+#     #   end_date: "2010-8-20",
+#     #   })
+#
+#     # @matching_res = booking_system.list_reservations_for_date("2010-8-5")
+#     # puts @matching_res
+#     # end
+#
+#     it "should return an array of Reservation objects" do
+#       expect(matching_res).must_be_kind_of Array
+#       expect(matching_res[0]).must_be_kind_of Hotel::Reservation
+#     end
+#
+#     it "accurately loads Reservation objects for specified date" do
+#       expect(matching_res.length).must_equal 2
+#       expect(matching_res[0].id).must_equal 1
+#       expect(matching_res[1].id).must_equal 4
+#     end
+#
+#     it "returns nil if no Reservations are found for specified date" do
+#       matching_res = booking_system.list_reservations_for_date("2010-8-30")
+#       expect(matching_res).must_equal nil
+#     end
+#   end
+#
+#   describe "#list_avail_rooms_for_date" do
+#     let(:start_date) {"2010-8-15"}
+#     let(:end_date) {"2010-8-16"}
+#
+#     it "returns an array of room numbers" do
+#       expect(booking_system.available_rooms_by_date(start_date, end_date)).must_be_kind_of Array
+#
+#       expect(booking_system.available_rooms_by_date(start_date, end_date)[0]).must_be_kind_of Integer
+#
+#     end
+#   end
+#
+#   #TODO
+#   describe "#list_avail_rooms_for_range" do
+#   end
+#
+#   # TODO
+#   describe "#create_res_id" do
+#   end
+#
 
 end
