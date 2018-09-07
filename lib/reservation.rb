c1138a373a1b5274726b3f9ec197c63f102a1271
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-07 07:42:58 -0700

    Change Reservation#initialize parameters and update tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,9 +1,11 @@
 class Reservation
-  attr_reader :check_in, :number_of_nights, :cost
+  attr_reader :check_in, :check_out, :number_of_nights, :cost
   PRICE = 200
-  def initialize(check_in, number_of_nights)
-    @check_in = Date.parse(check_in.to_s)
-    @number_of_nights = number_of_nights
+  def initialize(check_in, check_out)
+    # raise StandardError, "Invalid check-in date format." unless check_in =~ (/\d{6}/)
+    @check_in = Date.parse(check_in)
+    @check_out = Date.parse(check_out)
+    @number_of_nights = @check_out.mday - @check_in.mday
     @cost = PRICE * number_of_nights
   end
   def get_all_dates
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -2,36 +2,42 @@ require_relative 'spec_helper'
 
 describe Reservation do
   describe "#initialize" do
+    before do
+      @reservation = Reservation.new('181202', '181204')
+    end
     it "can be instantiated" do
-      @reservation = Reservation.new(181202, 2)
       expect(@reservation).must_be_kind_of Reservation
     end
-    it "takes check_in and number_of_nights" do
-      @reservation = Reservation.new(181202, 2)
+    it "takes check_in and check_out" do
       expect(@reservation).must_respond_to :check_in
-      expect(@reservation).must_respond_to :number_of_nights
+      expect(@reservation).must_respond_to :check_out
     end
+    # it "raises StandardError if check_in isn't 6 digits" do
+    #   expect(Reservation.new(20181202, 2)).must_raise StandardError, "Invalid check-in date format."
+    # end
     it "converts check_in to Date object" do
-      @reservation = Reservation.new(181202, 2)
       expect(@reservation.check_in).must_be_kind_of Date
       # expect(@reservation.check_in).must_equal 2018-12-02
     end
+    it "calculates and stores number of nights" do
+      expect(@reservation.number_of_nights).must_equal 2
+    end
     it "calculates and stores cost" do
-      @reservation = Reservation.new(181202, 2)
       expect(@reservation.cost).must_equal 400
       expect(@reservation).must_respond_to :cost
     end
   end
-  describe "#get_all_dates" do
-    it "returns Array" do
-      @reservation = Reservation.new(181202, 2)
-      expect(@reservation.get_all_dates).must_be_kind_of Array
-    end
-    it "contains all dates for reservation" do
-      @reservation = Reservation.new(181202, 2)
-      expect(@reservation.get_all_dates.length).must_equal 2
-      expect(@reservation.get_all_dates[0]).must_be_kind_of Date
-      expect(@reservation.get_all_dates[1]).must_be_kind_of Date
-    end
-  end
+
+  # describe "#get_all_dates" do
+  #   it "returns Array" do
+  #     @reservation = Reservation.new(181202, 2)
+  #     expect(@reservation.get_all_dates).must_be_kind_of Array
+  #   end
+  #   it "contains all dates for reservation" do
+  #     @reservation = Reservation.new(181202, 2)
+  #     expect(@reservation.get_all_dates.length).must_equal 2
+  #     expect(@reservation.get_all_dates[0]).must_be_kind_of Date
+  #     expect(@reservation.get_all_dates[1]).must_be_kind_of Date
+  #   end
+  # end
 end
