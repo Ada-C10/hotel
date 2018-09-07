6d00fac991944f2265b68064af3e0a7eb9b91542
Katie Jahanmir <katietj@gmail.com>
2018-09-07 15:21:37 -0700

    created tests for block intialization

--- lib/block.rb
+++ lib/block.rb
@@ -4,24 +4,34 @@ require_relative 'front_desk'
 require_relative 'room'
 
 class Block
+  attr_reader :block_start, :block_end, :rate, :number_of_rooms, :block_ID
+  attr_writer :block_ID
   def initialize(block_start, block_end, rate, number_of_rooms)
-    @block_start = block_start
-    @block_end = block_end
-    @rate = rate
+    @block_start = Date.parse(block_start)
+    @block_end = Date.parse(block_end)
+    @block_rate = rate
+    @block_ID = block_id_generator
     @number_of_rooms = number_of_rooms
-    @Block_ID = #reference some random number method
-    #put standard error checker that makes sure that the number of rooms is not more than 5
+    if number_of_rooms > 5
+      raise StandardError.new "Block is limited to 5 rooms"
+    end
+  end
+
+  def block_id_generator
+    letters = ('A'..'Z').to_a.shuffle[0..1].join
+    id = letters + (0..9).to_a.shuffle[0..2].join
+    return id
+  end
 end
 
-#In front_desk, create a block! gotta make all these rooms unavailable too.
-# Do I make the booker pick all rooms, probably the easier way to go right now, but actually
-# it might end up being harder to do....would have to take in user input or something...
-#   or do I not even have to think about that??? Like if I just take a array of them, that works
-#     logically and someone else can worry about the user interface....
-#  And then there is a seperate method for booking a room from
-# #a block...this will side step the issues of having to beef up the method for booking a regular room
-# # when you book a room from a block, you must have the ID for hte block to continue. Kinda like a
-# #password for a presale or something. The next part would be to keep track of how many rooms are booked from
-# #the block, so if 5 are blocked and they try to do the 6th, there is not anything left for them and this should raise an error.
-#Should go back and fix rooms and their cost calculations before going any futher with this, also
-#check ALL the tests, because this will fuck them all up
+  #In front_desk, create a block! gotta make all these rooms unavailable too.
+  # Do I make the booker pick all rooms, probably the easier way to go right now, but actually
+  # it might end up being harder to do....would have to take in user input or something...
+  #   or do I not even have to think about that??? Like if I just take a array of them, that works
+  #     logically and someone else can worry about the user interface....
+  #  And then there is a seperate method for booking a room from
+  # #a block...this will side step the issues of having to beef up the method for booking a regular room
+  # # when you book a room from a block, you must have the ID for hte block to continue. Kinda like a
+  # #password for a presale or something. The next part would be to keep track of how many rooms are booked from
+  # #the block, so if 5 are blocked and they try to do the 6th, there is not anything left for them and this should raise an error.
+  #maybe instead of changing the room cost i can have a multipler that is used on the block room reservations
--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -2,5 +2,18 @@ require_relative 'spec_helper'
 require 'date'
 
 describe "Block instatiation" do
+  before do
+    @new_block = Block.new("2012-10-5", "2012-10-7", 100, 4)
+    NAME_REGEXP = /^[A-Z]{2}\d{3}$/
+  end
+  it "returns an instance of Block" do
+    expect(@new_block).must_be_instance_of Block
+  end
+  it "raises an error if more than 5 rooms are booked" do
+    expect{@new_block2 = Block.new("2012-10-5", "2012-10-7", 100, 10)}.must_raise StandardError
+  end
+  it "creates a randomized id matching a regex" do
+    expect(@new_block.block_ID).must_match NAME_REGEXP
+  end
 
 end
