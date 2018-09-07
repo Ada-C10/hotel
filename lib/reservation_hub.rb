276c23e9ee456a3ee562c1178e9a33ecf96f5420
Lindsay Terchin <lterchin@gmail.com>
2018-09-07 15:45:38 -0700

    created method to assign an available room to a reservation

--- lib/reservation_hub.rb
+++ lib/reservation_hub.rb
@@ -22,7 +22,7 @@ module Hotel
 
       end_date = generate_date(end_year, end_month, end_day)
 
-      room_id = check_available_rooms(start_date, end_date)
+      room_id = assign_room(start_date, end_date)
 
       reservation = Reservation.new(start_date, end_date, room_id)
 
@@ -49,27 +49,55 @@ module Hotel
     def check_available_rooms(start_date, end_date)
       reservation_dates = create_date_array(start_date, end_date)
 
-      @room_bookings.each do |room|
+      available_rooms = []
+
+      @room_bookings.each do |room, dates|
         reservation_dates.each do |date|
           # binding.pry
 
-          if room.include?(date)
+          if @room_bookings[room].include?(date)
             break
           end
         end
+        # binding.pry
 
-        room_id = assign_room(room, reservation_dates)
-        return room_id
+        # room_id = assign_room(room, reservation_dates)
+        # return room_id
+        available_rooms << room
       end
-
-      return "I'm sorry but there are no rooms available for your dates."
+      #
+      # return "I'm sorry but there are no rooms available for your dates."
+      return available_rooms
     end
 
 
-    def assign_room(room_id, reservation_dates)
-      @room_bookings[room_id] << reservation_dates
+    def assign_room(start_date, end_date)
+
+      available_rooms = check_available_rooms(start_date, end_date)
+
+      return "I'm sorry but there are no rooms available for your dates" if available_rooms.length == 0
+
+      reservation_dates = create_date_array(start_date, end_date)
+
+      room_id = available_rooms[0]
+
+      reservation_dates.each{|date| @room_bookings[room_id] << date}
+
+      # @room_bookings[room_id] << reservation_dates
+      # binding.pry
       return room_id
     end
+
+
+
+# hash1 = {1 => ["hi", "bye"]}
+#
+# array = ["hello", "goodbye"]
+#
+# array.each{|x| hash1[1] << x}
+# hash1 => {1=>["hi", "bye", "hello", "goodbye"]}
+
+
 
 
     def find_reservations(year, month, day)
