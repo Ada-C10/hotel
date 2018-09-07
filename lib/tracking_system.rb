7a7a95760826fec99160e9e27d1b9d06191f4068
Kay <kayxn23@gmail.com>
2018-09-07 12:07:34 -0700

    added view_available_rooms_on method in TrackerSystem, and created two tests, all tests passing

--- lib/tracking_system.rb
+++ lib/tracking_system.rb
@@ -1,5 +1,6 @@
 require_relative 'room'
 require_relative 'reservation'
+require 'pry'
 
 NUMBER_OF_ROOMS = 20
 STANDARD_ROOM_PRICE = 200.0
@@ -29,26 +30,40 @@ class TrackingSystem
 
   # reserve an available room for a given date range
   # ******************************************************************************************
+  # def overlap?(x,y)
+  #   (x.first - y.end) * (y.first - x.end) > 0
+  # end
+
+  def ranges_overlap?(r1, r2)
+    r1.include?(r2.first) || r2.include?(r1.first)
+  end
+
+  # def reserve(start_time: start_time, end_time: end_time, num_of_rooms: num)
+  #
+  # end
+
   def make_reservation(start_time, end_time, num)
     @all_rooms.each do |room|
-      if room.reserved_dates.empty?
-         reservation = Reservation.new({rooms: [room.room_num], start_time: start_time, end_time: end_time, price: 200.0})
-         @reservations << reservation
-        room.reserved_dates << {start_time: start_time, end_time: end_time}
-      else
-        room.reserved_dates.each do |reserved_dates|
-          if !(reserved_dates[:start_time]..reserved_dates[:end_time]).include?(start_time) #&& room.block == :NA
-            reservation = Reservation.new({rooms: [room], start_time: start_time, end_time: end_time, price: 200.0})
-            @reservations << reservation
-            room.reserved_dates << {start_time: start_time, end_time: end_time}
-          else raise ArgumentError.new"There is no room available on that date, out of all 20 rooms"
+      num.times do
+        if room.reserved_dates.empty?
+          reservation = Reservation.new({rooms: [room.room_num], start_time: start_time, end_time: end_time, price: 200.0})
+          @reservations << reservation
+          room.reserved_dates << {start_time: start_time, end_time: end_time}
+        else
+          room.reserved_dates.each do |reserved_dates|
+            if !(reserved_dates[:start_time]...reserved_dates[:end_time]).include?(start_time) #&& room.block == :NA
+              reservation = Reservation.new({rooms: [room], start_time: start_time, end_time: end_time, price: 200.0})
+              @reservations << reservation
+              room.reserved_dates << {start_time: start_time, end_time: end_time}
+            else raise ArgumentError.new"There is no room available on that date, out of all 20 rooms"
+            end
           end
         end
+        return reservation
       end
-      return reservation
     end
   end
-# *********************************************************************************************************
+  # *********************************************************************************************************
   #block method idea
   #this method below is going to return a value that can into the Block.rooms << (aka list of rooms in a block), but how/when do i assign these available rooms a block id? Ah i think that i will
   # i will create another method called create_block, call view_available_rooms_on(start_time, end_time) within that
@@ -56,45 +71,52 @@ class TrackingSystem
   #
   # view a list of rooms that are not reserved(aka available) for a given date range
 
-  # def view_available_rooms_on(start_time, end_time)
-  #   #available_rooms = []
-  #   # @all_rooms.each do |room|
-  #   # room.reserved_dates.each do |date_range| <---date_range could be a hash like {checkin_time: checkin, checkout_time: checkout}
-  #   ###  if the date_range doesn't include date aka:
-  #   # if !date_range.include?(date)
-  #   # available_rooms << room
-  #   # end
-  #   # return available_rooms (this is an array of rooms avail on 'date' passed as param)
-  #
+  def view_available_rooms_on(start_time: start_time, end_time: end_time)
+    raise ArgumentError.new"start_time must be before end_time" unless start_time < end_time
+    available_rooms = []
+    @all_rooms.each do |room|
+      room.reserved_dates.each do |dates_hash| #<---date_range could be a hash like {checkin_time: checkin, checkout_time: checkout}
+        if ranges_overlap?((dates_hash[:start_time]...dates_hash[:end_time]).to_a, (start_time..end_time).to_a) == false
+          available_rooms << room
+        end
+        binding.pry
 
-  # #access the list of reservations for a specific date
-  # def view_reservations_on(date)
-  #   # list_of_res_on this date ^ above = []<--create emtpy array
-  #   #@reservations.each do |reservation|
-  #   #if (reservation.checkin_time..reservation.checkout_time).include?(date)
-  #   #then list_of_res_on (date) << reservation
-  #   #return the array  list_of_res_on (date)
-  #   # end
-  #
+      end
+      return available_rooms
+    end
+  end
+end
+# if !(dates_hash[:start_time]...dates_hash[:end_time]).include
+# if !dates_hash.include?(date)
+#   # available_rooms << room
+#   # end
+#   # return available_rooms (this is an array of rooms avail on 'date' passed as param)
+# def ranges_overlap?(r1, r2)
+#   r1.include?(r2.first) || r2.include?(r1.first)
+# end
+#
+
+# #access the list of reservations for a specific date
+# def view_reservations_on(date)
+#   # list_of_res_on this date ^ above = []<--create emtpy array
+#   #@reservations.each do |reservation|
+#   #if (reservation.checkin_time..reservation.checkout_time).include?(date)
+#   #then list_of_res_on (date) << reservation
+#   #return the array  list_of_res_on (date)
+#   # end
+#
 
 #
 
-  #
-  # private #helper methods below
-  # def view_two_dates_as_range()  #<--put params {checkin_time: checkin, checkout_time: checkout}
-  # def check_if_rooms_available_on(date_range)
+#
+# private #helper methods below
+# def view_two_dates_as_range()  #<--put params {checkin_time: checkin, checkout_time: checkout}
+# def check_if_rooms_available_on(date_range)
 
-  #############################  #############################   #############################
-end
+#############################  #############################   #############################
+
 #think about how each room can be reserved thru time
 
-
-#reservation attributes
-# @room_num = input[:room_num]
-# @checkin_time = input[:checkin_time]
-# @checkout_time = input[:checkout_time]
-# @price = input[:price]
-# @customer = input[:customer]
 
 
 # The hotel has 20 rooms, and they are numbered 1 through 20
--- spec/tracking_system_spec.rb
+++ spec/tracking_system_spec.rb
@@ -19,118 +19,146 @@ describe 'TrackingSystem class' do
   describe "#initialize" do
     before do
       @tracker = TrackingSystem.new
+      # @tracker.all_rooms.each do |room| room.reserved_dates << {checkin_time: Date.new(2018,8,1), checkout_time: Date.new(2018,8,5)}
+      end
+
+      it 'is an instance of TrackingSystem' do
+        expect(@tracker).must_be_kind_of TrackingSystem
+      end
+
+      it 'has a list of all rooms' do
+        expect(@tracker.all_rooms).must_be_instance_of Array
+      end
+
+      it 'has a list of reservations' do
+        expect(@tracker.reservations).must_be_instance_of Array
+      end
+
     end
 
-    it 'is an instance of TrackingSystem' do
-      expect(@tracker).must_be_kind_of TrackingSystem
+    describe "access the list of all 20 rooms in the hotel" do
+      before do
+        @tracker = TrackingSystem.new
+      end
+
+      it 'must return an Array' do
+        expect(@tracker.all_rooms).must_be_instance_of Array
+      end
+
+      it 'must contain 20 elements' do
+        expect(@tracker.all_rooms.length).must_equal 20
+      end
+
+      it 'must contain only instances of Room' do
+        expect(@tracker.all_rooms.all? {|room| room.class == Room}).must_equal true
+      end
     end
 
-    it 'has a list of all rooms' do
-      expect(@tracker.all_rooms).must_be_instance_of Array
+    # #method for making a reservation
+    # # Admin can reserve a room for a given date range
+    describe "#make_reservation" do
+      before do
+        @tracker = TrackingSystem.new
+        # @room = Room.new({room_num: 1, availability: :available })
+      end
+
+      it "creates a new instance of Reservation" do
+        @reservation = @tracker.make_reservation(Date.new(2018,8,1), Date.new(2018,8,25), 1) #<---1 is the number of rooms
+        # binding.pry
+        expect(@reservation).must_be_kind_of Reservation
+      end
+
+      # it "increases the number of reservations in the reservations list" do
+      #   num_of_reservations = @tracker.reservations.length
+      #   @reservation = @tracker.make_reservation(Date.new(2018,8,1),Date.new(2018,8,25),1)
+      #   @reservation2 = @tracker.make_reservation(Date.new(2018,8,1),Date.new(2018,8,25),1)
+      #   updated_num_of_reservations = @tracker.reservations.length
+      #   binding.pry
+      #   expect(updated_num_of_reservations - num_of_reservations).must_equal 1
+      # end
+
+      # it "changes block_status of the reserved room to :RESERVED for a given date range" do
+      # end
+      # it "saves the checkout_time as a day before " do
+      # end
+      # it "adds a hash of start/end time to each room.reserved_dates array" do
+      # end
+
+      # it "only selects rooms that are available  " do
+      #   expect(@reservation.)
+      # end
+      #
+      # it "creates reservations for the number_of_rooms requested " do
+      # end
+      #
+      # it "iterates through @all_rooms to find the first available rooms" do
+      # end
+      #
+      # it "raises ArgumentError if inside @all_rooms no room is available on this date range" do
+      # end
+      #
+      # it "creates a new instance of Reservation" do
+      #   @reservation = @tracker.make_reservation(start_time: Date.new(2018,8,1), end_time: Date.new(2018,8,25), rooms: 1) #<---1 is the number of rooms
+      #
+      #   expect(@reservation).must_be_kind_of Reservation
+      # end
+
     end
 
-    it 'has a list of reservations' do
-      expect(@tracker.reservations).must_be_instance_of Array
-    end
-
-  end
-
-  describe "access the list of all 20 rooms in the hotel" do
-    before do
-      @tracker = TrackingSystem.new
-    end
-
-    it 'must return an Array' do
-      expect(@tracker.all_rooms).must_be_instance_of Array
-    end
-
-    it 'must contain 20 elements' do
-      expect(@tracker.all_rooms.length).must_equal 20
-    end
-
-    it 'must contain only instances of Room' do
-      expect(@tracker.all_rooms.all? {|room| room.class == Room}).must_equal true
-    end
-  end
-
-  # #method for making a reservation
-  # # Admin can reserve a room for a given date range
-  describe "#make_reservation" do
-    before do
-      @tracker = TrackingSystem.new
-      # @room = Room.new({room_num: 1, availability: :available })
-    end
-
-    it "creates a new instance of Reservation" do
-      @reservation = @tracker.make_reservation(Date.new(2018,8,1), Date.new(2018,8,25), 1) #<---1 is the number of rooms
-# binding.pry
-      expect(@reservation).must_be_kind_of Reservation
-    end
-
-    it "increases the number of reservations in the reservations list" do
-      num_of_reservations = @tracker.reservations.length
-      @reservation = @tracker.make_reservation(Date.new(2018,8,1),Date.new(2018,8,25),1)
-      updated_num_of_reservations = @tracker.reservations.length
-
-      expect(updated_num_of_reservations - num_of_reservations).must_equal 1
-    end
-
-    # it "changes block_status of the reserved room to :RESERVED for a given date range" do
+    # def make_reservation(start_time: nil, end_time: nil, number_of_rooms)
+    #   # 1. pick a room that is available. the next available room?
+    #   @all_rooms.each do |room|
+    #     room.reserved_dates.each do |reserved_dates| # this is a hash containing {checkin_time: checkin, checkout_time: checkout}, can replace some fo this with helper method
+    #       if !(reserved_dates[:start_time]..reserved_dates[:end_time]).include?(:checkin) && room.block == :NA
+    #         #create a new reservation with |room| that is iterated in and fits the reqs
+    #       else
+    #        # raise argument error if inside @all_rooms no room is available on this date range (then admin would need to input a new date range)
+    #   # else..make the new reservation below
+    #   reservation = Reservation.new(date_range: view_two_dates_as_range {start_time: checkin, end_time: checkout -1}, room: room)  # <----room object contains {room_num:, price: STANDARD_ROOM_PRICE, customer: ""}
+    #   @reservations << reservation
+    #   room.reserved_dates << {start_time: checkin, end_time: checkout}
     # end
-    # it "saves the checkout_time as a day before " do
-    # end
-    # it "adds a hash of start/end time to each room.reserved_dates array" do
-    # end
-
-    # it "only selects rooms that are available  " do
-    #   expect(@reservation.)
-    # end
+    # def view_available_rooms_on(start_time, end_time)
+    #   #available_rooms = []
+    #   # @all_rooms.each do |room|
+    #   # room.reserved_dates.each do |date_range| <---date_range could be a hash like {checkin_time: checkin, checkout_time: checkout}
+    #   ###  if the date_range doesn't include date aka:
+    #   # if !date_range.include?(date)
+    #   # available_rooms << room
+    #   # end
+    #   # return available_rooms (this is an array of rooms avail on 'date' passed as param)
     #
-    # it "creates reservations for the number_of_rooms requested " do
-    # end
-    #
-    # it "iterates through @all_rooms to find the first available rooms" do
-    # end
-    #
-    # it "raises ArgumentError if inside @all_rooms no room is available on this date range" do
-    # end
-    #
-    # it "creates a new instance of Reservation" do
-    #   @reservation = @tracker.make_reservation(start_time: Date.new(2018,8,1), end_time: Date.new(2018,8,25), rooms: 1) #<---1 is the number of rooms
-    #
-    #   expect(@reservation).must_be_kind_of Reservation
-    # end
+    describe "#view_available_rooms_on" do
+      before do
+      end
 
-  end
+      it "returns an array" do
+        @tracker = TrackingSystem.new
+        @available_rooms = @tracker.view_available_rooms_on(start_time: Date.new(2018,8,1),end_time:Date.new(2018,9,5))
+        expect(@available_rooms).must_be_kind_of Array
+      end
 
-  # def make_reservation(start_time: nil, end_time: nil, number_of_rooms)
-  #   # 1. pick a room that is available. the next available room?
-  #   @all_rooms.each do |room|
-  #     room.reserved_dates.each do |reserved_dates| # this is a hash containing {checkin_time: checkin, checkout_time: checkout}, can replace some fo this with helper method
-  #       if !(reserved_dates[:start_time]..reserved_dates[:end_time]).include?(:checkin) && room.block == :NA
-  #         #create a new reservation with |room| that is iterated in and fits the reqs
-  #       else
-  #        # raise argument error if inside @all_rooms no room is available on this date range (then admin would need to input a new date range)
-  #   # else..make the new reservation below
-  #   reservation = Reservation.new(date_range: view_two_dates_as_range {start_time: checkin, end_time: checkout -1}, room: room)  # <----room object contains {room_num:, price: STANDARD_ROOM_PRICE, customer: ""}
-  #   @reservations << reservation
-  #   room.reserved_dates << {start_time: checkin, end_time: checkout}
-  # end
+      it "raises ArgumentError if start_time is > end_time" do
+        @tracker = TrackingSystem.new
+        expect{@tracker.view_available_rooms_on(start_time: Date.new(2018,10,5),end_time:Date.new(2018,9,5))}.must_raise ArgumentError
+      end
 
-
-
-
-
-
-  describe "#view_all_rooms" do
-    before do
-      @tracker = TrackingSystem.new
     end
 
-    it "returns a list" do
-      expect(@tracker.view_all_rooms).must_be_kind_of Array
+
+
+
+
+
+    describe "#view_all_rooms" do
+      before do
+        @tracker = TrackingSystem.new
+      end
+
+      it "returns a list" do
+        expect(@tracker.view_all_rooms).must_be_kind_of Array
+      end
+
     end
 
-  end
-
-end #end of class method
+  end #end of class method
