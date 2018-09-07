1b5b5cad196a7fe7ed691b319727dd49df192a77
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-07 15:57:38 -0700

    Begin creating Block class for wave 3

---
+++ spec/block_spec.rb
@@ -0,0 +1,4 @@
+require_relative 'spec_helper'
+
+describe "Block class" do
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative '../lib/reservation'
 require_relative '../lib/reservation_tracker'
 require_relative '../lib/date_range'
+require_relative '../lib/block'
