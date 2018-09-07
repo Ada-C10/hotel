646ee8875b60021c970c963acf50e53c9abe191e
Kat Perreira <kat.perreira@gmail.com>
2018-09-06 18:05:07 -0700

    in constructor, created rooms to be a hash with number and boolean for avaliability

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -1,9 +1,22 @@
+require_relative 'room.rb'
+require_relative 'reservation.rb'
+
 
 class BookingManager
-attr_reader :reservations
 
-def initialize
-  @reservations = []
-end
+  attr_reader :rooms
+  attr_reader :reservations
+
+  def initialize
+    @reservations = []
+    @rooms = []
+
+    i = 1
+    20.times do |i|
+      @rooms << {number: i, availiable: true}
+    end
+  end
+
+
 
 end
