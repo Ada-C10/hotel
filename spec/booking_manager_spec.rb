3de7cb5cdee48356742fc7bb4721397cd733e5bb
Kat Perreira <kat.perreira@gmail.com>
2018-09-06 18:03:55 -0700

    tested for rooms array to be hashes

--- spec/booking_manager_spec.rb
+++ spec/booking_manager_spec.rb
@@ -1,32 +1,38 @@
 require_relative 'spec_helper.rb'
 
 describe 'BookingManager' do
-  let (:reservation_manager) {
+
+  let (:sample_booking) {
     BookingManager.new
   }
 
-  let (:check_in1) {
-    Date.new(2015, 1, 1)
+  let (:sample_checkin) {
+    Date.new(2001,3,1)
   }
 
-  let (:check_out1){
-    Date.new(2015, 1, 7)
-  }
-  let (:reservation1) {
-    Reservation.new(5, check_in1, check_out1)
+  let (:sample_checkout) {
+    Date.new(2001,3,5)
   }
 
   describe '#initialize' do
 
-    it 'Creates an empty Array at start for Reservations to live in' do
-      expect(reservation_manager.reservations).must_be_kind_of Array
-    end
+    it 'must create an array of 20 room hashes when created' do
+      expect(sample_booking.rooms).must_be_kind_of  Array
+      expect(sample_booking.rooms.length).must_equal 20
+      expect(sample_booking.rooms[0]).must_be_kind_of Hash
 
+    end
   end
 
-  # describe '#make_reservation' do
-  #   it 'creates a new reservation object and pushes it into the reservations array'
-  #   expect(reservation_manager.make_reservation(5, check_in1, checkout1))
+  # describe "#book_reservation" do
+  #   # check info within the reservations array
+  #
+  #   it 'must fill the reservations array with stuff' do
+  #     expect(sample_booking.reservations).must_equal 4 # 4 nithgs from 1st to 5th
+  #   end
+  #
+  #   it 'must contain hashes' do
+  #     expect(sample_booking.reservations[0]).must_be_kind_of Hash
   # end
 
 end
