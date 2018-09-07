9369c7706b677126c7333a4fbaf6d710c281d680
Katricia Smith <smithkatricia@gmail.com>
2018-09-07 16:35:34 -0700

    Added Reservation#check_dates method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,11 +6,18 @@ module Hotel
     def initialize(room, guest_name:, start_date:, end_date:, cost_per_night: 200.00)
       @room = room.number
       @guest_name = guest_name
+      check_dates(start_date, end_date)
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
       @cost_per_night = cost_per_night
-      @number_nights = (@end_date - @start_date).to_i
+      @number_nights = (@end_date - @start_date).to_i 
     end # of initialize method
+
+    def check_dates(start_date, end_date)
+      if start_date > end_date
+        raise ArgumentError.new "Invalid date range. Start date must be before end date."
+      end
+    end
 
   end# of class Reservation
 end #of Hotel module
