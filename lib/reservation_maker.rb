4191fa47cec45c78f33add141c2b058d347ad320
Carly Reiter <yepodacarly@gmail.com>
2018-09-07 14:43:54 -0700

    Added a self.reset method and wrapped the reservation_maker_spec in a before block calling that method, per Dan's suggestion.

--- lib/reservation_maker.rb
+++ lib/reservation_maker.rb
@@ -20,43 +20,45 @@ class ReservationMaker
     @@reservations
   end
 
+  def self.reset
+    @@reservations = []
+  end
+
   def rooms
     @rooms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     return @rooms
   end
 
   def booked_rooms
-    @booked_rooms = []
+    booked_rooms = []
     @@reservations.each do |reservation|
       if reservation.start_date >= @start_date && reservation.end_date < @end_date || reservation.end_date - 1 >= @start_date
-        @booked_rooms << reservation.room
+        booked_rooms << reservation.room
       end
     end
     # binding.pry
-    return @booked_rooms
+    return booked_rooms
   end
 
   def available_rooms
-    @available_rooms = []
+    available_rooms = []
+    booked_room_list = booked_rooms
     @rooms.each do |room|
-      if @booked_room.exclude?(room)
-        @available_rooms << room
+      if booked_room_list.exclude?(room)
+        available_rooms << room
       end
-      return @available_rooms
+      return available_rooms
     end
   end
-
 
   def create_reservation
-    if @available_rooms.length == 0
-      return "There are no available rooms for the given dates."
-    else
-      new_reservation = Reservation.new(@start_date, @end_date, @available_rooms.sample)
-      @@reservations << new_reservation
+    room = available_rooms.sample
+    if room.nil?
+      raise
     end
-      # binding.pry
-    return new_reservation
-    # binding.pry
+    new_reservation = Reservation.new(@start_date, @end_date, room)
+    @@reservations << new_reservation
+  return new_reservation
+  # binding.pry
   end
-
 end
--- spec/reservation_maker_spec.rb
+++ spec/reservation_maker_spec.rb
@@ -2,76 +2,82 @@ require_relative 'spec_helper'
 require 'pry'
 require 'date'
 
-describe "tests for various pieces in each incidence of ReservationMaker" do
-
-  it "is an instance of ReservationMaker" do
-    start_date = Date.new(2018,2,3)
-    end_date = Date.new(2018,2,5)
-    @example = ReservationMaker.new(start_date, end_date)
-    expect(@example).must_be_kind_of ReservationMaker
+describe "ReservationMaker" do
+  before do
+    ReservationMaker.reset
   end
 
-  it "is set up for specific attributes and data types" do
-    start_date = Date.new(2018,2,3)
-    end_date = Date.new(2018,2,5)
-    @example = ReservationMaker.new(start_date, end_date)
+  describe "tests for various pieces in each incidence of ReservationMaker" do
 
-    expect(@example.create_reservation).must_be_kind_of Reservation
-    expect(ReservationMaker.reservations).must_be_kind_of Array
-    expect(@example.start_date).must_be_kind_of Date
-    expect(@example.end_date).must_be_kind_of Date
-    expect(@example.rooms).must_be_kind_of Array
-    expect(@example.rooms[0]).must_be_kind_of Integer
+    it "is an instance of ReservationMaker" do
+      start_date = Date.new(2018,2,3)
+      end_date = Date.new(2018,2,5)
+      @example = ReservationMaker.new(start_date, end_date)
+      expect(@example).must_be_kind_of ReservationMaker
+    end
 
+    it "is set up for specific attributes and data types" do
+      start_date = Date.new(2018,2,3)
+      end_date = Date.new(2018,2,5)
+      @example = ReservationMaker.new(start_date, end_date)
+
+      expect(@example.create_reservation).must_be_kind_of Reservation
+      expect(ReservationMaker.reservations).must_be_kind_of Array
+      expect(@example.start_date).must_be_kind_of Date
+      expect(@example.end_date).must_be_kind_of Date
+      expect(@example.rooms).must_be_kind_of Array
+      expect(@example.rooms[0]).must_be_kind_of Integer
+
+    end
   end
-end
 
-describe "test for errors in input" do
-  it "throws an ArgumentError if the start date is after the end date" do
-    expect do
-      ReservationMaker.new(Date.new(2018, 2, 3), Date.new(2018, 2, 1))
-    end.must_raise ArgumentError
+  describe "test for errors in input" do
+    it "throws an ArgumentError if the start date is after the end date" do
+      expect do
+        ReservationMaker.new(Date.new(2018, 2, 3), Date.new(2018, 2, 1))
+      end.must_raise ArgumentError
+    end
   end
-end
 
-describe "tests for reservations being added to master reservations list" do
-  it "collates all reservations into master @@reservations array" do
+  describe "tests for reservations being added to master reservations list" do
+    it "collates all reservations into master @@reservations array" do
 
-    start_date1 = Date.new(2018,2,3)
-    end_date1 = Date.new(2018,2,5)
-    ReservationMaker.new(start_date1, end_date1).create_reservation
-    # binding.pry
+      start_date1 = Date.new(2018,2,3)
+      end_date1 = Date.new(2018,2,5)
+      ReservationMaker.new(start_date1, end_date1).create_reservation
+      # binding.pry
 
-    start_date2 = Date.new(2018,3,1)
-    end_date2 = Date.new(2018,3,2)
-    ReservationMaker.new(start_date2, end_date2).create_reservation
-    # binding.pry
+      start_date2 = Date.new(2018,3,1)
+      end_date2 = Date.new(2018,3,2)
+      ReservationMaker.new(start_date2, end_date2).create_reservation
+      # binding.pry
 
-    expect(ReservationMaker.reservations.length).must_equal 2
+      expect(ReservationMaker.reservations.length).must_equal 2
 
+    end
   end
-end
 
-describe "tests for booked rooms being added to the booked rooms list, given a specific date range" do
-  it "collates all reservations that have competing dates with the given one into master @booked_rooms array" do
+  describe "tests for booked rooms being added to the booked rooms list, given a specific date range" do
+    it "collates all reservations that have competing dates with the given one into master @booked_rooms array" do
 
-    start_date1 = Date.new(2018,2,3)
-    end_date1 = Date.new(2018,2,5)
-    ReservationMaker.new(start_date1, end_date1).create_reservation
-    # binding.pry
+      start_date1 = Date.new(2018,2,3)
+      end_date1 = Date.new(2018,2,5)
+      ReservationMaker.new(start_date1, end_date1).create_reservation
+      # binding.pry
 
-    start_date2 = Date.new(2018,3,1)
-    end_date2 = Date.new(2018,3,2)
-    ReservationMaker.new(start_date2, end_date2).create_reservation
-    # binding.pry
+      start_date2 = Date.new(2018,3,1)
+      end_date2 = Date.new(2018,3,2)
+      ReservationMaker.new(start_date2, end_date2).create_reservation
+      # binding.pry
 
-    start_date3 = Date.new(2018,3,1)
-    end_date3 = Date.new(2018,3,3)
-    example = ReservationMaker.new(start_date3, end_date3).booked_rooms
-    # binding.pry
+      start_date3 = Date.new(2018,3,1)
+      end_date3 = Date.new(2018,3,3)
+      example = ReservationMaker.new(start_date3, end_date3).booked_rooms
+      # binding.pry
 
-    expect(example.length).must_equal 1
-    expect(example[0]).must_be_kind_of Integer
+      expect(example.length).must_equal 1
+      expect(example[0]).must_be_kind_of Integer
 
+    end
   end
 end
