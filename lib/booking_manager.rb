7be6fc99064d62f6a16e7d2a0eb54b0caf7b6db8
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-07 15:44:53 -0700

    Add BookingManager# methods - print_all_rooms, reservation_list, and print_available_rooms

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -1,5 +1,29 @@
 class BookingManager
-  def initialize
-
+  attr_reader :calendar
+  def initialize(calendar)
+    @calendar = calendar
   end
+
+  def print_all_rooms
+    return calendar.room_assignments.keys
+  end
+
+  def reservation_list(date)
+    reservations = []
+    room_assignments = calendar.room_assignments
+    room_assignments.each do |k, v|
+      if v.include? (Date.parse(date))
+        reservations << k
+      end
+    end
+    return reservations
+  end
+
+  def print_available_rooms(check_in, check_out)
+    request = Reservation.new(check_in, check_out)
+    return calendar.list_available_rooms(request)
+  end
+
+
+
 end
