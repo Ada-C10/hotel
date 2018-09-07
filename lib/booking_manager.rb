2e7d00b928255f8b158b2f52c36576709922c3cc
Kat Perreira <kat.perreira@gmail.com>
2018-09-06 21:07:04 -0700

    Refactored rooms from just number to a hash, included an avaliable status set to true at initialization. Created find rooms method based on availiability

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -6,17 +6,31 @@ class BookingManager
 
   attr_reader :rooms
   attr_reader :reservations
+  @@reservations = []
 
   def initialize
-    @reservations = []
     @rooms = []
-
-    i = 1
+    i = 0
     20.times do |i|
-      @rooms << {number: i, availiable: true}
+      @rooms << {number: i+1, availiable: true}
+      i += 1
+
     end
+    #binding.pry
   end
 
-
+  def find_room
+    room_number = 237
+    searching = true
+    while searching
+      @rooms.each do |hash|
+        if hash[:availiable]
+          room_number = hash[:number]
+          return room_number
+        end
+        searching = false
+      end
+    end
+  end
 
 end
