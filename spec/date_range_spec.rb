4fccc0c7475ba3769a82b4bd7090c8b8fa3317b2
Anibel America <ameriabel@gmail.com>
2018-09-07 08:53:00 -0700

    removes redundant code

--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -7,10 +7,6 @@ describe 'DateRange class' do
     let (:today_plus_2) { today + 2}
     let (:today_minus_2) {today - 2}
     let (:range) { Hotel::DateRange.new(today, today_plus_2) }
-    # before do
-    #   today = Date.today
-    #   today_plus_2 = @check_in + 2
-    # end
 
     it 'creates an instance of DateRange' do
       expect(Hotel::DateRange.new(today, today_plus_2)).must_be_instance_of Hotel::DateRange
@@ -30,6 +26,7 @@ describe 'DateRange class' do
       expect{
         Hotel::DateRange.new("2000-10-10", today_minus_2)
       }.must_raise ArgumentError
+      
       expect{
         Hotel::DateRange.new(today, 20001010)
       }.must_raise ArgumentError
@@ -37,20 +34,17 @@ describe 'DateRange class' do
 
     describe 'get_total_days method' do
       it 'returns the number of days in the date range' do
-        check_in = Date.new(2020, 1, 1)
-        check_out = check_in + 2
-        range = Hotel::DateRange.new(check_in, check_out)
         expect(range.get_total_days).must_equal 2
       end
     end
 
     describe 'is_within_date_range' do
       it 'returns true if date is within the range' do
-        expect(range.is_within_date_range(Date.today)).must_equal true
+        expect(range.is_within_date_range(today)).must_equal true
       end
 
       it 'returns false if date is outside the range' do
-        expect(range.is_within_date_range(Date.today - 1)).must_equal false
+        expect(range.is_within_date_range(today_minus_2)).must_equal false
       end
 
       it 'returns false if given an invalid date' do
