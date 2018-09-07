015c3dbe0f08c3fc16009da1f842501486df9ecb
Kat Perreira <kat.perreira@gmail.com>
2018-09-07 11:42:16 -0700

    changed find room to include date, took out status

--- spec/booking_manager_spec.rb
+++ spec/booking_manager_spec.rb
@@ -19,25 +19,32 @@ describe 'BookingManager' do
     it 'must create an array of 20 room hashes when created' do
       expect(sample_booking.rooms).must_be_kind_of  Array
       expect(sample_booking.rooms.length).must_equal 20
-      expect(sample_booking.rooms[0]).must_be_kind_of Hash
+      expect(sample_booking.rooms[0]).must_be_kind_of Integer
 
     end
   end
 
-  describe "#find_room" do
-    # check info within the reservations array
+  describe "#find_available_room" do
     it 'must return a valid room number' do
-      expect(sample_booking.find_room).must_be_kind_of Integer# 4 nithgs from 1st to 5th
+      expect(sample_booking.find_available_room(sample_checkin, sample_checkout)).must_be_kind_of Integer
     end
 
     it 'must equal a number greater than 0' do
-      expect(sample_booking.find_room).must_be :>, 0
+      expect(sample_booking.find_available_room(sample_checkin, sample_checkout)).must_be :>, 0
     end
 
     it 'must equal a number less than 21' do
-      expect(sample_booking.find_room).must_be :<, 21
+      expect(sample_booking.find_available_room(sample_checkin, sample_checkout)).must_be :<, 21
     end
 
   end
 
+  # describe "#find_avaliable_room"
+  # it ''
+
+  # it 'returns a Reservation object' do
+  #   expect(sample_booking.make_reservation).must_be_kind_of Reservation
+  # end
+  #
+  # it ''
 end
