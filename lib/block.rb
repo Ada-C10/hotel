cb105c9a120849ff6ccfd9e72e4e289be93bacf9
Anibel America <ameriabel@gmail.com>
2018-09-07 16:55:23 -0700

    refactors Block class, adds additional spec tests

--- lib/block.rb
+++ lib/block.rb
@@ -1,31 +1,18 @@
 module Hotel
 
-  class Block #< Reservation
+  class Block
 
     attr_reader :date_range, :blocked_rooms, :discounted_rate, :block_reservations, :block_id
-    #
-    def initialize(date_range, blocked_rooms, discounted_rate, block_id)
+
+    def initialize(date_range, blocked_rooms, discounted_rate, block_id, block_reservations: [])
       @date_range = date_range
       @blocked_rooms = blocked_rooms
       @discounted_rate = discounted_rate
-      @block_reservations = []
+      @block_reservations = block_reservations
       @block_id = block_id
 
-      if @blocked_rooms.length > 5
-        raise ArgumentError, "A block can contain a maximum of 5 rooms"
-      end
-
-      if !is_number(@discounted_rate)
-        raise ArgumentError, "Discounted rate is not a valid number"
-      end
-
-      # TODO: this should be rescued?
-      if @discounted_rate > 200
-        raise ArgumentError, "Discounted rate is greater than Base rate: 200."
-      end
-
-      if @blocked_rooms.uniq.length != @blocked_rooms.length
-        raise ArgumentError, "Cannot have repeating rooms"
+      if @blocked_rooms.length > 5 || @blocked_rooms.uniq.length != @blocked_rooms.length
+        raise ArgumentError, "Blocked Rooms: #{blocked_rooms}, Invalid set of rooms"
       end
 
       @blocked_rooms.each do |room_num|
@@ -34,16 +21,22 @@ module Hotel
         end
       end
 
-      # TODO: add a block_reserved array to hotel_booker class
+      if !is_number(@discounted_rate) || @discounted_rate > 200
+        raise ArgumentError, "Rate: $#{@discounted_rate} is not a valid discounted_rate."
+      end
+
     end
 
+
     def is_number(num)
-      return num.to_f.to_s == num.to_s || num.to_i.to_s == num.to_s
+      return num.is_a? Numeric
     end
+
 
     def get_block_availability
       return list_available_block_rooms.length > 0
     end
+
 
     def list_available_block_rooms
       occupied_block_rooms = @block_reservations.map do |reservation|
@@ -52,5 +45,6 @@ module Hotel
 
       return (@blocked_rooms - occupied_block_rooms)
     end
+    
   end
 end
--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -1,27 +1,30 @@
 require_relative 'spec_helper'
 
 describe 'Block class' do
-  let (:check_in) {
-    Date.today
-  }
-  let (:check_out) {
-    Date.today + 3
-  }
-  let (:blocked_rooms) {
-    [*1..5]
-  }
-  let (:discounted_rate) {
-    150
-  }
+  let (:check_in) { Date.today }
+  let (:check_out) { Date.today + 3 }
+  let (:blocked_rooms) { [*1..5] }
+  let (:discounted_rate) { 150 }
 
-  let (:block) {
-    Hotel::Block.new(Hotel::DateRange.new(check_in, check_out), blocked_rooms, discounted_rate, 1)
-  }
+  let (:block) { Hotel::Block.new(Hotel::DateRange.new(check_in, check_out), blocked_rooms, discounted_rate, 1) }
+
+  let (:reservation) { [ Hotel::Reservation.new(Hotel::DateRange.new(check_in, check_out),1, rate: 150)] }
+
+  let (:fully_reserved) { 5.times.map { |room_num|
+    Hotel::Reservation.new(Hotel::DateRange.new(check_in, check_out), room_num + 1, rate: 150) } }
+
+  let (:block_with_res) { Hotel::Block.new(Hotel::DateRange.new(check_in, check_out), blocked_rooms, discounted_rate, 1, block_reservations: reservation ) }
+
+  let (:full_block) { Hotel::Block.new(Hotel::DateRange.new(check_in, check_out), blocked_rooms, discounted_rate, 1, block_reservations: fully_reserved ) }
 
 
   describe 'Block Instantiation' do
     it 'creates a Block object' do
       expect(block).must_be_instance_of Hotel::Block
+      expect(block).must_respond_to :date_range
+      expect(block).must_respond_to :blocked_rooms
+      expect(block).must_respond_to :discounted_rate
+      expect(block).must_respond_to :block_id
     end
 
     it 'raises an error if given more than 5 rooms' do
@@ -43,18 +46,52 @@ describe 'Block class' do
     end
   end
 
+  describe 'is_number helper method' do
+    it 'returns true if given an Integer' do
+      expect(block.is_number(1)).must_equal true
+    end
+
+    it 'returns true if given a Float' do
+      expect(block.is_number(1.0)).must_equal true
+    end
+
+    it 'returns false if given anything besides a Float or Integer' do
+      expect(block.is_number("1")).must_equal false
+      expect(block.is_number("1.0")).must_equal false
+      expect(block.is_number("AA")).must_equal false
+      expect(block.is_number(:sym)).must_equal false
+      expect(block.is_number([1])).must_equal false
+    end
+  end
+
   describe 'get_block_availability' do
     it 'returns true if block has full availability' do
       expect(block.get_block_availability).must_equal true
     end
 
-    # TODO:
-    # it 'returns true if block any availability' do
-    # skip
-    # end
+    it 'returns true if block any availability' do
+      expect(block_with_res.get_block_availability).must_equal true
+    end
 
-    # TODO:
-    # it 'returns false if block has 0 availability' do
+    it 'returns false if block has no availability' do
+      expect(full_block.get_block_availability).must_equal false
+    end
+  end
+
+  describe 'list_available_block_rooms method' do
+    it 'returns an array listing the available rooms in block' do
+      expect(block_with_res.list_available_block_rooms).must_be_instance_of Array
+      expect(block_with_res.list_available_block_rooms).must_equal [*2..5]
+    end
+
+    it 'returns an empty array [] if no rooms available' do
+      expect(full_block.list_available_block_rooms).must_equal []
+    end
+
+    it 'returns all rooms in the block if all rooms available' do
+      all_block_rooms = block.blocked_rooms
+      expect(block.list_available_block_rooms).must_equal all_block_rooms
+    end
   end
 
 end
