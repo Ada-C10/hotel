42bbd4133c1ddd7530ea12bf076ab8e3f6ac0465
Karis Kim <ouniiq@hotmail.com>
2018-09-07 10:55:03 -0700

    added tests to pass assigned room to booking method

--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -2,7 +2,9 @@ require_relative 'spec_helper'
 
 describe "BookingSystem class" do
   before do
-    @booking = Hotel::BookingSystem.new(180904)
+    @reservation = Hotel::Reservation.new({check_in_date: 180904, check_out_date: 180907})
+
+    @booking = Hotel::BookingSystem.new
   end
 
   it "initialize method" do
@@ -10,7 +12,21 @@ describe "BookingSystem class" do
   end
 
   it "returns array of all room numbers in hotel" do
-    expect(@booking.rooms_in_hotel).must_be_kind_of Array
-    expect(@booking.rooms_in_hotel).must_equal [*1..20]
+    expect(@booking.list_all_rooms_in_hotel).must_be_kind_of Array
+    expect(@booking.list_all_rooms_in_hotel).must_equal [*1..20]
+  end
+
+  it "returns all dates that have been booked" do
+    expect(@booking.new_reservation(@reservation)).must_equal [Date.parse("180904"), Date.parse("180905"), Date.parse("180906")]
+  end
+
+  it "returns array of dates with the assigned room" do
+    expect(@booking.assign_room_to_booking(@reservation)).must_equal [[Date.parse("180904"), 1], [Date.parse("180905"), 1], [Date.parse("180906"), 1]]
+  end
+
+  xit "" do
+  end
+
+  xit "" do
   end
 end
