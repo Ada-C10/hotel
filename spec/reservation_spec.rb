854fc41339091e7427d5c1b2dea589296a239bbb
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-09-07 14:48:25 -0700

    Add two missing tests to reservation_spec file

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -3,13 +3,16 @@ require 'pry'
 
 describe "Reservation" do
   let (:reservation) {
-    Reservation.new({guest_id: "SoccerMom2010@gmail.com", room: 1, date_range: (Date.new(2018,10,20)..Date.new(2018,10,22))})
+    Reservation.new({guest_id: "SoccerMom2010@gmail.com", room: 1, date_range: (Date.new(2018,10,20)...Date.new(2018,10,22))})
   }
   let (:reservation2) {
-    Reservation.new({guest_id: "Guccifer2.0@ada.com", room: 1, date_range: (Date.new(2018,12,02)..Date.new(2018,12,07))})
+    Reservation.new({guest_id: "Guccifer2.0@ada.com", room: 1, date_range: (Date.new(2018,12,02)...Date.new(2018,12,07))})
   }
   let (:reservation3) {
-    Reservation.new({guest_id: "Jfahmy07@gmail.com", room: 2, date_range: (Date.new(2018,12,03)..Date.new(2018,12,06))})
+    Reservation.new({guest_id: "Jfahmy07@gmail.com", room: 2, date_range: (Date.new(2018,12,03)...Date.new(2018,12,06))})
+  }
+  let (:reservation4) {
+    Reservation.new({guest_id: "JimmyCricket@gmail.com", room: 5, status: :block_reserved, date_range: (Date.new(2018,12,03)...Date.new(2018,12,06))})
   }
 
   describe "#initialize" do
@@ -27,7 +30,7 @@ describe "Reservation" do
     end
 
     it "returns a date range for guests stay" do
-        expect(reservation.date_range).must_equal (Date.new(2018,10,20)..Date.new(2018,10,22))
+        expect(reservation.date_range).must_equal (Date.new(2018,10,20)...Date.new(2018,10,22))
     end
 
     it "returns default nightly rate" do
@@ -40,9 +43,18 @@ describe "Reservation" do
 
   end
 
-  # describe "#initialize block reservation" do
-  #   it ""
-  #
-  # end
+  describe "Reservation#stay_cost" do
+    it "will calculate cost of guest's stay" do
+      expect(reservation2.stay_cost).must_equal 1000.00
+    end
+  end
+
+  describe "Reservation#complete_reservation" do
+    it "will set a reservations status to complete" do
+      reservation4.complete_reservation
+      expect(reservation4.status).must_equal :complete
+    end
+  end
+
 
 end
