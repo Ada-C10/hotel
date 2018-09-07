8c0ed9e3b24bf6efe452d8d8dde7086eb1dc1a86
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-06 23:00:05 -0700

    Update spec_helper.rb to include new reservation_tracker and room class files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
 require_relative '../lib/booking_tracker'
+require_relative '../lib/reservation_tracker'
 require_relative '../lib/reservation'
+require_relative '../lib/room'
