1e04809d26b61dbab7538d151d033fce055953cc
Karis Kim <ouniiq@hotmail.com>
2018-09-07 11:14:59 -0700

    created test for checking existence of booked_dates and for creating the booking system class

--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -7,8 +7,12 @@ describe "BookingSystem class" do
     @booking = Hotel::BookingSystem.new
   end
 
-  it "initialize method" do
+  it "create BookingSystem class" do
     expect(@booking).must_be_kind_of Hotel::BookingSystem
+  end
+
+  it "initialize method" do
+    expect(@booking.booked_dates).must_be_kind_of Array
   end
 
   it "returns array of all room numbers in hotel" do
@@ -16,13 +20,8 @@ describe "BookingSystem class" do
     expect(@booking.list_all_rooms_in_hotel).must_equal [*1..20]
   end
 
-  it "returns all dates that have been booked" do
-    expect(@booking.list_all_rooms_in_hotel).must_be_kind_of Array
-    expect(@booking.new_reservation(@reservation)).must_equal [Date.parse("180904"), Date.parse("180905"), Date.parse("180906")]
-  end
-
   it "returns array of dates with the assigned room" do
-    expect(@booking.list_all_rooms_in_hotel).must_be_kind_of Array
+    expect(@booking.assign_room_to_booking(@reservation)).must_be_kind_of Array
     expect(@booking.assign_room_to_booking(@reservation)).must_equal [[Date.parse("180904"), 1], [Date.parse("180905"), 1], [Date.parse("180906"), 1]]
   end
 
