69dd359e0dc679db0c4a33c15389744a83056152
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-07 10:57:22 -0700

    Add test for Dates class.

---
+++ spec/dates_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'spec_helper'
+
+describe "Dates class" do
+  before do
+    @date = '9-10-2018'
+    @date_range = {begin_date: '9-10-2018', end_date: '10-10-2018'}
+  end
+
+  it "changes string date into instance of Date class" do
+    expect (Dates::date_format(@date)).must_be_kind_of Date
+  end
+
+  it "takes in begin_date and end_date and creates instances of Date classes of each" do
+    date_range_transformed = Dates::date_range_format(@date_range)
+    expect (date_range_transformed[:begin_date]).must_be_kind_of Date
+    expect (date_range_transformed[:end_date]).must_be_kind_of Date
+  end
+end
