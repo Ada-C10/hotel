aa20313173476186d2b2bbb4626cb9cd0f0ad6be
Katricia Smith <smithkatricia@gmail.com>
2018-09-07 11:56:20 -0700

    Added @number_nights to constructor of class Reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,6 +1,6 @@
 module Hotel
   class Reservation
-    attr_reader :room
+    attr_reader :room, :number_nights
     attr_accessor :guest_name, :start_date, :end_date, :cost_per_night
 
     def initialize(room, guest_name:, start_date:, end_date:, cost_per_night: 200.00)
@@ -9,6 +9,7 @@ module Hotel
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
       @cost_per_night = cost_per_night
+      @number_nights = @end_date - @start_date
     end # of initialize method
 
   end# of class Reservation
