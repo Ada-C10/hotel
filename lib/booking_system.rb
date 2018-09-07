b0b0f52d526d2814474c39e9fb270ca0efbae85c
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-09-07 10:44:22 -0700

    moved all reservation queries to BookingSystem class and test files to booking_system_spec - tests pass

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,8 +1,51 @@
 module Hotel
   class BookingSystem
-    def find_reservations(search_date)
-
-
+    # method to find a reservation based on an inspect date 'insp_date'
+    def self.find_reservations_by_date(inspect_date)
+      # standardize input date
+      search_date = Date.parse(inspect_date)
+      # select all reservation instances that have booked dates that match
+      # the inspect date
+      found_reservations = Hotel::Reservation.load_reservations.select do |reservation|
+        reservation.booked_dates.include? search_date.to_s
+      end
+      found_reservations = 0 if found_reservations.empty?
+      return found_reservations
     end
-  end
-end
+
+    # method to find reservation based on id
+    def self.find_a_reservation(id)
+      reservation = Hotel::Reservation.load_reservations.find { |rsv| rsv.id == id }
+      raise ArgumentError, 'ID does not exist' if reservation.nil?
+      return reservation
+    end
+
+    # method to return total cost based on reservation id
+    def self.get_reservation_total(id)
+      rsv = find_a_reservation(id)
+      total = rsv.total_cost
+      return total
+    end
+
+
+    def self.find_available_rooms(inspect_date)
+      # load reservations
+      rooms = find_reservations_by_date(inspect_date)
+      # all_rooms = Room.all
+      if rooms == 0
+        avail_rooms = (Room.all).map { |room| room.id}
+      else
+        # map booked rooms to new array - flatten it
+        booked = (rooms.map {|rsv| rsv.included_rooms }).flatten
+        # binding.pry
+        # search all room instances for rooms that don't match
+        rooms_arr = (Room.all).map { |room| room.id}
+        avail_rooms = rooms_arr - booked
+      end
+      # binding.pry
+      return avail_rooms
+    end
+
+
+  end # class
+end # module
--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -7,14 +7,47 @@ describe 'BookingSystem' do
     rs3 = Hotel::Reservation.new(104, "Dolly Raj", [4, 7, 8], "May 12, 2019", "May 13, 2019")
   end
 
-  it 'can load existing reservations' do
-    # expect(Hotel::BookingSystem.load_reservations).must_be_kind_of Array
-  end
-  it 'can show available rooms' do
+  describe 'it can find reservation data' do
+    let(:finder) do
+      find_date = "October 22 2019"
+      Hotel::BookingSystem.find_reservations_by_date(find_date)
+    end
+    it 'given a date, can find a reservation' do
+      expect((finder).length).must_equal 3
+    end
+    it 'returns an array of reservations on that date' do
+      expect(finder).must_be_kind_of Array
+    end
+    it 'returns an array of reservation instances' do
+      expect(finder[0]).must_be_kind_of Hotel::Reservation
+    end
+    it 'returns 0 if no reservations found on that day' do
+      expect(Hotel::BookingSystem.find_reservations_by_date("May 4 2020")).must_equal 0
+    end
+    it 'can find a reservation given an id' do
+      id = 1
+      expect(Hotel::BookingSystem.find_a_reservation(id)).must_be_kind_of Hotel::Reservation
+    end
+    it 'raises an argument error if no reservation are found w/ that id' do
+      id = 9999
+      expect{ Hotel::BookingSystem.find_a_reservation(id) }.must_raise ArgumentError
+    end
+    it 'can get total cost given a reservation id' do
+      id = 2 # 1 room, 1 night, baserate 200.00
+      expect(Hotel::BookingSystem.get_reservation_total(id)).must_equal 1 * 1 * 200.00
+      expect(Hotel::BookingSystem.get_reservation_total(id)).must_be_kind_of Float
+    end
+    it 'can find available rooms given a date' do
+      find_date = "October 23 2019"
+      expect(Hotel::BookingSystem.find_available_rooms(find_date)).must_be_kind_of Array
+      expect((Hotel::BookingSystem.find_available_rooms(find_date)).length).must_equal 18
+      find_date2 = "March 15 2020"
+      expect((Hotel::BookingSystem.find_available_rooms(find_date2)).length).must_equal 20
+    end
 
 
   end
-  it 'can make a reservation' do
-    # expect(Hotel::BookingSystem.new_reservation).must_be_kind_of Hotel::Reservation
-  end
+  # it 'can make a reservation' do
+  #   # expect(Hotel::BookingSystem.new_reservation).must_be_kind_of Hotel::BookingSystem
+  # end
 end
