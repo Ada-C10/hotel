e5d271707db6d47514e990457bc0498e5a54812e
Addison Cole <addison.cole85@gmail.com>
2018-09-07 21:21:42 -0700

    remove status from room class, not needed

--- lib/room.rb
+++ lib/room.rb
@@ -2,12 +2,9 @@ module Hotel
   class Room
     attr_reader :room_number, :reservations, :status
 
-    ROOM_STATUS = [:VACANT, :OCCUPIED, :BOOKED]
-
     def initialize(room_number)
       @room_number = room_number
       @reservations = []
-      @status = :VACANT
     end
 
   end
