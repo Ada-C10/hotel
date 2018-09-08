12f0707369d1ed58434af498d38b95e2e0a9dd48
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-08 10:33:17 -0700

    Write tests when calculating cost for unique rates entered as well as floats

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -30,6 +30,38 @@ describe "Reservation class" do
       expect(@reservation.calculate_cost).must_equal REG_RATE * total_nights_stayed
     end
 
+    it "calculates the cost with a unique rate assigned to the room" do
+      room = Hotel::Room.new(room_num: 1, rate: 150)
+
+      reservation_data = {
+        id: 8,
+        room: room,
+        date_range: @date_range
+      }
+
+      reservation = Hotel::Reservation.new(reservation_data)
+      total_nights_stayed = @date_range.find_num_nights
+
+      expect(reservation.calculate_cost).must_be_kind_of Numeric
+      expect(reservation.calculate_cost).must_equal room.rate * total_nights_stayed
+    end
+
+    it "calculates the cost if the rate is a float" do
+      room = Hotel::Room.new(room_num: 1, rate: 150.72)
+
+      reservation_data = {
+        id: 8,
+        room: room,
+        date_range: @date_range
+      }
+
+      reservation = Hotel::Reservation.new(reservation_data)
+      total_nights_stayed = @date_range.find_num_nights
+
+      expect(reservation.calculate_cost).must_be_kind_of Float
+      expect(reservation.calculate_cost).must_equal (room.rate * total_nights_stayed).round(2)
+    end
+
     it "calculates the total with the rate per nights stayed if there is a block_id" do
       reservation_data = {
         id: 8,
