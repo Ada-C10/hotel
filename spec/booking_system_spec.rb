d1ea68c8c73aebad373827bb81421d5bb41084b5
Karis Kim <ouniiq@hotmail.com>
2018-09-08 14:21:25 -0700

    added test for checking for edge case of overlapping dates

--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -29,8 +29,20 @@ describe "BookingSystem class" do
   end
 
   it "list all bookings of that date" do
-    @reservation = @booking.make_reservation(check_in_date: 180909, check_out_date: 1809010)
+    bad_reservation = @booking.make_reservation(check_in_date: 180909, check_out_date: 1809010)
 
-    expect(@booking.list_existing_reservations(180904)).must_equal [@reservation]
+    expect(@booking.list_reservations_by_date(180904)).must_include @reservation
+    expect(@booking.list_reservations_by_date(180904)).wont_include bad_reservation
   end
+
+  it "returns msg if more than one reservation created" do
+    bad_reservation = @booking.make_reservation(check_in_date: 180909, check_out_date: 1809010)
+    expect(@booking.reservations.length).must_equal 2
+  end
+
+  # it "does not create reservation if dates are booked" do
+  #   bad_reservation = @booking.make_reservation(check_in_date: 180904, check_out_date: 180905)
+  #
+  #   expect{(@booking)}.must_raise 0
+  # end
 end
