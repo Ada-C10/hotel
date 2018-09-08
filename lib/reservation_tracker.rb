4d747fdd5c33e18006f2fe9a09c77727ed6e4b29
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-07 17:57:09 -0700

    Added comments

--- lib/reservation_tracker.rb
+++ lib/reservation_tracker.rb
@@ -9,7 +9,9 @@ class ReservationTracker
   attr_accessor :all_rooms, :all_reservations
 
   def initialize()
+    # @all_rooms is a collection of all room instances
     @all_rooms = room_factory
+    # @all_reservations is a collection of all reservation instances
     @all_reservations = []
   end
 
@@ -54,14 +56,16 @@ class ReservationTracker
     reservations_for_date = []
     formatted_date = format_date(date)
 
+    # Iterating through all reservation instances listed in @all_reservations
     @all_reservations.each do |reservation|
+      # reservation_dates is a collection of date instances for the specific reservation instance
       reservation_dates = reservation.dates_booked_for_reservation
 
+      # If the reservation_dates collection includes the specified date, then that reservation instance is pushed into reservations_for_date
       if reservation_dates.include?(formatted_date)
         reservations_for_date << reservation
       end
     end
-
     return reservations_for_date
   end
 
@@ -73,10 +77,12 @@ class ReservationTracker
     if is_date_range_valid(starting_date, ending_date)
       available_rooms = []
 
+      # This loop will iterate through each room instance listed in @all_rooms
       date_to_check = starting_date
       @all_rooms.each do |room|
         room_status = []
 
+        # This loop will examine each room instance's @dates_booked collection (using Room.is_available?) and check if it is booked for each date that is being iterated through (using date_to_check += 1). If the room is NOT booked for the specified date then 'true' is pushed into room_status. If the room IS booked then 'false' is pushed into room_status.
         until date_to_check == ending_date
           check = room.is_available?(date_to_check)
           if check
@@ -87,6 +93,7 @@ class ReservationTracker
           date_to_check += 1
         end
 
+        # If the specific room instance is NOT booked (available) for all of the dates checked, then the room is pushed into available_rooms.
         if room_status.all?
           available_rooms << room
         end
@@ -99,6 +106,7 @@ class ReservationTracker
   def update_dates_booked_for_room(new_reservation)
     dates_booked_for_new_reservation = new_reservation.dates_booked_for_reservation
 
+    # Iterating through all room instances in @all_rooms. If the room instance has the same room number as the room_num in the reservation instance, then date instances representing all dates that the reservation is made for will be pushed into that room instance's @dates_booked
     @all_rooms.each do |room|
       if room.room_number == new_reservation.room_num
         dates_booked_for_new_reservation.each do |date|
@@ -120,11 +128,13 @@ class ReservationTracker
     end_date = format_date(check_out)
 
     if is_date_range_valid(start_date, end_date)
+      # available_rooms is a collection of all room instances that are available for the given start and end date
       available_rooms = show_available_rooms(check_in, check_out)
+      # available_rooms_by_number is a collection of all room numbers that are available for the given start and end date
       available_rooms_by_number = available_rooms.map do |room_instance|
                                   room_instance.room_number.to_i
                                 end
-
+      # If the available_rooms_by_number collection contains the desired room_number that the user wants to reserve, then create a new reservation
       if available_rooms_by_number.include?(room_number)
         reservation_number = make_reservation_number
         new_reservation = Reservation.new(reservation_number, room_number, start_date, end_date, :standard)
@@ -135,7 +145,6 @@ class ReservationTracker
       else
         raise StandardError.new("The specified room is not available for the date range provided")
       end
-
     end
   end
 
--- lib/room.rb
+++ lib/room.rb
@@ -6,10 +6,11 @@ class Room
 
   def initialize(room_number)
     @room_number = room_number.to_i
-    # @dates_booked contains date instances
+    # @dates_booked is a collection of date instances in which a specific room instance is booked for
     @dates_booked = []
   end
 
+  # Checking if a specific room instance is booked on a specific date
   def is_available?(formatted_date)
 
     if @dates_booked.include?(formatted_date)
