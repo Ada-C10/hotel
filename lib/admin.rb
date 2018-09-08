f763cd9e18837dc919b66b69a2129426f37d0572
jackie <jacquelyn.cheng@me.com>
2018-09-08 14:00:50 -0700

    Updated Admin methods for making reservations and blocks, created and passing associated tests

--- lib/admin.rb
+++ lib/admin.rb
@@ -4,45 +4,50 @@
 
 module Hotel
   class Admin
-    attr_reader :rooms, :reservations
+    attr_reader :rooms, :reservations, :blocks
 
-    def initialize(room_count = 20)
+    AVAILABLE_STARTING = Date.parse("2019-01-01")
+    AVAILABLE_THRU = Date.parse("2019-12-31")
+    ROOM_COUNT = 20
+
+    def initialize
       # Creates an array of 20 Room objects, numbered 1-20
       @rooms = []
-      room_count.times do |room_num|
-        @rooms << Room.new(room_num + 1)
+      ROOM_COUNT.times do |room_num|
+        @rooms << Room.new(room_num + 1, AVAILABLE_STARTING, AVAILABLE_THRU)
       end
-      # Creates an empty hash to which all reservations are added
+      # Creates empty hashes to which all reservations and blocks are added
       @reservations = {}
       @blocks = {}
     end
 
     # Makes a reservation by 1. finding an available room,
     # 2. updating its availability, 3. creating a new Reservation that has
-    # that room, 4. returning that new Reservation
-    def make_reservation(arrive, depart)
-      checkin_date = Date.parse(arrive)
-      checkout_date = Date.parse(depart)
-      valid_dates?(checkin_date, checkout_date) # check dates
-      room = assign_rooms(1, checkin_date, checkout_date, :BOOKED) # check availability and return available room
-      confirmation_num = reservations.length + 1
-      new_res = Reservation.new(checkin_date, checkout_date, room, confirmation_num) # create res
-      reservations[confirmation_num] = new_res
+    # that room, 4. adding that room to the list of reservations and to a block
+    # if applicable, and 5. returning that new Reservation
+    def make_reservation(checkin_date, checkout_date, room_quantity = 1, block = nil)
+      valid_dates?(checkin_date, checkout_date) # validate dates
+      valid_block?(checkin_date, checkout_date, block) unless block == nil
+      rooms = book_rooms(checkin_date, checkout_date, room_quantity, block) # find and book available rooms
+      confirmation_num = reservations.length + 1 # generate confirmation number
+      new_res = Reservation.new(checkin_date, checkout_date, confirmation_num, rooms, block) # create res
+      reservations[confirmation_num] = new_res # add to admin's list of reservations
+      block.add_reservation(new_res) unless block == nil # add res to block and update block availability
       return new_res
     end
 
-    def make_block(block, arrive, depart, room_quantity, discount_rate)
-      checkin_date = Date.parse(arrive)
-      checkout_date = Date.parse(depart)
+    def make_block(block_name, checkin_date, checkout_date, discount_rate, room_quantity = 1)
       valid_dates?(checkin_date, checkout_date) # check dates
-      blocked_rooms = assign_rooms(room_quantity, checkin_date, checkout_date, :BLOCKED) # check availability and return available room
-      confirmation_num = blocks.length + 1
-      new_block = Block.new(block, checkin_date, checkout_date, discount_rate, blocked_rooms, nil, confirmation_num) # create res
-      blocks[confirmation_num] = new_block
-      return blocked_rooms
+      if room_quantity > 5 || room_quantity < 1
+        raise ArgumentError, "Can only block between 1-5 rooms"
+      end
+      new_block = Block.new(checkin_date, checkout_date, block_name, discount_rate) # create block
+      # check availability and return available rooms with updated nightly rates
+      block_rooms(new_block, checkin_date, checkout_date, discount_rate, room_quantity)
+      blocks[block_name] = new_block # add to admin's list of blocks
+      return new_block
     end
 
-    # TODO dependency on Reservation for checkin_date and checkout_date
     def list_reservations(date)
       if reservations.empty?
         return nil
@@ -53,9 +58,16 @@ module Hotel
       end
     end
 
-    # TODO dependency on Room for is_available?
-    def list_available_rooms(day)
-      return rooms.select { |room| !room.is_available?(day) }
+    def find_reservation(confirmation_num)
+      return reservations[confirmation_num]
+    end
+
+    def find_block(block)
+      return blocks[block]
+    end
+
+    def list_available_rooms(checkin_date, checkout_date = nil)
+      return rooms.select { |room| !room.is_available?(checkin_date, checkout_date) }
     end
 
     private # helper methods
@@ -63,37 +75,65 @@ module Hotel
       def valid_dates?(checkin_date, checkout_date)
         if checkin_date > checkout_date
           raise ArgumentError, "Check-in date must be before check-out date."
+        elsif checkin_date > AVAILABLE_THRU || checkout_date > AVAILABLE_THRU
+          raise ArgumentError, "Hotel only has availability through #{AVAILABLE_THRU}."
         else
           return true
         end
       end
 
-      # returns an array of the requested number of available Rooms (or 1 Room),
-      # or raises an error if not enough rooms availble for the dates/quantity requested
-      # TODO: dependency on Room for is_available?
-      def available_rooms(room_quantity, checkin_date, checkout_date)
+      def valid_block?(checkin_date, checkout_date, block)
+        found_block = blocks.find { |existing_block| block == existing_block }
+        if found_block == nil
+          raise ArgumentError, "Block for #{block} not found."
+        elsif (found_block.checkin_date == checkin_date && found_block.checkout_date == checkout_date) == false
+          raise ArgumentError, "Invalid dates for #{block}."
+        else
+          return blocked_rooms.length > 0
+        end
+      end
+
+      # returns an array of the requested number of available Rooms
+      # changes rooms' status to :BOOKED for the selected dates
+      # raises an error if not enough rooms availble for the dates/quantity requested
+      def book_rooms(checkin_date, checkout_date, room_quantity = 1, block = nil)
+        if block == nil
+          selected_rooms = rooms.select { |room|
+            room.is_available?(checkin_date, checkout_date)
+          }
+        else # block != nil
+          selected_rooms = block.blocked_rooms.select { |room|
+            room.is_available?(checkin_date, checkout_date)
+          }
+        end
+        if selected_rooms.empty?
+          raise RoomAvailabilityError, "No rooms available from #{checkin_date} to #{checkout_date}."
+        elsif selected_rooms.length < room_quantity
+          raise RoomAvailabilityError, "Only #{room_quantity} rooms available from #{checkin_date} to #{checkout_date}."
+        else # get the requested number of rooms and change their status to booked
+          booked_rooms = selected_rooms.first(room_quantity)
+          booked_rooms.each { |room| room.change_room_status(checkin_date, :BOOKED, checkout_date)}
+          return booked_rooms
+        end
+      end
+
+      # returns an array of the requested number of available Rooms from the block,
+      # raises an error if not enough rooms availble for the dates/quantity requested
+      def block_rooms(block, checkin_date, checkout_date, discount_rate, room_quantity = 1)
         selected_rooms = rooms.select { |room|
-          room.is_available?(checkin_date, checkout_date.prev_day)
+          room.is_available?(checkin_date, checkout_date)
         }
         if selected_rooms.empty?
           raise RoomAvailabilityError, "No rooms available from #{checkin_date} to #{checkout_date}."
         elsif selected_rooms.length < room_quantity
           raise RoomAvailabilityError, "Only #{room_quantity} rooms available from #{checkin_date} to #{checkout_date}."
         else
-          return selected_rooms.first(room_quantity)
-        end
-      end
-
-      # TODO dependency on Room for change_room_status
-      def assign_rooms(room_quantity, checkin_date, checkout_date, room_status)
-        selected_rooms = available_rooms(room_quantity, checkin_date, checkout_date)
-        selected_rooms.each do |room|
-          room.change_room_status(checkin_date, checkout_date.prev_day, room_status)
-        end
-        if selected_rooms.length == 1
-          return selected_rooms.first
-        else
-          return selected_rooms
+          blocked_rooms = selected_rooms.first(room_quantity)
+          blocked_rooms.each do |room|
+            room.change_nightly_rate(checkin_date, discount_rate, checkout_date)
+            block.add_room(room)
+          end
+          return blocked_rooms
         end
       end
   end
--- spec/admin_spec.rb
+++ spec/admin_spec.rb
@@ -7,8 +7,6 @@ require_relative 'spec_helper.rb'
 describe "Admin" do
   before do
     @admin = Hotel::Admin.new
-    @res1 = @admin.make_reservation("2019-01-01", "2019-01-04")
-    @res2 = @admin.make_reservation("2019-01-01", "2019-01-02")
   end
 
   describe "Admin#initialize" do
@@ -22,34 +20,61 @@ describe "Admin" do
       expect(@admin.rooms.last).must_be_instance_of Hotel::Room
     end
 
+    it "has an empty colletion of reservations when initialized" do
+      expect(@admin.reservations.length).must_equal 0
+    end
+
+    it "has an empty colletion of blocks when initialized" do
+      expect(@admin.blocks.length).must_equal 0
+    end
   end
 
   describe "Admin#make_reservation" do
-    it "returns a Reservation" do
-      expect(@admin.make_reservation("2019-01-01", "2019-01-02")).must_be_instance_of Hotel::Reservation
+    let (:res1) {
+      @admin.make_reservation(Date.parse("2019-01-01"), Date.parse("2019-01-04"))
+    }
+
+    let (:res2) {
+      @admin.make_reservation(Date.parse("2019-01-01"), Date.parse("2019-01-02"), 2)
+    }
+
+    it "returns a Reservation with correct reservation dates" do
+      expect(res1).must_be_instance_of Hotel::Reservation
+      expect(res1.checkin_date).must_be_instance_of Date
+      expect(res1.checkin_date.to_s).must_equal "2019-01-01"
+      expect(res1.checkout_date).must_be_instance_of Date
+      expect(res1.checkout_date.to_s).must_equal "2019-01-04"
     end
 
-    it "assigns a room to the Reservation" do
-      expect(@res1.rooms).must_be_instance_of Hotel::Room
+    it "assigns a collection of rooms to the Reservation" do
+      expect(res1.rooms).must_be_kind_of Enumerable
+      expect(res1.rooms.first).must_be_instance_of Hotel::Room
+      expect(res2.rooms).must_be_kind_of Enumerable
+      expect(res2.rooms.last).must_be_instance_of Hotel::Room
     end
 
-    it "updates the assigned room's availability" do
-      expect(@admin.rooms[2].is_available?(Date.parse("2019-01-01"))).must_equal true
+    it "makes the assigned rooms unavailable" do
+      res1.checkin_date.upto(res1.checkout_date.prev_day) do |date|
+        expect(res1.rooms.first.is_available? date).must_equal false
+      end
+    end
 
-      res3 = @admin.make_reservation("2019-01-01", "2019-01-03")
-      expect(@admin.rooms[2].is_available?(Date.parse("2019-01-01"))).must_equal false
+    it "updates the assigned rooms' availability to :BOOKED" do
+      res1.checkin_date.upto(res1.checkout_date.prev_day) do |date|
+        expect(res1.rooms.first.availability[date.to_s][:status]).must_equal :BOOKED
+      end
     end
 
     it "raises an exception if provided end date before start date" do
       expect{
-        @admin.make_reservation("2019-01-04", "2019-01-01")
+        @admin.make_reservation(Date.parse("2019-01-04"), Date.parse("2019-01-01"))
       }.must_raise ArgumentError
     end
 
     it "raises an exception if no rooms are available for dates selected" do
       expect{
         21.times do
-          @admin.make_reservation("2019-01-01", "2019-01-04")
+          @admin.make_reservation(Date.parse("2019-01-01"), Date.parse("2019-01-04"))
         end
       }.must_raise Hotel::RoomAvailabilityError
     end
@@ -58,16 +83,102 @@ describe "Admin" do
   describe "Admin#list_reservations" do
     it "returns a list of all reservations on a specific date" do
       10.times do
-        @admin.make_reservation("2019-01-01", "2019-01-04")
+        @admin.make_reservation(Date.parse("2019-01-01"), Date.parse("2019-01-04"))
       end
-      expect(@admin.list_reservations(Date.parse("2019-01-01")).length).must_equal 12
+      expect(@admin.list_reservations(Date.parse("2019-01-01")).length).must_equal 10
     end
   end
 
   describe "Admin#list_available_rooms" do
     it "returns a list of available rooms for a given date" do
-      expect(@admin.list_available_rooms("2019-01-01").length).must_equal 2
-      expect(@admin.list_available_rooms("2019-01-03").length).must_equal 1
+      @admin.make_reservation(Date.parse("2019-01-01"), Date.parse("2019-01-02"))
+      @admin.make_reservation(Date.parse("2019-01-01"), Date.parse("2019-01-04"))
+      expect(@admin.list_available_rooms(Date.parse("2019-01-01")).length).must_equal 2
+      expect(@admin.list_available_rooms(Date.parse("2019-01-03")).length).must_equal 1
+    end
+  end
+
+  describe "Admin#make_block" do
+    before do
+      @block_name = "Jackie's Event"
+      @checkin_date = Date.parse("2019-02-01")
+      @checkout_date = Date.parse("2019-02-05")
+      @discount_rate = 150.00
+      @room_quantity = 5
+    end
+
+    let (:block) {
+      @admin.make_block(@block_name, @checkin_date, @checkout_date, @discount_rate, @room_quantity)
+    }
+
+    it "returns a Block with correct reservation dates" do
+      expect(block).must_be_instance_of Hotel::Block
+      expect(block.checkin_date).must_be_instance_of Date
+      expect(block.checkin_date.to_s).must_equal "2019-02-01"
+      expect(block.checkout_date).must_be_instance_of Date
+      expect(block.checkout_date.to_s).must_equal "2019-02-05"
+    end
+
+    it "assigns a collection of rooms to the Block" do
+      expect(block.blocked_rooms).must_be_kind_of Enumerable
+      expect(block.blocked_rooms.first).must_be_instance_of Hotel::Room
+      expect(block.blocked_rooms.last).must_be_instance_of Hotel::Room
+      expect(block.blocked_rooms.length).must_equal 5
+    end
+
+    it "makes the assigned rooms unavailable" do
+      block.checkin_date.upto(block.checkout_date.prev_day) do |date|
+        expect(block.blocked_rooms.first.is_available? date).must_equal false
+        expect(block.blocked_rooms.last.is_available? date).must_equal false
+      end
+    end
+
+    it "updates the assigned rooms' availability to :BLOCKED" do
+      block.checkin_date.upto(block.checkout_date.prev_day) do |date|
+        expect(block.blocked_rooms.first.availability[date.to_s][:status]).must_equal :BLOCKED
+        expect(block.blocked_rooms.last.availability[date.to_s][:status]).must_equal :BLOCKED
+      end
+    end
+
+    it "raises an exception if provided end date before start date" do
+      expect{
+        @admin.make_block(@block_name, @checkout_date, @checkin_date, @discount_rate, @room_quantity)
+      }.must_raise ArgumentError
+    end
+
+    it "raises an exception if no rooms are available for dates selected" do
+      expect{
+        5.times do
+          @admin.make_block(@block_name, @checkin_date, @checkout_date, @discount_rate, @room_quantity)
+        end
+      }.must_raise Hotel::RoomAvailabilityError
+    end
+  end
+
+  describe "Admin#find_reservation" do
+    it "returns nil if no reservation found" do
+      expect(@admin.find_reservation(10)).must_be_nil
+    end
+
+    it "returns the reservation with the corresponding confirmation number if found" do
+      @admin.make_reservation(Date.parse("2019-02-01"), Date.parse("2019-02-05"))
+      expect(@admin.find_reservation(1)).must_be_instance_of Hotel::Reservation
+      expect(@admin.find_reservation(1).confirmation_number).must_equal 1
+      expect(@admin.find_reservation(1).rooms.first).must_be_instance_of Hotel::Room
+      expect(@admin.find_reservation(1).checkin_date).must_equal Date.parse("2019-02-01")
+    end
+  end
+
+  describe "Admin#find_block" do
+    it "returns nil if no block found" do
+      expect(@admin.find_block("Johnson Wedding")).must_be_nil
+    end
+
+    it "returns the block with the corresponding block name if found" do
+      @admin.make_block("Jackie's Event", Date.parse("2019-02-01"), Date.parse("2019-02-05"), 150.00, 3)
+      expect(@admin.find_block("Jackie's Event")).must_be_instance_of Hotel::Block
+      expect(@admin.find_block("Jackie's Event").block_name).must_equal "Jackie's Event"
+      expect(@admin.find_block("Jackie's Event").discount_rate).must_equal 150.00
     end
   end
 end
