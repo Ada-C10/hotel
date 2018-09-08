d4da0060018431195dd94ee9a32fef064365ef7c
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-07 21:07:52 -0700

    Wave 1 Reservation class done, tests passing

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,14 +5,14 @@ require "awesome_print"
 module HotBook
 # The Reservation class is responsible for calculating reservation cost
   class Reservation
-    require "date"
-
-    attr_reader :range, :room_number, :room_rate
+    require_relative "daterange"
+    attr_reader :daterange, :duration, :room_number, :room_rate
 
     def initialize(daterange:, room_number:, room_rate:)
-      @range = daterange.to_range
+      @daterange = daterange
       @room_number = room_number # what about room.number
       @room_rate = room_rate # and room.rate
+      @duration = daterange.duration
     end
 
     def cost
@@ -22,25 +22,3 @@ module HotBook
   end
 
 end
-
-# TODO:
-# Custom error message!
-# valid as a class method?
-#
-# - provide :start_date and :end_date
-#     - later, reservation class comes up with a room number
-#     - ruby makes a reservation object
-#         - :start_date
-#         - :end_date
-#         - :room_number (default to default, define default as room 1)
-# provide reservation
-#    - reservation returns number of nights total
-#    - RoomCharge class knows it's duration - 1, and multiplies nights by room cost
-  # - reservation shouldn't know anything about cost
-# later, reservation could ask hotel for room number
-# to assign to a new reservation
-
-
-# ## WAVE2
-# - provide: start_date and :end_date
-#     - Reservation class can return
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,14 +1,38 @@
 require_relative "spec_helper"
 
 describe "HotBook::Reservation class" do
-  let(:daterange) {HotBook::DateRange.new("apr_6", "apr_8")}
-  let(:args) {daterange: daterange}
-  let(:reservation) {HotBook::Reservation.new(args)}
+  let(:daterange) { HotBook::DateRange.new(start_date: "apr_6",
+                                           end_date: "apr_8") }
+  let(:reservation) { HotBook::Reservation.new(daterange: daterange,
+                                               room_number: "one",
+                                               room_rate: 1.0) }
 
   describe "initialize method" do
+    it "will correctly calculate duration" do
+      expect(reservation.duration).must_equal 2
+    end
+
+    it "will correctly accept room_number" do
+      expect(reservation.room_number).must_equal "one"
+    end
+
+    it "will correctly accept room_rate" do
+      expect(reservation.room_rate).must_equal 1.0
+    end
+
+    it "will raise NoMethodError if daterange doesn't respond to duration" do
+      badrange = "some_string"
+      expect{
+        HotBook::Reservation.new(daterange: badrange,
+                                 room_number: "one",
+                                 room_rate: 0.0) }.must_raise NoMethodError
+    end
   end
 
   describe "cost method" do
+    it "will return correct cost" do
+      expect(reservation.cost).must_equal 2.0
+    end
   end
 
 end
