47864f1661dc8f6458bc4e7bdd3cde8ff804742e
Karis Kim <ouniiq@hotmail.com>
2018-09-08 14:20:09 -0700

    added code in make reservation method for edge case of overlapping dates

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,4 +1,5 @@
 require_relative 'reservation'
+require 'pry'
 
 module Hotel
   class BookingSystem
@@ -11,12 +12,34 @@ module Hotel
 
     def make_reservation(check_in_date:, check_out_date:)
       # TODO find a room that's available
+
+
+
       room_number = get_available_room(check_in_date: check_in_date, check_out_date: check_out_date)
 
       reservation = Reservation.new(check_in_date: check_in_date, check_out_date: check_out_date, room_number: room_number)
 
-      @reservations << reservation
-      return reservation
+      if @reservations.empty?
+        @reservations << reservation
+      # binding.pry
+        return reservation
+      elsif @reservations.length >= 1
+        requested_dates = reservation.date_range
+
+        @reservations.each do |booking|
+            reserved_dates = booking.date_range
+
+            reserved_dates.zip(requested_dates).each do |date1, date2|
+              unless date1 != date2
+                raise ArgumentError, "unavailable date"
+              end
+            end
+          end
+
+          @reservations << reservation
+        # binding.pry
+          return @reservations
+      end
     end
 
     def get_available_room(check_in_date:, check_out_date:)
@@ -27,7 +50,7 @@ module Hotel
       return @rooms
     end
 
-    def list_existing_reservations(date)
+    def list_reservations_by_date(date)
       bookings_by_date = []
 
       specific_date = Date.parse("#{date}")
@@ -40,6 +63,12 @@ module Hotel
       end
 
       return bookings_by_date
+    end
+
+    def available_rooms_for_date_range(check_in_date:, check_out_date:)
+
+
+      @reservations
     end
   end
 end
