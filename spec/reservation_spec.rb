659cc3f24c6bc2596d572917541bd76c3bc0d4c8
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-08 13:45:32 -0700

    Added conflict? method in DateRange, tests pass

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,11 +1,23 @@
 require_relative "spec_helper"
+require "csv"
+TEST_FILENAME = "support/test_reservation.csv"
 
+# First, overwrite the test CSV
+CSV.open(TEST_FILENAME, "w") #["start date", "end date", "room number", "room rate"] }
+# Next, initialize a new Reservation, which stores itself in the CSV along with
+# a header row
+daterange = HotBook::DateRange.new(start_date: "apr_6", end_date: "apr_8")
+HotBook::Reservation.new(daterange: daterange, room_number: "before tests",
+                         room_rate: 1.0, filename: TEST_FILENAME)
+
+## Project Requirement: User can reserve a room for a given date range
 describe "HotBook::Reservation class" do
   let(:daterange) { HotBook::DateRange.new(start_date: "apr_6",
                                            end_date: "apr_8") }
   let(:reservation) { HotBook::Reservation.new(daterange: daterange,
                                                room_number: "one",
-                                               room_rate: 1.0) }
+                                               room_rate: 1.0,
+                                               filename: TEST_FILENAME) }
 
   describe "initialize method" do
     it "will correctly calculate duration" do
@@ -19,19 +31,31 @@ describe "HotBook::Reservation class" do
     it "will correctly accept room_rate" do
       expect(reservation.room_rate).must_equal 1.0
     end
-
-    it "will raise NoMethodError if daterange doesn't respond to duration" do
-      badrange = "some_string"
-      expect{
-        HotBook::Reservation.new(daterange: badrange,
-                                 room_number: "one",
-                                 room_rate: 0.0) }.must_raise NoMethodError
-    end
   end
 
+### Project Requirement: Code should raise StandardError when an invalid
+### DateRange is provided
+
+##### Project Requirement: User can get the total cost for a given reservation
+#### The last day of a reservation is the checkout day, so the guest should not
+#### be charged for that night
   describe "cost method" do
     it "will return correct cost" do
       expect(reservation.cost).must_equal 2.0
+    end
+  end
+
+  describe "save_reservation method" do
+    it "will add itself to CSV upon initialization" do
+      before_data = CSV.open(TEST_FILENAME).map { |line| line }
+      HotBook::Reservation.new(daterange: daterange,
+                               room_number: "thisisanewroom",
+                               room_rate: 99.99,
+                               filename: TEST_FILENAME)
+      after_data = CSV.open(TEST_FILENAME).map { |line| line }
+      expect(after_data.size).must_equal (before_data.size + 1)
+      expect(after_data.last).must_equal ["2018-04-06", "2018-04-08",
+                                          "thisisanewroom","99.99"]
     end
   end
 
