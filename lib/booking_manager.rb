d141f9bfb2008a092c0232901af8ea62853cad47
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-08 22:52:43 -0700

    Create BookingManager#add_block method and tests

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -16,6 +16,19 @@ class BookingManager
     return "Added!"
   end
 
-
+  def add_block(block)
+    dates = block.get_all_dates
+    rooms = calendar.list_available_rooms(block)
+    if rooms.length < block.number_of_rooms
+      return "Not enough available rooms."
+    end
+    block_rooms = rooms[0..(block.number_of_rooms - 1)]
+    block_rooms.each do |room|
+      # dates.each do |date|
+        calendar.room_assignments[room] << dates
+      # end
+    end
+    return "Added!"
+  end
 
 end
--- spec/booking_manager_spec.rb
+++ spec/booking_manager_spec.rb
@@ -23,7 +23,25 @@ describe BookingManager do
   let(:reservation5) {
     Reservation.new('181130', '181206')
   }
-
+  let(:block2) {
+    Block.new('181202', '181206', 2)
+  }
+  let(:block3) {
+    Block.new('181206', '181210', 3)
+  }
+  let(:block4) {
+    Block.new('181202', '181206', 4)
+  }
+  let(:block5) {
+    Block.new('181202', '181206', 5)
+  }
+  before do
+    manager.add_reservation(reservation1)
+    manager.add_reservation(reservation2)
+    manager.add_reservation(reservation3)
+    manager.add_reservation(reservation4)
+    manager.add_reservation(reservation5)
+  end
   describe "#initialize" do
     it "can be instantiated" do
       expect(manager).must_be_kind_of BookingManager
@@ -31,13 +49,6 @@ describe BookingManager do
   end
 
   describe "#add_reservation" do
-    before do
-      manager.add_reservation(reservation1)
-      manager.add_reservation(reservation2)
-      manager.add_reservation(reservation3)
-      manager.add_reservation(reservation4)
-      manager.add_reservation(reservation5)
-    end
     it "returns 'Added!' message" do
       expect(manager.add_reservation(reservation1)).must_equal "Added!"
     end
@@ -47,4 +58,26 @@ describe BookingManager do
       expect(calendar.room_assignments[3]).must_equal reservation5.get_all_dates
     end
   end
+
+  describe "#add_block" do
+    it "returns 'Added!' message" do
+      expect(manager.add_block(block2)).must_equal "Added!"
+    end
+    it "adds all reservation dates to all block rooms" do
+      manager.add_block(block2)
+      # binding.pry
+      expect(calendar.room_assignments[4][0]).must_equal block2.get_all_dates
+      expect(calendar.room_assignments[5][0]).must_equal block2.get_all_dates
+    end
+    it "adds block dates as array" do
+      before = calendar.room_assignments[1].length
+      # binding.pry
+      manager.add_block(block3)
+      # binding.pry
+      expect(calendar.room_assignments[1].length).must_equal before + 1
+      expect(calendar.room_assignments[1].include? block3.get_all_dates).must_equal true
+    end
+  end
 end
+  # end
+  # end
