4e41d061865744089e21c61fa8a9541069f9813a
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-08 18:17:36 -0700

    Created is_available_for_block? and corresponding unit test

--- lib/room.rb
+++ lib/room.rb
@@ -1,18 +1,27 @@
 require_relative 'lib_file_helper'
 
 class Room
-  attr_accessor :room_number, :dates_booked
+  attr_accessor :room_number, :dates_booked, :dates_held_for_block
 
   def initialize(room_number)
     @room_number = room_number.to_i
     # @dates_booked is a collection of date instances in which a specific room instance is booked for
     @dates_booked = []
+    @dates_held_for_block = []
   end
 
   # Checking if a specific room instance is booked on a specific date
   def is_available?(formatted_date)
 
     if @dates_booked.include?(formatted_date)
+      return false
+    else
+      return true
+    end
+  end
+
+  def is_available_for_block?(formatted_date)
+    if @dates_held_for_block.include?(formatted_date)
       return false
     else
       return true
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -6,7 +6,7 @@ describe 'Room' do
     expect(new_room).must_be_kind_of Room
   end
 
-  it 'checks if the room is available on the specified date' do
+  it 'checks if the room is unavailable on the specified date' do
     new_room = Room.new(15)
     reservation_date_1 = Date.new(2018, 9, 10)
     reservation_date_2 = Date.new(2018, 9, 11)
@@ -27,5 +27,29 @@ describe 'Room' do
 
     expect(new_room.is_available?(date_to_check)).must_equal true
   end
+
+  it 'checks if the room is available for a block' do
+    new_room = Room.new(15)
+    reservation_date_1 = Date.new(2018, 9, 10)
+    reservation_date_2 = Date.new(2018, 9, 11)
+    date_to_check = Date.new(2018, 9, 13)
+    new_room.dates_held_for_block << reservation_date_1
+    new_room.dates_held_for_block << reservation_date_2
+
+    expect(new_room.is_available_for_block?(date_to_check)).must_equal true
+  end
+
+  it 'checks if the room is unavailable for a block' do
+    new_room = Room.new(15)
+    reservation_date_1 = Date.new(2018, 9, 10)
+    reservation_date_2 = Date.new(2018, 9, 11)
+    date_to_check = Date.new(2018, 9, 11)
+    new_room.dates_held_for_block << reservation_date_1
+    new_room.dates_held_for_block << reservation_date_2
+
+    expect(new_room.is_available_for_block?(date_to_check)).must_equal false
+  end
+
+
 
 end
