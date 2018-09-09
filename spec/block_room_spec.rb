78b3c5859a45b046592a254a07967f39463caf83
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-08 18:01:09 -0700

    Changed unit test

--- spec/block_room_spec.rb
+++ spec/block_room_spec.rb
@@ -6,7 +6,8 @@ describe 'BlockRoom' do
     event_name = "AdaCon"
     start_date = "2018, 9, 10"
     end_date = "2018, 9, 15"
-    new_block = BlockRoom.new(event_name, start_date, end_date)
+    room_instances = [Room.new(15), Room.new(14)]
+    new_block = BlockRoom.new(event_name, start_date, end_date, room_instances)
 
     expect(new_block).must_be_kind_of BlockRoom
   end
