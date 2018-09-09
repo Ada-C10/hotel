e92746c8171095187245b4727848fe6ea86e6d12
Layla <layla.bristol@gmail.com>
2018-09-09 16:17:15 -0700

    Cleaned up some code and Add method for list of reservations on given date

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,8 +1,59 @@
+require_relative 'date_range'
+require_relative 'reservation'
+
 module Hotel
   class BookingSystem
+    attr_reader :reservations, :rooms
 
-    def initilize
+    VALID_ROOM_NUMBERS = (1..20)
+    ROOM_COST = 200
+
+    def initialize
+      @reservations = []
+      @rooms = VALID_ROOM_NUMBERS
     end
+
+
+    ####
+    # room: an Integer for the room number
+    # check_in: a Date object for the check in day
+    # check_out: a Date object for the check out day
+    def book_room(room_number, check_in, check_out)
+      unless VALID_ROOM_NUMBERS.include?(room_number)
+        raise ArgumentError
+      end
+
+      date_range = Hotel::DateRange.new(check_in, check_out)
+      new_reservation = Hotel::Reservation.new(room_number, ROOM_COST, date_range)
+
+      @reservations << new_reservation
+
+      return new_reservation
+    end
+
+
+    def list_available_rooms
+      available_room = []
+      reservation_list.include?(available_room)
+
+      return available_room
+
+      # to find available room: check if dates overlap - two dates to compare
+      # output boolean
+      #
+      # reservation_attempt, has start and end date, comparing existing reservations start and end date
+    end
+
+    def reservations_on_date(date)
+      reservations = []
+      @reservations.each do |reservation|
+        if reservation.within_reservation?(date)
+          reservations << reservation
+        end
+      end
+      return reservations
+    end
+    
 
   end
 end
--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -3,10 +3,40 @@ require_relative '../lib/booking_system'
 
 
 describe "what the hotel booking system does" do
-
-  it "create a class for the booking system" do
+  before do
+    @room_number = 12
+    @check_in = Date.new(2018, 8, 24)
+    @check_out = @check_in + 5
+    @booking_system = Hotel::BookingSystem.new
   end
 
-  
+  it "create a class for the booking system" do
+    @booking_system.must_be_kind_of Hotel::BookingSystem
+  end
+
+  it "books a new room" do
+    new_reservation = @booking_system.book_room(@room_number, @check_in, @check_out)
+    new_reservation.must_be_kind_of Hotel::Reservation
+
+    @booking_system.reservations.length.must_equal 1
+  end
+
+  it "returns argument if room_number is outside range" do
+    expect{ @booking_system.book_room(50, @check_in, @check_out) }.must_raise ArgumentError
+  end
+
+  it "returns all reservations" do
+    @booking_system.must_respond_to :reservations
+  end
+
+  it "returns all rooms" do
+    @booking_system.must_respond_to :rooms
+  end
+
+  it "returns list of reservations on a given date" do
+    @booking_system.reservations_on_date(@check_in).must_be_kind_of Array
+
+  end
+
 
 end
