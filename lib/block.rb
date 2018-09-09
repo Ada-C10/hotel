007fc019e17ff8a2b92ee670b15bae1634b60b71
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-09 11:26:19 -0700

    Added empty files for new Block class

---
+++ lib/block.rb
@@ -0,0 +1,10 @@
+# Optional - for developer use
+require "pry"
+require "awesome_print"
+
+module HotBook
+# The Reservation class is responsible for calculating reservation cost
+  class Block
+    require_relative "daterange"
+  end
+end
