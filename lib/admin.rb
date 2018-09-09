8111b136eda6296f9f6639a4841ff5985224ea01
Divya <divyamohanan@gmail.com>
2018-09-09 16:17:14 -0700

    Tests passing, changes made to admin.rb

--- lib/admin.rb
+++ lib/admin.rb
@@ -9,41 +9,46 @@ ROOMS_PRIOR_TO_BOOKING =  [{1=>[]},{2=>[]},{3=>[]},{4=>[]},{5=>[]},{6=>[]},{7=>[
 {18=>[]},{19=>[]},{20=>[]}]
 
 class Admin
-
-  @@rooms = ROOMS_PRIOR_TO_BOOKING
-  @@blocked_rooms = []
+  # @@rooms = ROOMS_PRIOR_TO_BOOKING
+  # @@blocked_rooms = []
   def initialize
-    @blocked_start_date = ""
-    @blocked_end_date = ""
+    @rooms = ROOMS_PRIOR_TO_BOOKING
+    @blocked_rooms = []
+    @blocked_start_date = nil
+    @blocked_end_date = nil
   end
+
+  attr_accessor :blocked_start_date, :blocked_end_date
 
   def block_rooms(number_blocked, check_in_date, check_out_date)
     @blocked_start_date = check_in_date
     @blocked_end_date = check_out_date
     number_blocked.times do |i|
-      rooms_sent = @@rooms
+      rooms_sent = @rooms
       entry = Room.new((20 - i), @blocked_start_date, @blocked_end_date, rooms_sent)
-      entry.block_room
-      @@rooms = Room.rooms
-      @@blocked_rooms << 20 - i
+      entry.block_room_in_room
+      @rooms = Room.rooms
+      @blocked_rooms << 20 - i
     end
+    return @blocked_rooms
   end
 
   def book_blocked_room
-    if @@blocked_rooms != []
-      room = @@blocked_rooms[0]
+    if @blocked_rooms != []
+      room = @blocked_rooms[0]
       special_reservation = Reservation.new(room, @blocked_start_date, @blocked_end_date)
       special_reservation.cost_special
-      rooms_sent = @@rooms
+      rooms_sent = @rooms
       entry = Room.new(room, @blocked_start_date, @blocked_end_date, rooms_sent)
-      entry.book_blocked_room
-      @@rooms = Room.rooms
-      @@blocked_rooms.delete(@@blocked_rooms[0])
+      entry.book_blocked_room_in_room
+      @rooms = Room.rooms
+      @blocked_rooms.delete(@blocked_rooms[0])
     end
+    return @blocked_rooms
   end
 
   def view_all_rooms
-    puts @@rooms
+    return @rooms
   end
 
   def reserve(room_number, check_in_date, check_out_date)
@@ -54,14 +59,14 @@ class Admin
     end
 
     Reservation.new(room_number, check_in_date, check_out_date)
-    rooms_sent = @@rooms
+    rooms_sent = @rooms
     entry = Room.new(room_number, check_in_date, check_out_date, rooms_sent)
     entry.book_room
-    @@rooms = Room.rooms
+    @rooms = Room.rooms
   end
 
   def rooms_reserved_by_date(date)
-    @@rooms.each do |hash|
+    @rooms.each do |hash|
       hash.each do |key, value|
         value.each do |date_range|
           if Date.parse(date) > Date.parse(date_range[0]) &&
@@ -84,7 +89,7 @@ class Admin
   def check_availability (check_in_date, check_out_date = nil)
     if check_out_date == nil
       date = check_in_date
-      @@rooms.each do |hash|
+      @rooms.each do |hash|
         hash.each do |key,value|
           value.each do |date_range|
             if Date.parse(date) < Date.parse(date_range[0]) || Date.parse(date) >= Date.parse(date_range[1])
@@ -97,7 +102,7 @@ class Admin
         end
       end
    else
-    @@rooms.each do |hash|
+    @rooms.each do |hash|
       hash.each do |key, value|
         if value != []
           value.each do |date_range|
@@ -118,25 +123,25 @@ end
 
 end
 
-test = Admin.new
-test.reserve(7,"2018-08-09","2018-08-11")
-#test.reserve(7,"2018-08-01","2018-08-05")
-# test.print_total_cost_per_reservation("2018/1")
-# # #
-#test.reserve(8,"2018-06-21","2018-07-05")
-# #print Reservation.reservations
-# test.print_total_cost_per_reservation("2018/2")
-#test.view_all_rooms
-
-test.reserve(8,"2018-06-23","2018-07-02")
-test.block_rooms(4, "2018-06-11","2018-07-11")
-test.view_all_rooms
-# test.rooms_available_by_date("2018-08-19")
-test.rooms_reserved_by_date("2018-08-10")
-test.check_availability("2018-08-10")
-test.rooms_reserved_by_date("2018-07-10")
-test.book_blocked_room
-test.view_all_rooms
-Reservation.reservations
-#test.print_total_cost_per_reservation(3)
-# test.view_reservations_on_a_date("2018-08-24")
+# test = Admin.new
+# test.reserve(7,"2018-08-09","2018-08-11")
+# test.reserve(7,"2018-08-02","2018-08-05")
+# # test.print_total_cost_per_reservation("2018/1")
+# # # #
+# #test.reserve(8,"2018-06-21","2018-07-05")
+# # #print Reservation.reservations
+# # test.print_total_cost_per_reservation("2018/2")
+# #test.view_all_rooms
+#
+# test.reserve(8,"2018-06-23","2018-07-02")
+#test.block_rooms(4, "2018-06-11","2018-07-11")
+#puts r.length
+# print test.view_all_rooms
+# test.rooms_reserved_by_date("2018-08-10")
+# test.check_availability("2018-08-10")
+# test.rooms_reserved_by_date("2018-07-10")
+#test.book_blocked_room
+# test.view_all_rooms
+# Reservation.reservations
+#test.print_total_cost_per_reservation("2018/2")
+# # test.view_reservations_on_a_date("2018-08-24")
--- spec/admin_spec.rb
+++ spec/admin_spec.rb
@@ -1,6 +1,9 @@
 require_relative 'spec_helper'
 
-describe "Wave 1" do
+# describe "Wave 1 + 2 + 3" do
+#   before do
+#     test = Admin.new
+#   end
 
   describe "#initialize" do
     it "Initializes a class variable that stores an empty string" do
@@ -12,14 +15,39 @@ describe "Wave 1" do
       expect(test).must_respond_to :blocked_end_date
       expect(test.blocked_start_date).must_be_nil
     end
-  end
+   end
 
   describe "block rooms" do
+
     it "Returns an array of blocked rooms" do
-        test = Admin.new
-        check = test.block_rooms(3, "2018-10-01", "2018-10-15")
+      test = Admin.new
+      check = test.block_rooms(3, "2018-10-01", "2018-10-15")
 
         expect(check.length).must_equal 3
     end
   end
-end
+
+  describe "Checks if blocked room is booked" do
+    it "Confirms a room from blocked rooms is booked" do
+      test = Admin.new
+      check = test.block_rooms(3, "2018-10-01", "2018-10-15")
+      new = test.book_blocked_room
+
+      expect(new.length).must_equal 2
+    end
+  end
+
+#end
+
+
+
+# describe "Checks that array of rooms contains all rooms" do
+#   describe "Checks rooms array " do
+#     it "Checks length of array containing rooms" do
+#       admin1 = Admin.new
+#       list = admin1.view_all_rooms
+#
+#       expect(list.length).must_equal 20
+#     end
+#   end
+# end
