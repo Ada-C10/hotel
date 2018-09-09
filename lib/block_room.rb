1563adaddf0652010e7772ecb41791d3bd62e123
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-08 18:00:16 -0700

    Added attr_accessor

--- lib/block_room.rb
+++ lib/block_room.rb
@@ -1,19 +1,17 @@
 require_relative 'lib_file_helper'
 
 class BlockRoom
-
-  def initialize(event_name, start_date, end_date)
+  attr_accessor :event_name, :start_date, :end_date, :rooms_blocked
+  
+  def initialize(event_name, start_date, end_date, rooms_blocked)
     @event_name = event_name
 
     # date must be entered in the format: yyyy,mm,dd
     @starting_date = format_date(start_date)
     @ending_date = format_date(end_date)
 
-    @rooms_blocked = []
+    @rooms_blocked = rooms_blocked
   end
-
-  # def add_room_to_block(room_num)
-  # end
 
   def format_date(date_string)
     parsed_date = date_string.split(",")
