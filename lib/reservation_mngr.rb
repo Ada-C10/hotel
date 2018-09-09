261d7c6fe5de972e5866ab41bc0ce118f2923ecf
Amber Lynn <griffiam@gmail.com>
2018-09-09 02:14:11 -0700

    Completed wave 1

--- lib/reservation_mngr.rb
+++ lib/reservation_mngr.rb
@@ -1,14 +1,22 @@
+require 'pry'
+
 require 'time'
 require 'date'
 
+
+require_relative 'reservation'
+
 module Hotel
 
-  class Reservation_mngr
+  class Reservation_mngr < Reservation
+
     attr_reader :rooms, :reservations
+    attr_accessor :current_res_id
 
     def initialize
       @rooms = build_room_list
       @reservations = []
+      @current_res_id = 1
     end
 
     def build_room_list
@@ -30,18 +38,30 @@ module Hotel
       # return availability
     end
 
-    # def create_reservation(check_in, check_out)
-    #   room = find_room(check_in, check_out)
-    #
-    #   reservation = Reservation.new(...)
-    #   @reservations << reservation
-    #   return reservation
-    # end
-    #
-    # def get_total(reservation_id)
-    #
-    # end
+    def create_reservation(check_in, check_out)
+      raise ArgumentError if check_out == nil
+      raise ArgumentError if check_in == nil
+      raise ArgumentError if check_out < check_in
+      raise ArgumentError if check_in > check_out
+
+      room = find_room(check_in, check_out)
+      reservation = Hotel::Reservation.new(reservation_id: @current_res_id, room: room, check_in: check_in, check_out: check_out)
+
+      @reservations << reservation
+      @current_res_id += 1
+      return reservation
+    end
+
+    def get_total(reservation_id)
+      raise ArgumentError if reservation_id.to_s !~ /\d/
+
+      @reservations.each do |reservation|
+        if reservation.reservation_id == reservation_id
+          return reservation.calculate_cost
+        end
+      end
+      return 1000
+    end
 
   end
-
 end
--- spec/reservation_mngr_spec.rb
+++ spec/reservation_mngr_spec.rb
@@ -50,18 +50,75 @@ describe 'Reservation_mngr class' do
 
     it "omits rooms that aren't available" do
       skip
-      res1 = @front_desk.create_reservation("01/03/1988", "01/10/1988")
-      res2 = @front_desk.create_reservation("01/09/1988", "01/17/1988")
+      res_1 = @front_desk.create_reservation("01/03/1988", "01/10/1988")
+      res_2 = @front_desk.create_reservation("01/09/1988", "01/17/1988")
 
       room = @front_desk.find_room("01/03/1988", "01/14/1988")
+      expect(res_1)
+      expect(res_2)
       expect(room.room_number).must_equal 3
     end
   end
 
-  # describe "create_reservation" do
-  #   before do
-  #     @front_desk = Hotel::Reservation_mngr.new()
-  #   end
-  # end
+  describe "create_reservation" do
+    before do
+      @room = Hotel::Room.new( {room_number: 1, price: 200} )
+      @front_desk = Hotel::Reservation_mngr.new()
+    end
+
+    it "throws an ArgumentError if check_out is before check_in" do
+      expect{@front_desk.create_reservation("01/10/2018", "01/09/2018")}.must_raise ArgumentError
+    end
+
+    it "throws an ArgumentError if check_in is after check_out" do
+      expect{@front_desk.create_reservation("01/18/1988", "01/17/1988")}.must_raise ArgumentError
+    end
+
+    it "throws an ArgumentError if check_in is nil" do
+      expect{@front_desk.create_reservation(nil, "01/17/1988")}.must_raise ArgumentError
+    end
+
+    it "throws an ArgumentError if check_out is nil" do
+      expect{@front_desk.create_reservation("01/18/1988", nil)}.must_raise ArgumentError
+    end
+
+    it "throws an ArgumentError if check_in and check_out are both nil" do
+      expect{@front_desk.create_reservation(nil, nil)}.must_raise ArgumentError
+    end
+
+    it "stores reservation instance to class variable - reservations" do
+      @front_desk.create_reservation("01/10/2018", "01/17/2018")
+      res = @front_desk.reservations.length
+      expect(res).must_equal 1
+    end
+
+    it "increases reservation_id by 1" do
+      expect(@front_desk.current_res_id).must_equal 1
+      @front_desk.create_reservation("01/10/2018", "01/17/2018")
+      expect(@front_desk.current_res_id).must_equal 2
+    end
+
+    it "returns a reservation" do
+      expect(@front_desk.create_reservation("01/10/2018", "01/17/2018")).must_be_kind_of Hotel::Reservation
+    end
+  end
+
+  describe "get_total" do
+    before do
+      @front_desk = Hotel::Reservation_mngr.new()
+      
+      @front_desk.create_reservation("01/10/2018", "01/17/2018")
+    end
+
+    it "throws an ArgumentError if reservation id invalid" do
+
+      expect{@front_desk.get_total("A")}.must_raise ArgumentError
+    end
+
+    it "returns reservation if id matches" do
+      expect(@front_desk.get_total(1)).must_equal 1400
+    end
+
+  end
 
 end
