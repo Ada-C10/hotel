4545cb16738fcc88cd05856aa85d2038b83daa48
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-09 00:20:29 -0700

    Remove unused lines of code and add spacing. Add refactors file.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,12 +1,11 @@
 class Reservation
-  attr_reader :date_range, :begin_date, :end_date, :room_num, :rate, :cost
+  attr_reader :date_range, :room_num, :rate, :cost
 
   def initialize(date_range, room_num, rate)
     @date_range = {}
     @date_range[:begin_date] = date_range[:begin_date]
     @date_range[:end_date] = date_range[:end_date]
     @room_num = room_num
-    raise ArgumentError if @room_num == nil
     @rate = rate
     @cost = ( date_range[:end_date] - date_range[:begin_date] ) * rate
   end
--- lib/reservation_tracker.rb
+++ lib/reservation_tracker.rb
@@ -1,7 +1,5 @@
 require 'date'
 require 'pry'
-require_relative 'reservation'
-require_relative 'dates'
 
 class ReservationTracker
   attr_reader :rooms, :all_reservations, :cost
@@ -17,11 +15,8 @@ class ReservationTracker
 
   def new_reservation(date_range)
     date_range = Dates::date_range_format(date_range)
-
     room_num = @rooms.find_available_room(occupied_rooms(date_range))
-
     rate = @rooms.rate
-
     reservation = Reservation.new(date_range, room_num, rate)
     @all_reservations << reservation
     return reservation
@@ -30,7 +25,6 @@ class ReservationTracker
 
   def reservation_list_by_date(date)
     date = Dates::date_format(date)
-
     reservation_list_by_date = @all_reservations.find_all { |reservation|
       date >= reservation.date_range[:begin_date] && date < reservation.date_range[:end_date] }
     return reservation_list_by_date
@@ -43,7 +37,8 @@ class ReservationTracker
     @all_reservations.each do |reservation|
       if Dates::date_range_comparison(date_range, reservation.date_range)
         if reservation.room_num.class == Array
-          reservation.room_num.each do |num| occupied_rooms << num
+          reservation.room_num.each do |num|
+            occupied_rooms << num
           end
         else
           occupied_rooms << reservation.room_num
@@ -57,11 +52,8 @@ class ReservationTracker
   def reserve_block(date_range, num_rooms, discount_rate)
     raise ArgumentError, "5 room limit for blocks" if num_rooms > 5
     date_range = Dates::date_range_format(date_range)
-
     room_block = @rooms.find_block_of_rooms(num_rooms, occupied_rooms(date_range))
-
     reservation = Reservation.new(date_range, room_block, discount_rate)
-
     @all_blocks << reservation
     @all_reservations << reservation
     return reservation
@@ -85,19 +77,11 @@ class ReservationTracker
     return open_rooms
   end
 
-  def first_available_block_room(date_range)
-    rooms = open_rooms_in_block(date_range)
-    return rooms.first
-  end
-
 
   def new_block_room_reservation(date_range)
     date_range = Dates::date_range_format(date_range)
-
-    room_num = first_available_block_room(date_range)
-
+    room_num = open_rooms_in_block(date_range).first
     rate = (@all_blocks.find{ |block| block.date_range == date_range }).rate
-
     reservation = Reservation.new(date_range, room_num, rate)
     @all_reservations << reservation
     @block_room_reservations << reservation
