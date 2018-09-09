a1c7fd74a4a1b55ec93f98a7e16306c354734866
Divya <divyamohanan@gmail.com>
2018-09-08 21:28:55 -0700

    Functions added to classes, one exception raised

--- lib/admin.rb
+++ lib/admin.rb
@@ -1,128 +1,123 @@
+require_relative 'reservation.rb'
+require_relative 'room.rb'
 require 'Date'
 
+
+ROOMS_PRIOR_TO_BOOKING =  [{1=>[]},{2=>[]},{3=>[]},{4=>[]},{5=>[]},{6=>[]},{7=>[]},{8=>[]},
+{9=>[]},{10=>[]},{11=>[]},{12=>[]},{13=>[]},{14=>[]},{15=>[]},{16=>[]},{17=>[]},
+{18=>[]},{19=>[]},{20=>[]}]
+
 class Admin
-  def initialize
-  @available_rooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-  @reserved_rooms = []
-  @reservation_id = 0
+
+  @@rooms = ROOMS_PRIOR_TO_BOOKING
+
+  # def initialize
+  #   @check_array = []
+  # end
+
+  def view_all_rooms
+    puts @@rooms
   end
 
-  def view_available_rooms
-    puts "AVAILABLE ROOMS:"
-    puts @available_rooms
+  def reserve(room_number, check_in_date, check_out_date)
+    if Date.parse(check_in_date).class != Date ||
+      Date.parse(check_out_date).class != Date ||
+      Date.parse(check_in_date) > Date.parse(check_out_date)
+       raise StandardError, "Dates are invalid!"
+    end
+    # if @check_array != []
+    #   @check_array.each do |array|
+    #     if array[0] == room_number &&
+    #        Date.parse((array[1])[0]) >= check_in_date &&
+    #         Date.parse((array[1])[1]) <= check_out_date
+    #           raise Error, "Dates booked!!"
+    #     end
+    #   end
+    # end
+    Reservation.new(room_number, check_in_date, check_out_date)
+    rooms_sent = @@rooms
+    Room.new(room_number, check_in_date, check_out_date, rooms_sent)
+    @@rooms = Room.rooms
   end
 
-  def reserve(number_of_rooms_needed)
-      return @reserved_rooms
-  end
-
-  def view_current_reservations
-    puts "RESERVED ROOMS"
-    puts @reserved_rooms
-  end
-
-  def view_details_of_reservation(reservation_id)
-
-
+  def rooms_reserved_by_date(date)
+    @@rooms.each do |hash|
+      hash.each do |key, value|
+        value.each do |date_range|
+          if Date.parse(date) > Date.parse(date_range[0]) &&
+            Date.parse(date) < Date.parse(date_range[1])
+            puts "Room number #{key} is booked during this date"
+          end
+        end
+      end
+    end
   end
 
   def print_total_cost_per_reservation(reservation_id)
-  end
-
-  def calculate_number_of_days
-
-  end
-  def create_id
-
-  end
-  def cost(id)
-
-  end
-  def booking_details
-  end
-
-  def reservations_on_a_date()
-end
-
-class Room
-  def initialize
-  @available_rooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-  @reserved_rooms = []
-  @reservation_id = 0
-  end
-  def available_rooms
-
-  end
-
-  @cost
-
-  def check_status()
-  end
-
-  def reserved_rooms
-  end
-
-  def reservation_list
-  end
-end
-
-class Reservation
-  @check_in_date
-  @check_out_date
-  @total_cost
-  @user_id
-  def available_rooms
-  end
-
-  def reserved_rooms
-  end
-
-  def reserve(number_of_rooms_needed)
-
-    if number_of_rooms_needed > @available_rooms.length
-      puts "We don't have enough rooms"
-    else
-      rooms_selected = (0..(@available_rooms.length-1)).to_a.sample(number_of_rooms_needed)
-      number_of_rooms_needed.times do |i|
-        @reserved_rooms << rooms_selected[i]
-        @available_rooms.delete(rooms_selected[i])
+    Reservation.reservations.each do |array|
+      if array[3] == reservation_id
+        puts array[4]
       end
     end
-      return @reserved_rooms
   end
 
-  def calculate_number_of_days
-    puts "What is your starting date yyyy-mm-dd"
-    start_date_input = gets.chomp
-    start_date = Date.parse(start_date_input)
-    puts start_date
-    puts "What is your ending date yyyy-mm-dd"
-    end_date_input = gets.chomp
-    end_date = Date.parse(end_date_input)
-    puts end_date
-    number_of_days = end_date - start_date
-    return number_of_days.to_i
-  end
-
-  def create_id
-    @@reservation_id += 1
-    return @@reservation_id
-  end
-  def cost
-    days = calculate_number_of_days
-    return (days - 1) * 200
+  def check_availability (check_in_date, check_out_date = nil)
+    if check_out_date == nil
+      date = check_in_date
+      @@rooms.each do |hash|
+        hash.each do |key,value|
+          value.each do |date_range|
+            if Date.parse(date) < Date.parse(date_range[0]) || Date.parse(date) >= Date.parse(date_range[1])
+             puts "Date is available for room number #{key}"
+            # else
+            #   @check_array << [key,value]
+            end
+          end
+          if value == []
+            puts "Date is available for room number #{key}"
+          end
+        end
+        return false
+      end
+   else
+    #unavailable = 0
+    @@rooms.each do |hash|
+      hash.each do |key, value|
+        if value != []
+          value.each do |date_range|
+            if Date.parse(check_in_date) < Date.parse(date_range[0]) && Date.parse(check_out_date) <= Date.parse(date_range[0]) ||
+              Date.parse(check_in_date) >= Date.parse(date_range[1]) && Date.parse(check_out_date) > Date.parse(date_range[1])
+              puts "Room #{key} is available"
+            # else
+            #   @check_array << [key,value]
+            end
+          end
+        else
+          puts "Room #{key} is available"
+        end
+        if unavailable == 20
+          puts "No rooms available!"
+        end
+      end
+    end
   end
 end
+end
 
-# class BlockRooms
-#   @rooms = []
-#   @date_range
-#   @cost
-#end
+test = Admin.new
+test.reserve(7,"2018-08-09","2018-08-11")
+test.reserve(7,"2018-08-10","2018-08-11")
+# test.print_total_cost_per_reservation("2018/1")
+# # #
+# test.reserve(8,"2018-06-21","2018-07-05")
+# #print Reservation.reservations
+# test.print_total_cost_per_reservation("2018/2")
+#test.view_all_rooms
 
-#end
-admi = Admin.new
-admi.view_available_rooms
-admi.reserve(4)
-admi.view_available_rooms
-puts admi.calculate_number_of_days
+
+test.view_all_rooms
+#test.rooms_available_by_date("2018-08-19")
+test.rooms_reserved_by_date("2018-08-14")
+#test.check_availability("2018-08-17")
+#test.print_total_cost_per_reservation(3)
+# test.view_reservations_on_a_date("2018-08-24")
---
+++ lib/reservation.rb
@@ -0,0 +1,38 @@
+
+require 'Date'
+COST = 200
+
+class Reservation
+  @@num = 0
+  @@array_of_reservations = []
+  def initialize(room_number, check_in_date, check_out_date)
+    @room_number = room_number
+    @check_in_date = check_in_date
+    @check_out_date = check_out_date
+    @@num += 1
+    @@reservation_id = "2018/#{@@num}"
+    @cost = cost
+    @@array_of_reservations << [@room_number, @check_in_date, @check_out_date, @@reservation_id, @cost]
+  end
+
+  attr_writer :room_number, :check_in_date, :check_out_date, :reservation_id
+
+  def calculate_number_of_days
+      start_date = Date.parse(@check_in_date)
+      end_date = Date.parse(@check_out_date)
+      number_of_days = end_date - start_date
+      return number_of_days.to_i
+  end
+
+  def cost
+    if @check_in_date != nil && @check_out_date != nil
+      days = calculate_number_of_days
+      total = (days - 1) * COST
+    end
+    return total
+  end
+
+  def self.reservations
+    return @@array_of_reservations
+  end
+end
