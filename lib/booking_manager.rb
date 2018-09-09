96696079b574b93916bdc38dcc4857a29f48425c
Kat Perreira <kat.perreira@gmail.com>
2018-09-09 12:04:19 -0700

    refactored make_reservation, added attr accessor

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -4,7 +4,8 @@ require 'pry'
 class BookingManager
 
   attr_reader :rooms
-  attr_reader :reservations
+  attr_accessor :reservations
+  attr_accessor :make_reservation
 
   def initialize
     @reservations = []
@@ -14,34 +15,43 @@ class BookingManager
       @rooms << i + 1
       i += 1
     end
+    @make_reservation
   end
 
   def find_available_room(start_date, end_date)
     requested_dates = (start_date...end_date).to_a
-    if @reservations.empty?
-      return rooms.first
-    end
     # use some boolean logic and intersection to call out errors for conflit
+    room = 237
     room_count = 1
     while room_count < 20
+      if @reservations.empty?
+        room = 1
+        return room
+      end
       @reservations.each_with_index do |reservation, index|
-        if reservation.room == (index + 1)
-          if (reservation.reservation_dates & requested_dates).length > 0
-            room_count += 1
-          else
-            room = index + 1
-            return room
-          end
+        if reservation.room == (index + 1) && (reservation.reservation_dates & requested_dates).length == 0
+          room = index + 1
+          return room
+        elsif reservation.room == (index + 1)
+          room_count += 1
+        elsif reservation.room != (index + 1)
+          room = index + 1
+          return room
         end
       end
     end
-     # raise StandardError.new("No room available")
+    raise StandardError.new("No room available")
   end
+end
 
-  def make_reservation(start_date, end_date)
-    room = find_available_room(start_date, end_date)
+
+def make_reservation(start_date, end_date)
+
+  room = find_available_room(start_date, end_date)
+  if @rooms.include? room
     reservation = Reservation.new(room, start_date, end_date)
     @reservations << reservation
-    return reservation
+    # binding.pry
+    @make_reservation
   end
 end
