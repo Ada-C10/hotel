321236dfc918ff5333f3c12b2b28ebb6ac56a9ce
Layla <layla.bristol@gmail.com>
2018-09-09 14:14:41 -0700

    Add Reservation and its test

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,5 +1,17 @@
-class Reservation
+# every reservation has a room and date range
+module Hotel
+  class Reservation
+    attr_reader :room_number, :room_price, :stay_duration
 
-  def initilize
+    def initialize(room_number, room_price, stay_duration)
+      @room_number = room_number
+      @room_price = room_price
+      @stay_duration = stay_duration
+    end
+
+    def stay_total_cost
+      return @room_price * @stay_duration.duration
+    end
+
   end
 end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,10 +1,38 @@
 require_relative 'spec_helper'
 require_relative '../lib/reservation'
-
+require_relative '../lib/date_range'
 
 describe "a reservation" do
-
-  it "create a method" do
+  before do
+    room_number = 12
+    room_price = 200
+    check_in = Date.new(2018, 8, 24)
+    check_out = check_in + 5
+    stay_duration = Hotel::DateRange.new(check_in, check_out)
+    @reservation = Hotel::Reservation.new(room_number, room_price, stay_duration)
   end
+
+  it "creates a new reservation" do
+    @reservation.must_be_kind_of Hotel::Reservation
+  end
+
+  it "each reservation has a room" do
+    @reservation.must_respond_to :room_number
+  end
+
+  it "each reservation has a room" do
+    @reservation.must_respond_to :room_price
+  end
+
+  it "each reservation has a stay duration" do
+    @reservation.must_respond_to :stay_duration
+  end
+
+  it "calculates total cost of stay" do
+    @reservation.stay_total_cost.must_equal 1000
+  end
+
+
+
 
 end
