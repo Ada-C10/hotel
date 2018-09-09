fe5e78cd1deb02e8718066bc61e6671d845ddf59
Carly Reiter <yepodacarly@gmail.com>
2018-09-09 09:24:00 -0700

    Added comments to each of the methods in both library files.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,6 +5,7 @@ class Reservation
 
   attr_reader :start_date, :end_date, :room
 
+  #receives the start_date, end_date, and room info from ReservationMaker
   def initialize (start_date, end_date, room)
     @start_date = start_date
     @end_date = end_date
@@ -13,11 +14,13 @@ class Reservation
     @cost = cost
   end
 
+  #calculates the duration of the reservation, to be used to calculate cost in the below method
   def reservation_duration
     duration = @end_date - @start_date
     return duration.to_i
   end
 
+  #The below method works when the rooms are all $200 per night.  
   def cost
     total_cost = @reservation_duration * 200
     return total_cost.to_f
--- lib/reservation_maker.rb
+++ lib/reservation_maker.rb
@@ -11,24 +11,29 @@ class ReservationMaker
     @start_date = start_date
     @end_date = end_date
 
+    #Makes sure dates entered are realistic.  A start date after the end date is not.
     if @end_date < @start_date
       raise ArgumentError.new ("The start date cannot be after the end date.")
     end
   end
 
+  # This makes it so the @@reservations data is cumulative and keeps track of all reservations ever made with the program.
   def self.reservations
     @@reservations
   end
 
+  # Dan added this part so that the above would work with the testing setup.
   def self.reset
     @@reservations = []
   end
 
+  # lists all rooms in the hotel
   def rooms
     rooms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     return rooms
   end
 
+  # creates a list of all Booked Rooms (or rooms that are unavailable to be booked by the current customer) during the time frame requested.
   def booked_rooms
     booked_rooms = []
     unless @@reservations.nil?
@@ -42,6 +47,7 @@ class ReservationMaker
     return booked_rooms
   end
 
+  # Creates a list of Available Rooms for the current customer by removing the booked rooms from the main rooms list and returning a list of rooms available to be booked by the customer.
   def available_rooms
     available_rooms = []
     booked_room_list = booked_rooms
@@ -58,6 +64,7 @@ class ReservationMaker
     return available_rooms
   end
 
+  # If there are rooms available, a reservation will be created using a randomly chosen room number from the Available Rooms list.
   def create_reservation
     room = available_rooms.sample
     if room.nil?
