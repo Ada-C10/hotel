3288ef2af0d7c5bb7b613134f5a51033e8d71005
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-09 19:43:23 -0700

    Implement Reservation#reservation_cost

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,13 +1,20 @@
+require_relative 'date_logic'
 require_relative 'room_booker'
 
 module BookingLogic
   class Reservation
+    include DateLogic
     attr_reader :room, :check_in, :check_out
 
     def initialize(room, check_in, check_out)
       @room = room
       @check_in = check_in
       @check_out = check_out
+    end
+
+    def reservation_cost
+      days_reserved = check_out - check_in
+      return days_reserved.to_i * room.cost
     end
   end
 end
