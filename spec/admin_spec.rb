309240ed87dac2c1eeae33ac1caecb59d097c2d4
Divya <divyamohanan@gmail.com>
2018-09-09 17:14:50 -0700

    89% test coverage, one error

--- spec/admin_spec.rb
+++ spec/admin_spec.rb
@@ -1,53 +1,80 @@
 require_relative 'spec_helper'
 
-# describe "Wave 1 + 2 + 3" do
-#   before do
-#     test = Admin.new
-#   end
+describe "#initialize" do
+  it "Initializes a class variable that stores an empty string" do
+    test = Admin.new
 
-  describe "#initialize" do
-    it "Initializes a class variable that stores an empty string" do
-      test = Admin.new
+    expect(test).must_respond_to :blocked_start_date
+    expect(test.blocked_start_date).must_be_nil
 
-      expect(test).must_respond_to :blocked_start_date
-      expect(test.blocked_start_date).must_be_nil
-
-      expect(test).must_respond_to :blocked_end_date
-      expect(test.blocked_start_date).must_be_nil
-    end
-   end
-
-  describe "block rooms" do
-
-    it "Returns an array of blocked rooms" do
-      test = Admin.new
-      check = test.block_rooms(3, "2018-10-01", "2018-10-15")
-
-        expect(check.length).must_equal 3
-    end
+    expect(test).must_respond_to :blocked_end_date
+    expect(test.blocked_start_date).must_be_nil
   end
+ end
 
-  describe "Checks if blocked room is booked" do
-    it "Confirms a room from blocked rooms is booked" do
-      test = Admin.new
-      check = test.block_rooms(3, "2018-10-01", "2018-10-15")
-      new = test.book_blocked_room
+describe "block rooms" do
 
-      expect(new.length).must_equal 2
-    end
+  it "Returns an array of blocked rooms" do
+    test = Admin.new
+    check = test.block_rooms(3, "2018-10-01", "2018-10-15")
+
+      expect(check.length).must_equal 3
   end
+end
 
-#end
+describe "Checks if blocked room is booked" do
+  it "Confirms a room from blocked rooms is booked" do
+    test = Admin.new
+    check = test.block_rooms(3, "2018-10-01", "2018-10-15")
+    new = test.book_blocked_room
 
+    expect(new.length).must_equal 2
+  end
+end
 
+describe "Checks rooms array " do
+  it "Checks length of array containing rooms" do
+    test = Admin.new
+    list = test.view_all_rooms
 
-# describe "Checks that array of rooms contains all rooms" do
-#   describe "Checks rooms array " do
-#     it "Checks length of array containing rooms" do
-#       admin1 = Admin.new
-#       list = admin1.view_all_rooms
-#
-#       expect(list.length).must_equal 20
-#     end
-#   end
-# end
+    expect(list.length).must_equal 20
+  end
+end
+
+describe "Prints cost per reservation" do
+  it "Checks the total cost for a reservation" do
+    person = Admin.new
+    person.reserve(3, "2018-11-01", "2018-11-15")
+    pay = person.print_total_cost_per_reservation
+
+    expect(pay).must_equal 2600
+  end
+end
+
+describe "Checks availability of room according to dates" do
+  it "Checks room availability" do
+    guy = Admin.new
+    answer = guy.check_availability("2018-09-01", "2018-09-11")
+
+    expect(answer).must_be_kind_of String
+  end
+end
+
+describe "Checks rooms reserved for a certain date" do
+  it "Checks if a room is reserved" do
+    new_guy = Admin.new
+    new_guy.reserve(3,"2018-12-03","2018-12-13")
+    answer = new_guy.rooms_reserved_by_date("2018-12-04")
+
+    expect(answer).must_be_kind_of String
+  end
+end
+
+describe "Checks to make sure an error is raised if invalid date range is given" do
+  it "raises Error while booking invalid dates" do
+    old_guy = Admin.new
+    mistake = old_guy.reserve(9,"2018-12-24","2018-12-15")
+
+    expect(mistake).must_raise StandardError
+  end
+end
