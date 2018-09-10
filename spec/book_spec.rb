20e9c2f2502441261bf9434d84009f67c2d7eb33
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-10 03:28:05 -0700

    Book class can now identify reservation and block conflicts

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -10,76 +10,87 @@ describe "HotBook::Book class" do
   let(:block) { HotBook::Block.new(daterange, ["1", "2", "3", "4", "5"]) }
   let(:rooms) { ["1", "2", "3", "4", "5"] }
   let(:load_block) { book.new_block(daterange, rooms) }
+  let(:date) {Date.parse("apr_15")}
+  let(:overlaprange) { HotBook::DateRange.new( start_date: "apr_08",
+                                               end_date: "apr_16") }
+
+  before do
+    book # test reservation data is loaded into @reservations
+    block = load_block # a single block, rooms 1-5, 4/15-4/30, is loaded into @blocks
+    block.disable("1") # This manually mimics what would happen if test reservations
+    block.disable("2") # with note "This is a block (private) reservation"
+    block.disable("3") # were created as block reservations
+    block.disable("4")
+  end
 
   describe "new_reservation method" do
-
     it "creates a Reservation object and stores in @reservations" do
       expect(new_reservation).must_be_instance_of HotBook::Reservation
-      previous_size = book.reservations.size
-      3.times { book.new_reservation(daterange) }
-      expect((book.reservations).size).must_equal previous_size + 3
+      expect(book.reservations.size).must_equal 11
+      3.times {book.new_reservation(daterange) }
+      expect(book.reservations.size).must_equal 11 + 3
     end
 
-    it "correctly raises error if room is already reserved during daterange" do
-      book
-      load_block
-      expect{book.new_reservation(daterange, room_number: "20")}.must_raise HotBook::RoomIsTakenError
+    it "raises unique errors if room is booked or blocked during daterange" do
+      expect{book.new_reservation(daterange, room_number: "6")}.must_raise HotBook::RoomIsTakenError
       expect{book.new_reservation(daterange, room_number: "1")}.must_raise HotBook::RoomIsBlockedError
     end
-
-    it "correctly raises error if room is part of a block during daterange" do
-
-    end
-
   end
-  #
-  # describe "suggested_room method" do
-  #   it "returns the correct room number" do
-  #     expect(book.suggested_room(daterange)).must_equal "4"
-  #   end
-  # end
-  #
-  # describe "default_rate method" do
-  #   it "does the right thing" do
-  #     expect(book.default_rate).must_equal 200.0
-  #   end
-  # end
 
-### PROJECT REQ: User can access the list of reservations for a specific date
-  # describe "list_reservations" do
-  #   it "raises ArgError if the arg isn't a Date" do
-  #     baddate = "some_string"
-  #     expect{book.list_reservations(baddate)}.must_raise ArgumentError
-  #   end
-  #
-  #   it "returns an array of reservations " do
-  #     note = "This is the first reservation"
-  #     date = Date.parse("apr_6")
-  #     list = book.list_reservations(date)
-  #     expect(list).must_be_instance_of Array
-  #     expect(list.first.notes).must_equal note
-  #   end
-  # end
+  describe "suggest_room method" do
+    it "returns the first publicly available room number that isn't booked or blocked" do
+      expect(book.suggest_room(daterange)).must_equal "7"
+    end
+  end
 
-  # describe "room_is_bookable? method" do
-  #   it "correctly checks if a room has a reservation on that date" do
-  #     room_number = "10"
-  #     expect(book.room_is_bookable?(room_number, daterange)).must_equal true
-  #     room_number = "2"
-  #     expect{book.room_is_bookable?(room_number, daterange)}.must_raise StandardError
-  #   end
-  # end
+  describe "list_by_nights method" do
+    it "returns an array of reservations excluding checkout date" do
+      expect(book.list_by_nights(date).size).must_equal 1
+      expect(book.list_by_nights(date)[0].room_number).must_equal "6"
+    end
+  end
 
-  # describe "list_available_room_numbers method" do
-  #   it "returns an array that's a subset (or equal to) all room numbers" do
-  #     all = hotel.room_numbers
-  #     list = book.list_available_room_numbers(daterange)
-  #     expect(book.reservations.size).must_equal 6
-  #     expect((all - list).size).must_equal (all.size - list.size)
-  #     expect(list.first).must_equal "4"
-  #     expect(list.last).must_equal "20"
-  #   end
-  # end
+  describe "public_avail_rooms method" do
+    it "returns an array of room numbers that aren't booked or blocked" do
+      list = %w(7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+      expect(book.public_avail_rooms(overlaprange)).must_equal list
+      book.new_reservation(overlaprange)
+      list.shift
+      expect(book.public_avail_rooms(overlaprange)).must_equal list
+    end
+  end
+
+  describe "room_taken? method" do
+    it "given daterange, lists rezzies with conflict, " \
+    "and tells you if the given room is in that list" do
+      expect(book.room_taken?(daterange, "6")).must_equal true
+      expect(book.room_taken?(daterange, "1")).must_equal false
+    end
+  end
+
+  describe "room_blocked? method" do
+    it "given daterange, lists blocks with conflict, " \
+    "and tells you if the given room is in that list" do
+      expect(book.room_blocked?(daterange, "1")).must_equal true
+      expect(book.room_blocked?(daterange, "6")).must_equal false
+    end
+  end
+
+  describe "conflicting_reservations method" do
+    it "returns an array of reservations with a daterange conflict" do
+      array = book.conflicting_reservations(daterange)
+      expect(array.size).must_equal 1
+      expect(array[0]).must_be_instance_of HotBook::Reservation
+    end
+  end
+
+  describe "conflicting_blocks method" do
+    it "returns an array of blocks with a daterange conflict" do
+      array = book.conflicting_blocks(daterange)
+      expect(array.size).must_equal 1
+      expect(array[0]).must_be_instance_of HotBook::Block
+    end
+  end
 
   describe "validate(type, var) method" do
     it "raises ArgError for invalid inputs" do
