6f851e331653831545a24eb4502e3da78a364fd4
Karis Kim <ouniiq@hotmail.com>
2018-09-10 04:14:24 -0700

    changed file names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,6 @@ require 'awesome_print'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
-require_relative '../lib/reservation'
+require_relative '../lib/reservation_creator'
 require_relative '../lib/rooms'
 require_relative '../lib/booking_system'
