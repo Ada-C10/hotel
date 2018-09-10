43e3e122c8ed5f97c6238dccca56850a9efce543
Hayden Williams <hwilliams.deva@gmail.com>
2018-09-09 19:49:25 -0700

    Move all date related methods DateLogic module, implement RoomBooker#reserve_available_room, and refactor RoomBooker#list_available_rooms and RoomBooker#list_reservations, move RoomBooker#reservation_cost to Reservation class

--- lib/room_booker.rb
+++ lib/room_booker.rb
@@ -1,4 +1,5 @@
 require 'date'
+require_relative 'date_logic'
 require_relative 'reservation'
 
 require 'pry'
@@ -37,22 +38,24 @@ module BookingLogic
 
     def new_reservation(room_id, check_in, check_out)
       room = find_room_by_id(room_id)
-      reservations << BookingLogic::Reservation.new(room, check_in, check_out)
+      new_reservation = BookingLogic::Reservation.new(room, check_in, check_out)
+      reservations << new_reservation
+      return new_reservation
     end
 
-    def date_range_include?(reservation, date)
-      if (reservation.check_in..reservation.check_out).cover?(date)
-        return true
-      else
-        return false
-      end
-    end
+    # def date_range_include?(reservation, date)
+    #   if (reservation.check_in..reservation.check_out).cover?(date)
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # end
 
     def list_reservations(date)
       list_of_reservations = []
 
       reservations.each do |reservation|
-        if date_range_include?(reservation, date)
+        if DateLogic.date_range_include?(reservation, date)
           list_of_reservations << reservation
         end
       end
@@ -68,40 +71,41 @@ module BookingLogic
       end
     end
 
-    def reservation_cost(room_id, check_in)
-      reservation = find_reservation(room_id, check_in)
-      days_reserved = reservation.check_out - reservation.check_in
-      return days_reserved.to_i * reservation.room.cost
-    end
+    # def reservation_cost(room_id, check_in)
+    #   reservation = find_reservation(room_id, check_in)
+    #   days_reserved = reservation.check_out - reservation.check_in
+    #   return days_reserved.to_i * reservation.room.cost
+    # end
 
-    def create_date_range_array(check_in, check_out)
-      date_range_array = check_in...check_out
-      return date_range_array.to_a
-    end
+    # def create_date_range_array(check_in, check_out)
+    #   date_range_array = check_in...check_out
+    #   return date_range_array.to_a
+    # end
 
-    def date_ranges_overlap?(
-      existing_check_in,
-      existing_check_out,
-      new_check_in,
-      new_check_out)
+    # def date_ranges_exclusive?(
+    #   existing_reservation,
+    #   new_check_in,
+    #   new_check_out)
+    #
+    #   existing_range_array = existing_reservation.date_range_array
+    #   new_range_array = new_reservation.date_range_array
+    #
+    #   intersecting_dates = existing_range_array & new_range_array
+    #
+    #   if intersecting_dates.empty?
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # end
 
-      existing_range = create_date_range_array(existing_check_in, existing_check_out)
-      new_range = create_date_range_array(new_check_in, new_check_out)
 
-      intersecting_dates = existing_range & new_range
 
-      if intersecting_dates.empty?
-        return true
-      else
-        return false
-      end
-    end
-
-    def list_available_rooms(date)
+    def list_available_rooms(check_in, check_out)
       reserved_rooms = []
 
       reservations.each do |reservation|
-        if date_range_include?(reservation, date)
+        unless DateLogic.date_ranges_exclusive?(reservation, check_in, check_out)
           reserved_rooms << reservation.room
         end
       end
@@ -109,6 +113,13 @@ module BookingLogic
       available_rooms = rooms - reserved_rooms
 
       return available_rooms
+    end
+
+    def reserve_available_room(check_in, check_out)
+      available_rooms = list_available_rooms(check_in, check_out)
+      room = available_rooms.first
+
+      return new_reservation(room.id, check_in, check_out)
     end
 
   end
