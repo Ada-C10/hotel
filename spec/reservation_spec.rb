048f3daead92d8f8c3b8037fe97cc6b71362f547
Kat Perreira <kat.perreira@gmail.com>
2018-09-10 06:23:16 -0700

    Refactored whitespace

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -33,32 +33,32 @@ describe 'Reservation' do
     end
 
 
-      it 'parses the reservation_dates within check in and check out and converts them to a list of reservation_dates' do
-        expect(general_reservation.reservation_dates).must_be_kind_of Array
-        expect(general_reservation.reservation_dates.length).must_equal 6
-        expect(general_reservation.reservation_dates[0]).must_equal check_in
-      end
-
-
-      it 'Raises a standard error if check out date is before check in date' do
-        expect{Reservation.new(room, check_in, throwback_date)}.must_raise StandardError
-      end
-
-  end
-
-    describe "#stay_duration" do
-      it 'Calculates the amount of time that the reservation is active' do
-        #general reservation for 6 nights
-        expect (general_reservation.stay_duration).must_equal 6
-      end
-    end
-
-    describe "#total_charge" do
-      it 'Charges 200.00 a night for a reservation' do
-        # general reservation is for 7 days, 6 nights, 6 * 200 = 1200
-        expect(general_reservation.total_charge(6)).must_equal 1200
-      end
+    it 'parses the reservation_dates within check in and check out and converts them to a list of reservation_dates' do
+      expect(general_reservation.reservation_dates).must_be_kind_of Array
+      expect(general_reservation.reservation_dates.length).must_equal 6
+      expect(general_reservation.reservation_dates[0]).must_equal check_in
     end
 
 
+    it 'Raises a standard error if check out date is before check in date' do
+      expect{Reservation.new(room, check_in, throwback_date)}.must_raise StandardError
+    end
+
   end
+
+  describe "#stay_duration" do
+    it 'Calculates the amount of time that the reservation is active' do
+      #general reservation for 6 nights
+      expect (general_reservation.stay_duration).must_equal 6
+    end
+  end
+
+  describe "#total_charge" do
+    it 'Charges 200.00 a night for a reservation' do
+      # general reservation is for 7 days, 6 nights, 6 * 200 = 1200
+      expect(general_reservation.total_charge(6)).must_equal 1200
+    end
+  end
+
+
+end
