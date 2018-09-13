a79583be88e098a9406751ab09fb8106b9ca8619
Karis Kim <ouniiq@hotmail.com>
2018-09-12 23:04:32 -0700

    submitting original

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -2,121 +2,69 @@ require_relative 'reservation_creator'
 require 'pry'
 
 module Hotel
-  class BookingSystem < ReservationCreator
-    attr_reader :num_of_rooms, :reservations, :list_of_reservations
+  class BookingSystem
+    attr_reader :rooms, :reservations, :dates
 
     def initialize
-      @reservations = {}
-      @num_of_rooms = 20
-      @list_of_reservations = []
+      @reservations = []
+      @rooms = [*1..20]
+      @dates = []
     end
 
-    def any_rooms(date)
-      if @reservations[date] == nil
-        @reservations[date] = {}
-        return true
-      elsif @reservations[date].keys.length < @num_of_rooms
-        return true
-      else
-        return false
-      end
+    def make_reservation(check_in_date:, check_out_date:)
+      # TODO find a room that's available
+
+      room_number = get_available_room(check_in_date: check_in_date, check_out_date: check_out_date)
+
+      reservation = ReservationCreator.new(check_in_date: check_in_date, check_out_date: check_out_date, room_number: room_number)
+
+
+
+      # @dates.each do |date_with_room|
+      #   date
+      #     if reservation.date_range.include?(date) && reservation.room_number == room
+      #
+      #       reservation.room_number += 1
+      #     end
+      #     binding.pry
+      #   end
+      # end
+
+
+      @reservations << reservation
+
+      return reservation
     end
 
-    def reserve_room(date, room_number)
-      if @reservations[date] == nil
-        @reservations[date] = {}
-      end
-      if @reservations[date][room_number] == true
-        return false
-      end
-
-      @reservations[date][room_number] = true
-      # ouput = {180904=>{0=>true}
-
-      return true
+    def get_available_room(check_in_date:, check_out_date:)
+      return @rooms.first
     end
 
-    def reserve_room_for_date_range(check_in_date:, check_out_date:)
-      space = can_make_reservation(check_in_date: check_in_date, check_out_date: check_out_date)
-      if space == true
-
-        available_room = show_available_rooms_in_range(check_in_date: check_in_date, check_out_date: check_out_date).first
-        date = check_in_date
-
-        while date != check_out_date
-          reserved = reserve_room(date, available_room)
-          date += 1
-        end
-      else
-        raise ArgumentError, "there are no more rooms"
-      end
-
-      reservation = ReservationCreator.new(check_in_date: check_in_date, check_out_date: check_out_date, room_number: available_room)
-      @list_of_reservations << reservation
+    def list_all_rooms
+      return @rooms
     end
 
-    def automatically_reserve_and_return_room_number(date)
-      room_number = 1
-      room_reserved = reserve_room(date, room_number)
-      if room_reserved == true
-        return room_number
-      end
-
-      while room_reserved == false
-        room_number += 1
-        if room_number > @num_of_rooms
-          raise ArgumentError, "unavailable room"
-        end
-
-        room_reserved = reserve_room(date, room_number)
-      end
-
-      return room_number
-    end
-
-    def can_make_reservation(check_in_date:, check_out_date:)
-
-      date = check_in_date
-
-      while date != check_out_date
-        if any_rooms(date) == false
-          return false
-        else
-          date += 1
-        end
-      end
-
-
-      return true
-    end
-
-    def show_reserved_rooms(date)
-      if @reservations[date] == nil
-        return []
-      end
-
-      return @reservations[date].keys
-    end
-
-    def show_available_rooms_in_range(check_in_date:, check_out_date:)
-      rooms = [*1..@num_of_rooms]
-      # rooms = rooms + group2
-      date = check_in_date
-
-      while date != check_out_date
-        rooms += show_reserved_rooms(date)
-          date += 1
-          # binding.pry
-      end
-
-      return rooms.find_all {|room| rooms.count(room) == 1}
-    end
+    # def add_dates_with_rooms
+    #
+    #   @reservations.each do |booking|
+    #
+    #     booking.date_range.each do |date|
+    #       date_with_room = {}
+    #       # binding.pry
+    #
+    #       date_with_room[date] = booking.room_number
+    #       @dates << date_with_room
+    #     end
+    #   end
+    #
+    #   return @dates
+    # end
 
     def list_reservations_by_date(date)
       specific_date = Date.parse("#{date}")
 
       bookings_by_date = []
-      @list_of_reservations.each do |booking|
+      @reservations.each do |booking|
         dates = booking.date_range
         if dates.include?(specific_date)
           bookings_by_date << booking
@@ -124,10 +72,6 @@ module Hotel
       end
 
       return bookings_by_date
-    end
-
-    def list_all_rooms
-      return [*1..@num_of_rooms]
     end
   end
 end
