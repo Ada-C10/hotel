c2f08f78f3673b640c420e3777f904dd5b977f6e
Karis Kim <ouniiq@hotmail.com>
2018-09-12 23:05:15 -0700

    submitting original

--- spec/booking_system_spec.rb
+++ spec/booking_system_spec.rb
@@ -3,6 +3,7 @@ require_relative 'spec_helper'
 describe "BookingSystem class" do
   before do
     @booking = Hotel::BookingSystem.new
+    @reservation = @booking.make_reservation(check_in_date: 180904, check_out_date: 180907)
   end
 
   it "create BookingSystem class" do
@@ -10,115 +11,7 @@ describe "BookingSystem class" do
   end
 
   it "initialize method" do
-    expect(@booking.reservations).must_be_kind_of Hash
-  end
-
-  it "any rooms should return true when no reservations" do
-    expect(@booking.any_rooms(180904)).must_equal true
-  end
-
-  it "any rooms should return true when only 1 reservation" do
-    @booking.reserve_room(180904, 1)
-    expect(@booking.any_rooms(180904)).must_equal true
-  end
-
-  it "returns false if room is taken for that date" do
-    numbers = [*1..20]
-
-    numbers.each do |num|
-      @booking.reserve_room(180904, num)
-      num += 1
-    end
-
-    expect(@booking.any_rooms(180904)).must_equal false
-  end
-
-  it "returns true when no reservation" do
-
-    check_in_date = 180904
-    check_out_date = 180905
-    result = @booking.can_make_reservation(check_in_date: check_in_date, check_out_date: check_out_date)
-    expect(result).must_equal true
-  end
-
-  it "returns first room number when no other reservations" do
-    result = @booking.automatically_reserve_and_return_room_number(180904)
-
-    expect(result).must_equal 1
-  end
-
-  it "returns next available room number" do
-    @booking.reserve_room(180904, 1)
-    result = @booking.automatically_reserve_and_return_room_number(180904)
-
-    expect(result).must_equal 2
-  end
-
-  it "raises argument error when all rooms are booked" do
-    numbers = [*1..20]
-
-    numbers.each do |num|
-      @booking.reserve_room(180904, num)
-      num += 1
-    end
-
-    expect{(@booking.automatically_reserve_and_return_room_number(180904))}.must_raise ArgumentError
-  end
-
-  it "return empty array when no rooms are reserved" do
-    expect(@booking.show_reserved_rooms(180904)).must_equal []
-  end
-
-  it "return an array with one room when one room is reserved" do
-    @booking.reserve_room(180904, 1)
-
-    expect(@booking.show_reserved_rooms(180904).length).must_equal 1
-    expect(@booking.show_reserved_rooms(180904)).must_equal [1]
-  end
-
-  it "return an array of rooms that are available on all days" do
-    @booking.reserve_room(180904, 1)
-    # @booking.reserve_room(180905, 1)
-
-    result = @booking.show_available_rooms_in_range(check_in_date: 180904, check_out_date: 180905)
-    expect(result).must_equal [*2..20]
-  end
-
-  it "return an empty array if no rooms available on all days" do
-    room_number = [*1..20].each do |num|
-      @booking.reserve_room(180904, num)
-      num += 1
-    end
-
-
-    result = @booking.show_available_rooms_in_range(check_in_date: 180904, check_out_date: 180905)
-    expect(result).must_equal []
-  end
-
-  it "return array of available rooms when we reserve it in the date range" do
-    @booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180905)
-
-    result = @booking.show_available_rooms_in_range(check_in_date: 180904, check_out_date: 180905)
-    expect(result.length).must_equal 19
-    expect(result).must_equal [*2..20]
-  end
-
-  it "return empty array when we reserve it in the date range" do
-    20.times do
-      @booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180905)
-    end
-
-    result = @booking.show_available_rooms_in_range(check_in_date: 180904, check_out_date: 180905)
-    expect(result).must_equal []
-  end
-
-  it "raises argument error when try to overbook" do
-    20.times do
-      @booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180905)
-    end
-
-
-    expect{(@booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180905))}.must_raise ArgumentError
+    expect(@booking.reservations).must_be_kind_of Array
   end
 
   it "returns array of all room numbers in hotel" do
@@ -126,13 +19,26 @@ describe "BookingSystem class" do
     expect(@booking.list_all_rooms).must_equal [*1..20]
   end
 
-  it "returns booked rooms of that specific date" do
-    @booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180906)
-    @booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180906)
-    @booking.reserve_room_for_date_range(check_in_date: 180904, check_out_date: 180906)
-
-    expect(@booking.list_reservations_by_date(180904)).must_be_kind_of Array
-    expect(@booking.list_reservations_by_date(180904)[2]).must_be_kind_of Hotel::ReservationCreator
-    expect(@booking.list_reservations_by_date(180904).length).must_equal 3
+  it "returns array of dates" do
+    expect(@reservation).must_be_kind_of Hotel::ReservationCreator
+    expect(@reservation.date_range).must_equal [Date.parse("180904"), Date.parse("180905"), Date.parse("180906")]
   end
+
+  it "returns the total cost of the booking" do
+    expect(@reservation.calculate_booking_cost).must_equal 600
+  end
+
+  it "list all bookings of that date" do
+    bad_reservation = @booking.make_reservation(check_in_date: 180909, check_out_date: 1809010)
+
+    expect(@booking.list_reservations_by_date(180904)).must_include @reservation
+    expect(@booking.list_reservations_by_date(180904)).wont_include bad_reservation
+  end
+
+  # it "does not assign room when room is booked for that date" do
+  #   bad_reservation = @booking.make_reservation(check_in_date: 180904, check_out_date: 180905)
+  #
+  #   expect()
+  #
+  # end
 end
