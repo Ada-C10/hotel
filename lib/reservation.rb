c93c962e2b560a2fe7b7b5445fd7fd0f6d896675
Lindsay Terchin <lterchin@gmail.com>
2018-09-12 21:24:55 -0700

    cleaned up reservation class & tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -4,34 +4,31 @@ require 'pry'
 
 module Hotel
   class Reservation
-    attr_reader :start_date, :end_date, :room_id
+    attr_reader :start_date, :end_date, :room_id, :date_range, :total_cost
 
-    def initialize(start_date, end_date, room_id)
+    def initialize(date_range, room_id)
 
-      @start_date = start_date
-      @end_date = end_date
+      @date_range = date_range
       @room_id = room_id
-      @total_cost = reservation_cost(@start_date, @end_date)
-
-      validate_dates
+      @total_cost = reservation_cost(date_range)
 
     end
 
-
-    def validate_dates
-      raise ArgumentError.new("The end date must be after the start date") if @end_date <= @start_date
-
-      raise ArgumentError.new("The start date can't be nil") if @start_date == nil
-
-      raise ArgumentError.new("The end date can't be nil") if @end_date == nil
-    end
-
-
-    def reservation_cost(start_date, end_date)
+    def reservation_cost(date_range)
       nightly_cost = 200
-      total_days = (end_date - start_date).to_i
+      total_days = (date_range.length).to_i
       total_cost = total_days * nightly_cost
     end
 
   end
 end
+
+
+
+# def validate_dates
+#   raise ArgumentError.new("The end date must be after the start date") if @end_date <= @start_date
+#
+#   raise ArgumentError.new("The start date can't be nil") if @start_date == nil
+#
+#   raise ArgumentError.new("The end date can't be nil") if @end_date == nil
+# end
--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -5,32 +5,39 @@ describe "Reservation class" do
 
   describe "Reservation initialization" do
 
+    before do
+      date_range = [1,2,3]
+      @reservation = Hotel::Reservation.new(date_range, 4)
+    end
+
     it "is an instance of Reservation" do
-      start_date = Date.new(2015, 03, 01)
-      end_date = Date.new(2015, 03, 05)
-      @reservation = Hotel::Reservation.new(start_date, end_date, 4)
       expect(@reservation).must_be_kind_of Hotel::Reservation
     end
 
+    it "is initialized with a date range, room id and total cost" do
+      expect(@reservation).must_respond_to :date_range
+      expect(@reservation.date_range).must_be_kind_of Array
 
-    it "must throw an argument error if the end date is before the start date" do
-      start_date = Date.new(2015, 03, 05)
-      end_date = Date.new(2015, 03, 01)
-      expect {Hotel::Reservation.new(start_date, end_date)}.must_raise ArgumentError
+      expect(@reservation).must_respond_to :room_id
+      expect(@reservation.room_id).must_be_kind_of Integer
+
+      expect(@reservation).must_respond_to :total_cost
+      expect(@reservation.total_cost).must_be_kind_of Integer
+
     end
   end
 
 
   describe "reservation cost" do
     it "returns the total amount of the new reservation" do
-      start_date = Date.new(2015, 03, 01)
-      end_date = Date.new(2015, 03, 05)
-      @reservation = Hotel::Reservation.new(start_date, end_date, 3)
+
+      date_range = [1,2,3,4]
+      @reservation = Hotel::Reservation.new(date_range, 3)
 
       start_date = @reservation.start_date
       end_date = @reservation.end_date
 
-      expect(@reservation.reservation_cost(start_date, end_date)).must_equal 800
+      expect(@reservation.reservation_cost(date_range)).must_equal 800
     end
   end
 
