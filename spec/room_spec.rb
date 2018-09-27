7c51ff97f1492f7f6685a4c2bb3d487142388183
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-27 12:22:51 -0700

    add test for room to check find_available_room if all rooms unavailable

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -24,5 +24,10 @@ describe "Room class" do
       expect(new_block.length).must_equal 5
       expect(new_block).must_equal [3, 4, 6, 7, 8]
     end
+
+    it "raises an ArgumentError if a request is made for an unavailable room" do
+      @array_of_used_rooms = %w[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20]
+      expect { @rooms.find_available_room }.must_raise ArgumentError
+    end
   end
 end
