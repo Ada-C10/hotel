345edb73205cc6db95110acf52cfb2c483754eb5
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-27 15:56:39 -0700

    Updating tests for Reservation attribute added

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -16,33 +16,33 @@ describe 'Reservation' do
   end
 
   it 'raises an StandardError if an invalid room is entered' do
-    # reservation_num = 666
+    room_instance = Room.new(15)
     room_num = 30
     start_date = Date.new(2018,10,6)
     end_date = Date.new(2018,10,8)
     room_type = :standard
 
-    expect{Reservation.new(room_num, start_date, end_date, room_type)}.must_raise StandardError
+    expect{Reservation.new(room_instance, room_num, start_date, end_date, room_type)}.must_raise StandardError
   end
 
   it 'raises an StandardError if the start date is later than the end date' do
-    # reservation_num = 666
+    room_instance = Room.new(15)
     room_num = 15
     start_date = Date.new(2018,10,11)
     end_date = Date.new(2018,10,6)
     room_type = :standard
 
-    expect{Reservation.new(room_num, start_date, end_date, room_type)}.must_raise StandardError
+    expect{Reservation.new(room_instance, room_num, start_date, end_date, room_type)}.must_raise StandardError
   end
 
   it 'raises an StandardError if an invalid room type is entered' do
-    # reservation_num = 666
+    room_instance = Room.new(15)
     room_num = 15
     start_date = Date.new(2018,10,10)
     end_date = Date.new(2018,10,15)
     room_type = :deluxe
 
-    expect{Reservation.new(room_num, start_date, end_date, room_type)}.must_raise StandardError
+    expect{Reservation.new(room_instance, room_num, start_date, end_date, room_type)}.must_raise StandardError
   end
 
   it 'correctly calculates duration of trip' do
@@ -69,6 +69,20 @@ describe 'Reservation' do
     new_reservation = Reservation.new(room_instance, room_num, start_date, end_date, room_type)
 
     expect(new_reservation.total_cost).must_equal 400
+  end
+
+  it 'updates booked dates for a room instance' do
+    # reservation_num = 5555
+    room_instance = Room.new(1)
+    room_num = 1
+    start_date = Date.new(2018,9,6)
+    end_date = Date.new(2018,9,8)
+    room_type = :standard
+
+    new_reservation = Reservation.new(room_instance, room_num, start_date, end_date, room_type)
+
+    new_reservation.update_room_booked_dates
+    expect(room_instance.dates_booked.length).must_equal 2
   end
 
   # it 'correctly calculates total cost of a reservation for a block booking' do
