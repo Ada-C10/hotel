7cbf753a12b1cf169e619d4ac5112c3a33b2622e
Kat Perreira <kat.perreira@gmail.com>
2018-09-28 18:54:55 -0700

    Made changes to reflect feedback from Dee in comments and during one-on-one

--- spec/booking_manager_spec.rb
+++ spec/booking_manager_spec.rb
@@ -1,5 +1,4 @@
 require_relative 'spec_helper.rb'
-require 'pry'
 require 'minitest/skip_dsl'
 
 
@@ -27,34 +26,61 @@ describe 'BookingManager' do
 
   end
 
-end
+  describe '#find_available_rooms' do
+    # I commented the following tests out because I took out the standard error check.
+    # fully_booked: hotel filled from 2018, 1, 1 til 2018 1, 7
+    # - Same dates
+    # it 'must raise standard error if two reservations made for the same date' do
+    #   expect{fully_booked.make_reservation(Date.new(2018, 1, 1), Date.new(2018, 1, 7))}.must_raise StandardError
+    # end
+    # # # - Overlaps in the front: overlap from 2018, 1 ,1 til 2018, 1, 5
+    # it 'raises an error if room bookings overlap in the front' do
+    #   expect{fully_booked.make_reservation(Date.new(2017, 12, 25), Date.new(2018, 1, 5))}.must_raise StandardError
+    # end
+    # #
+    # # # - Overlaps in the back: overlap from 2018, 1, 3 to 2018, 1, 5
+    # it 'raises an error if room bookings overlap in the back' do
+    #   expect{fully_booked.make_reservation(Date.new(2017, 1, 3), Date.new(2018, 1, 9))}.must_raise StandardError
+    # end
+    # #
+    # # # - Completely contained
+    # it 'raises an error if room bookings completely contained in fully booked hotel' do
+    #   expect{fully_booked.make_reservation(Date.new(2017, 1, 2), Date.new(2018, 1, 5))}.must_raise StandardError
+    # end
+    # #
+    # # # Completely containing
+    # it 'raises an error if room bookings completely containing block where hotel is maxed out' do
+    #   expect{fully_booked.make_reservation(Date.new(2017, 12, 25), Date.new(2018, 1, 10))}.must_raise StandardError
+    # end
 
 
-describe '#find_available_rooms' do
-  # fully_booked: hotel filled from 2018, 1, 1 til 2018 1, 7
-  # - Same dates
-  it 'must raise standard error if two reservations made for the same date' do
-    expect{fully_booked.make_reservation(Date.new(2018, 1, 1), Date.new(2018, 1, 7))}.must_raise StandardError
+    # - Completely before. ***THIS is where everything fell apart. I took it out in my final submission and a few things happend when I did. 1. The block closures thruought this spec got messed up. I command-i'ed my way to a bunch of passes and hit submit because of fatigue, I had spent too many hours debugging and I was ready to let the instructors solve this for me and be done with it. Not an excuse, just my explination for why. 2. All the passes were wrong, the errors were actually from my taking ends and putting in the wrong place. Also from failed logic with all my raising error tests. I put back all the tests about conditions when the reservations are supposed to be made. I also rewrote/reviewed my notes on how to write tests and how errors work from a couple weeks ago.
+    it 'Successfully books reservations if reservations made completely before maxed out rooms' do
+      good_reservation1 = fully_booked.make_reservation(Date.new(2017, 12, 25), Date.new(2017, 12, 30))
+      #reservation is valid, returns an booking_manager object with  21 good reservations
+      expect(good_reservation1.length).must_equal 21
+    end
+
+    # - Completely after
+    it 'Successfully books reservations if reservations made completely after maxed out rooms' do
+      good_reservation2 = fully_booked.make_reservation(Date.new(2018, 1, 8), Date.new(2018, 1, 12))
+      expect(good_reservation2.length).must_equal 21
+    end
+
+    # - Ends on the checkin date
+    it 'Successfully books reservations if reservation ends on the check in date' do
+      good_reservation2 = fully_booked.make_reservation(Date.new(2017, 12, 22), Date.new(2018, 1, 1))
+      expect(good_reservation2.length).must_equal 21
+    end
+
+    # - Starts on the checkout date (edited)
+    it 'Successfully books reservations if reservation starts on the checkout date' do
+      good_reservation2 = fully_booked.make_reservation(Date.new(2018, 1, 7), Date.new(2018, 1, 10))
+      expect(good_reservation2.length).must_equal 21
+    end
+
+    # This is the end that belongs to the describe block above
   end
-  # - Overlaps in the front: overlap from 2018, 1 ,1 til 2018, 1, 5
-  it 'raises an error if room bookings overlap in the front' do
-    expect{fully_booked.make_reservation(Date.new(2017, 12, 25), Date.new(2018, 1, 5))}.must_raise StandardError
-  end
 
-  # - Overlaps in the back: overlap from 2018, 1, 3 to 2018, 1, 5
-  it 'raises an error if room bookings overlap in the back' do
-    expect{fully_booked.make_reservation(Date.new(2017, 1, 3), Date.new(2018, 1, 9))}.must_raise StandardError
-  end
-
-  # - Completely contained
-  it 'raises an error if room bookings completely contained in fully booked hotel' do
-    expect{fully_booked.make_reservation(Date.new(2017, 1, 2), Date.new(2018, 1, 5))}.must_raise StandardError
-  end
-
-  # Completely containing
-  it 'raises an error if room bookings completely containing block where hotel is maxed out' do
-    expect{fully_booked.make_reservation(Date.new(2017, 12, 25), Date.new(2018, 1, 10))}.must_raise StandardError
-  end
-
-  
+  # This is the end for the 'let' block
 end
